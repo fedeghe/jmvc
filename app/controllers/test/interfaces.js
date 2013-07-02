ed5251564a5624cbab80c854d2549937740ac0a1
fedeghe <fedeghe@gmail.com>
2013-07-02 23:25:16 +0200

    mini test for Interface object

---
+++ app/controllers/test/interfaces.js
@@ -0,0 +1,23 @@
+JMVC.controllers.interfaces = function() {
+
+	this.index = function(){
+		JMVC.loadInterfaces('loader');
+		var t1 = function () {
+			this.load = function (x) {console.debug(x)};
+			this.unload = function (x) {console.debug(x)};
+		},
+		t2 = function () {
+			this.check = function (x) {console.debug(x)};
+			this.init = function (x) {console.debug(x)};
+		},
+		t3 = function () {
+			this.check = function (x) {console.debug(x)};
+			this.init = function (x) {console.debug(x)};
+			this.parse = function (x) {console.debug(x)};
+			this.inwit = function (x) {console.debug(x)};
+		};
+		console.debug(JMVC.interfaces.loader.check(t1));
+		console.debug(JMVC.interfaces.grep.check(t2))
+		console.debug(JMVC.interfaces.grep.check(t3));
+	};
+};
