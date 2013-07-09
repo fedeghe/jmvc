d5b6bd9c125cb0615d9ff85cf9a6ca1757957228
fedeghe <fedeghe@gmail.com>
2013-07-10 00:19:13 +0200

    enhanced settings object, better styling

--- app/extensions/tabs.js
+++ app/extensions/tabs.js
@@ -90,51 +90,51 @@ JMVC.extend('tabs', {
 			JMVC.events.end(function () {
 				var realheight = parseInt(JMVC.css.getComputedStyle(  JMVC.dom.find('#' + ul_id), 'height'), 10) * 3,
 					colors = {
-						'oriz':{
-							'base' : '#aaa',
-							'selected' : '#fff',
-							'hover' : '#eee',
-							'border':'#000'
+						oriz : {
+							base : '#aaa',
+							selected : '#fff',
+							hover : '#ddd',
+							border : '#888',
+							text : '#000',
+							textselected : '#000',
+							texthover : '#000',
+							containerbg : '#fff',
+							textcontent : '#000' 
 						},
-						'vert':{
-							'base' : '#aaa',
-							'selected' : '#fff',
-							'hover' : '#eee',
-							'border':'#080'
+						vert : {
+							base : '#aaa',
+							selected : '#fff',
+							hover : '#ddd',
+							border : '#888',
+							text : '#000',
+							textselected : '#000',
+							texthover : '#000',
+							containerbg : '#fff',
+							textcontent : '#000' 
 						}
 					},
 					sizes = {
-						'oriz' : {
-							'border' : 2,
-							'round' : 5
-						},
-						'vert' : {
-							'border' : 2,
-							'round' : 5
-						}
+						oriz : {border : 2, round : 5},
+						vert : {border : 2, round : 5}
 					}
 					styles = {
-						'ul.tablabels_o':'height:30px; list-style:none;margin:0px;padding:0px;',
-						'ul.tablabels_o li':'float:left; line-height:1;font-size:100%;padding:0px;margin:0px;border:0px;background-color:' + colors['oriz']['base'] + ';padding:5px; cursor:pointer;margin-left:5px;'+
-							'border:solid ' + sizes['oriz']['border'] + 'px ' + colors['oriz']['border'] + ';-moz-border-radius-topleft: ' + sizes['oriz']['round'] + 'px;-moz-border-radius-topright:' + sizes['oriz']['round'] + 'px;-moz-border-radius-bottomleft:0px;-moz-border-radius-bottomright:0px;'+
-							'-webkit-border-top-left-radius:' + sizes['oriz']['round'] + 'px;-webkit-border-top-right-radius:' + sizes['oriz']['round'] + 'px;-webkit-border-bottom-left-radius:0px;-webkit-border-bottom-right-radius:0px;'+
-							'border-top-left-radius:' + sizes['oriz']['round'] + 'px;border-top-right-radius:' + sizes['oriz']['round'] + 'px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;',
-						'ul.tablabels_o li:hover':'background-color:' + colors['oriz']['hover'] + ';',
-						'ul.tablabels_o li.sel':'border:1px solid ' + colors['oriz']['border'] + ';border-bottom:none; background-color:' + colors['oriz']['selected'] + '; z-index:10;position:relative;top:' + (sizes['oriz']['border']) + 'px',
-						'div.tabcontainer_o' : border3 + 'line-height:1;font-size:100%;padding:0px;margin:0px;border:0px;border:' + sizes['oriz']['border'] + 'px solid ' + colors['oriz']['border'] + ';position:relative;top:-3px;z-index:5; padding:5px;background-color:white',
-
-
-
-
-						'ul.tablabels_v':'float:left;list-style:none;margin:0px;padding:0px;',
-
-						'ul.tablabels_v li':'display:block; text-align:right;line-height:1;font-size:100%;padding:0px;margin:0px;border:0px;background-color:' + colors['vert']['base'] + ';padding:5px; cursor:pointer;margin-top:5px;left:' + sizes['vert']['round'] + 'px;padding-right:' + 2*sizes['vert']['round'] + 'px;position:relative;' +
-								'border:solid ' + sizes['vert']['border'] + 'px ' + colors['vert']['border'] + ';-moz-border-radius-topleft: ' + sizes['vert']['round'] + 'px;-moz-border-radius-topright:0px;-moz-border-radius-bottomleft:' + sizes['vert']['round'] + 'px;-moz-border-radius-bottomright:0px;'+
-								'-webkit-border-top-left-radius:' + sizes['vert']['round'] + 'px;-webkit-border-top-right-radius:0px;-webkit-border-bottom-left-radius:' + sizes['vert']['round'] + 'px;-webkit-border-bottom-right-radius:0px;'+
-								'border-top-left-radius:' + sizes['vert']['round'] + 'px;border-top-right-radius:0px;border-bottom-left-radius:' + sizes['vert']['round'] + 'px;border-bottom-right-radius:0px;',
-						'ul.tablabels_v li:hover':'background-color:' + colors['vert']['hover'] + ';',
-						'ul.tablabels_v li.sel':'border:' + sizes['vert']['border'] + 'px solid ' + colors['vert']['border'] + ';border-right:none; background-color:' + colors['vert']['selected'] + '; z-index:10;position:relative;left:' + (sizes['vert']['border']-1) + 'px',
-						'div.tabcontainer_v' : border3 + 'min-height:' + realheight + 'px;overflow:hidden; position:relative;line-height:1;font-size:100%;padding:0px;margin:0px;border:0px;border:' + sizes['vert']['border'] + 'px solid ' + colors['vert']['border'] + ';left:-1px;z-index:5; padding:5px;background-color:white'
+						'ul.tablabels_o':'overflow:hidden; list-style:none;margin:0px;padding:0px;',
+						'ul.tablabels_o li':'color:' + colors.oriz.text + ';float:left; line-height:1;font-size:100%;padding:0px;margin:0px;border:0px;background-color:' + colors.oriz.base + ';padding:5px; cursor:pointer;margin-left:5px;'+
+							'border:solid ' + sizes.oriz.border + 'px ' + colors.oriz.border + ';-moz-border-radius-topleft: ' + sizes.oriz.round + 'px;-moz-border-radius-topright:' + sizes.oriz.round + 'px;-moz-border-radius-bottomleft:0px;-moz-border-radius-bottomright:0px;'+
+							'-webkit-border-top-left-radius:' + sizes.oriz.round + 'px;-webkit-border-top-right-radius:' + sizes.oriz.round + 'px;-webkit-border-bottom-left-radius:0px;-webkit-border-bottom-right-radius:0px;'+
+							'border-top-left-radius:' + sizes.oriz.round + 'px;border-top-right-radius:' + sizes.oriz.round + 'px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;',
+						'ul.tablabels_o li:hover':'color:' + colors.oriz.texthover + ';background-color:' + colors.oriz.hover + ';',
+						'ul.tablabels_o li.sel':'color:' + colors.oriz.textselected + ';border:' + sizes.oriz.border + 'px solid ' + colors.oriz.border + ';border-bottom:none; background-color:' + colors.oriz.selected + '; z-index:10;position:relative;bottom:-1px; ',
+						'div.tabcontainer_o' : 'color:' + colors.oriz.textcontent +';background-color:' + colors.oriz.containerbg +';' + border3 + 'line-height:1;font-size:100%;padding:0px;margin:0px;border:0px;border:' + sizes.oriz.border + 'px solid ' + colors.oriz.border + ';position:relative;top:-3px;z-index:5;padding:5px;',
+						///////////////////////////////////////////////////////////////////////
+						'ul.tablabels_v':'color:' + colors.vert.text + ';float:left;list-style:none;margin:0px;padding:0px;',
+						'ul.tablabels_v li':'display:block; text-align:right;line-height:1;font-size:100%;padding:0px;margin:0px;border:0px;background-color:' + colors.vert.base + ';padding:5px; cursor:pointer;margin-top:5px;left:' + sizes.vert.round + 'px;padding-right:' + 2 * sizes.vert.round + 'px;position:relative;' +
+								'border:solid ' + sizes.vert.border + 'px ' + colors.vert.border + ';-moz-border-radius-topleft: ' + sizes.vert.round + 'px;-moz-border-radius-topright:0px;-moz-border-radius-bottomleft:' + sizes.vert.round + 'px;-moz-border-radius-bottomright:0px;'+
+								'-webkit-border-top-left-radius:' + sizes.vert.round + 'px;-webkit-border-top-right-radius:0px;-webkit-border-bottom-left-radius:' + sizes.vert.round + 'px;-webkit-border-bottom-right-radius:0px;'+
+								'border-top-left-radius:' + sizes.vert.round + 'px;border-top-right-radius:0px;border-bottom-left-radius:' + sizes.vert.round + 'px;border-bottom-right-radius:0px;',
+						'ul.tablabels_v li:hover':'color:' + colors.vert.texthover + ';background-color:' + colors.vert.hover + ';',
+						'ul.tablabels_v li.sel':'color:' + colors.vert.textselected + ';border:' + sizes.vert.border + 'px solid ' + colors.vert.border + ';border-right:none; background-color:' + colors.vert.selected + '; z-index:10;position:relative;left:1px',
+						'div.tabcontainer_v' : 'color:' + colors.vert.textcontent +';background-color:' + colors.vert.containerbg +';' + border3 + 'min-height:' + realheight + 'px;overflow:hidden; position:relative;line-height:1;font-size:100%;padding:0px;margin:0px;border:0px;border:' + sizes.vert.border + 'px solid ' + colors.vert.border + ';left:-1px;z-index:5; padding:5px;'
 					};
 				
 				
@@ -166,13 +166,13 @@ JMVC.extend('tabs', {
 					hideall();
 
 					var target = JMVC.events.eventTarget(e),
-
-
 						//show tab
 						id_orig = JMVC.dom.attr(target, 'id'),
 						id_tab2show = id_orig.replace('jvmc_tb','jvmc_cont');
 
-					JMVC.css.style(JMVC.dom.find('#'+id_tab2show), 'display', 'block');
+					//JMVC.css.style(JMVC.dom.find('#'+id_tab2show), 'display', 'block');
+					JMVC.css.show(JMVC.dom.find('#'+id_tab2show));
+
 					JMVC.dom.switchClass(target, 'unsel', 'sel');
 
 				});
@@ -181,9 +181,6 @@ JMVC.extend('tabs', {
 			return ids2return;
 		};
 		
-		//JMVC.debug(JMVC.util.json2css(this.styles));
 		
 	}
-	
 });
-
