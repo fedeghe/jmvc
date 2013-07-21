2353b1086578eeb15ae04d59d8611ad15a97adf7
fedeghe <fedeghe@gmail.com>
2013-07-21 16:57:18 +0200

    meta to remove upper url bar

--- app/extensions/core/mobile/mobile.js
+++ app/extensions/core/mobile/mobile.js
@@ -1,8 +1,14 @@
 JMVC.extend('mobile', {
 	'noZoom' : function(){
 		JMVC.head.meta('viewport', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0');
+		
 	},
 	topHide : function () {
-		JMVC.W.scrollTo(0, 100);
+		//JMVC.W.scrollTo(0, 100);
+		JMVC.head.meta('apple-mobile-web-app-capable', 'yes');
+		JMVC.W.setTimeout(function(){
+			// Hide the address bar!
+			JMVC.W.scrollTo(0, 1);
+		}, 0);
 	}
 });
