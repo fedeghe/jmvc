fe87b0d2fee5bcaba808dc10e0bad0874245fb72
fedeghe <fedeghe@gmail.com>
2013-07-26 00:57:59 +0200

    experiment with carpet controller action

--- app/controllers/carpet.js
+++ app/controllers/carpet.js
@@ -1,7 +1,7 @@
 JMVC.controllers.carpet = function () {
 	"use strict";
 	//default action
-	this.action_index = function () {
+	this.action_index = function (f) {
 
 		// get lib & view 
 		JMVC.require('core/lib/carpet', 'core/dim');
@@ -13,15 +13,21 @@ JMVC.controllers.carpet = function () {
 			jmap = false,
 			dims = JMVC.dim.getViewportSize(),
 			Ww = dims.width,
-			Wh = dims.height;
+			Wh = dims.height,
+			that = this;
 
 		//top = left = 100 from default css
+			
 		
+
+
 		index.render(function(){
 
 
 			// cssmargin5 * sides2 +border1 
 			jmap = JMVC.carpet.create(JMVC.dom.body(), Ww-11, Wh-11, 3);
+
+			that.set('jmap', jmap);
 
 			if (JMVC.p.debug == 'true') {		
 				jmap.beforeAdd(function (i) {console.debug('adding ID: '+i); });
@@ -35,7 +41,24 @@ JMVC.controllers.carpet = function () {
 			//console.dir(jmap.carpet.actualNodes);
 			//jmap.move(-10,1)
 			//console.dir(jmap.carpet.actualNodes);
+			f && f();
 		});
+
+	};
+
+	this.action_logo = function () {
+		var that = this;
+
+		this.action_index(function () {
+			console.debug(that);
+			var jmap = that.get('jmap');
+				jmap.beforeAdd(function (i) {console.debug('adding ID: '+i); });
+				jmap.afterAdd(function (i) {console.debug('added ID: '+i); });
+				jmap.beforeRemove(function (i) {console.debug('removing ID: '+i); });
+				jmap.afterRemove(function (i) {console.debug('removed ID: '+i); });
+			console.debug(jmap);	
+		});
+		
 
 	};
 
