a253e4b19cb28dda17e9fe89235289f8642bbfb1
fedeghe <fedeghe@gmail.com>
2013-08-09 23:28:56 +0200

    interface -> Interface

--- app/interfaces/loader.interface.js
+++ app/interfaces/loader.interface.js
@@ -1,2 +1,2 @@
-JMVC.interfaces.loader = new JMVC.interface(['load', 'unload']);
-JMVC.interfaces.grep = new JMVC.interface(['check', 'init', 'parse']);
+JMVC.interfaces.loader = new JMVC.Interface(['load', 'unload']);
+JMVC.interfaces.grep = new JMVC.Interface(['check', 'init', 'parse']);
