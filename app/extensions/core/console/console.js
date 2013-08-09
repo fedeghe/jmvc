6014b8b6bf036c3ceac19f78766b4bcd59416c7a
fedeghe <fedeghe@gmail.com>
2013-08-09 23:26:43 +0200

    JMVC.console function update

--- app/extensions/core/console/console.js
+++ app/extensions/core/console/console.js
@@ -9,18 +9,23 @@ JMVC.extend('console', {
 		JMVC.head.meta("generator", "jmvc resident in your machine");
 	},
 	'_status' : false,
+	'scroll' : 0,
+	'tpl' : '<!DOCTYPE html><html><head><style type="text/css">%style%</style><script type="text/javascript">function ___(){%script%}; window.onload = ___();</script></head><body>%body%</body></html>',
 	'toggle' : function () {
 		if (JMVC.console._status) {
 			JMVC.dom.remove(JMVC.dom.find('#jmvc_console'));
 			JMVC.events.enable_scroll();
+			JMVC.W.scrollTo(0, JMVC.console.scroll);
 		} else {
-			JMVC.events.disable_scroll();
+			
 			var dims = JMVC.dim.getViewportSize(),
 				border_size = 0,
-				margin = 0,
+				margin = -1,
 				top_height = 10,
 				foot_height = 100,
-				// create the main container
+				scrollTop = JMVC.dim.getScreenData().scrollTop;
+				
+				// main container
 				container = JMVC.dom.create(
 					'div',{
 						'id' : 'jmvc_console',
@@ -28,6 +33,7 @@ JMVC.extend('console', {
 						'style':'left:' + margin + 'px;right:' + margin + 'px;top:' + margin + 'px;bottom:' + margin + 'px;border:' + border_size + 'px solid black'
 					}
 				),
+
 				config  = [
 					{
 						"target" : '#jmvc_console',
@@ -42,8 +48,20 @@ JMVC.extend('console', {
 							{
 								"attrs" : {"id" : "html"},
 								"float" : "left",
-								"class" : "p50",
+								"class" : "p33",
 								"inner" : [{
+									"attrs" : {"id" : "htmlarea"},
+									"class" : "innerbox round4",
+									"tag":"textarea",
+									"style" : {"width": "99%"}
+								}]
+							},
+							{
+								"attrs" : {"id" : "js"},
+								"float" : "left",
+								"class" : "p33",
+								"inner" : [{
+									"attrs" : {"id" : "jsarea"},
 									"class" : "innerbox round4",
 									"tag":"textarea",
 									"style" : {"width": "99%"}
@@ -52,8 +70,9 @@ JMVC.extend('console', {
 							{
 								"attrs" : {"id" : "css"},
 								"float" : "left",
-								"class" : "p50",
+								"class" : "p33",
 								"inner" : [{
+									"attrs" : {"id" : "cssarea"},
 									"class" : "innerbox round4",
 									"tag":"textarea",
 									"style" : {"width": "99%"}
@@ -61,34 +80,69 @@ JMVC.extend('console', {
 							},
 							"clearer",
 							{
-								"attrs" : {"id" : "javascript"},
-								"float" : "left",
-								"class" : "p50",
+								"attrs" : {"id" : "out"},
+								"class" : "p100",
 								"inner" : [{
 									"class" : "innerbox round4",
-									"tag":"textarea",
-									"style" : {"width": "99%"}
+									"tag":"iframe",
+									"attrs" : {
+										"id" : "outarea",
+										"width":"100%",
+										"height":"300"
+									},
+									"style" : {"width": "100%"}
 								}]
-							},
-							{
-								"attrs" : {"id" : "result"},
-								"float" : "left",
-								"class" : "p50",
-								"inner" : [{
-									"class" : "innerbox round4",
-									"tag":"textarea",
-									"style" : {"width": "99%"}
-								}]
-							},
-							"clearer"
+							}
 						]
 					},
 				];
+
+			//save scroll vertical position
+			JMVC.console.scroll = scrollTop;
+
+			//scroll to top
+			JMVC.W.scrollTo(0, 1);
+
+			//disable scroll
+			JMVC.events.disable_scroll();
+
 			JMVC.set('height', (dims.height-70) / 2);
 			JMVC.head.addstyle(JMVC.vars.baseurl + '/app/extensions/core/console/console.css', true);
 			JMVC.dom.append(JMVC.dom.body(), container);
-
 			JMVC.grind.render(config);
+
+
+			JMVC.dom.html(JMVC.dom.find('#htmlarea'), JMVC.htmlspecialchars("<h1>hello</h1>"));
+			JMVC.dom.html(JMVC.dom.find('#jsarea'), JMVC.htmlspecialchars("var foo = JMVC.dom.find('h1');"));
+			JMVC.dom.html(JMVC.dom.find('#cssarea'), JMVC.htmlspecialchars("body{\n\tbackground-color:red;\n}"));
+
+
+			JMVC.events.bind(JMVC.dom.find('#htmlarea'), 'keyup', function () {update(); });
+			JMVC.events.bind(JMVC.dom.find('#jsarea'), 'keyup', function () {update(); });
+			JMVC.events.bind(JMVC.dom.find('#cssarea'), 'keyup', function () {update(); });
+
+			function update(){
+				
+				var h = JMVC.htmlspecialchars_decode(JMVC.dom.find('#htmlarea').value),
+					j = JMVC.htmlspecialchars_decode(JMVC.dom.find('#jsarea').value),
+					c = JMVC.dom.find('#cssarea').value;
+
+				JMVC.dom.find('#outarea').contentDocument.documentElement.innerHTML = JMVC.string.replaceall(JMVC.console.tpl, {
+					'style' : c,
+					'script' : j,
+					'body' : h
+				});
+
+				JMVC.dom.find('#outarea').contentWindow.JMVC = JMVC;
+				
+				try {
+					eval.call(JMVC.dom.find('#outarea').contentWindow, j);
+				}catch(e){}
+			}
+			JMVC.events.delay(function () {update(); }, 0);
+			
+
+
 		}
 		JMVC.console._status = !JMVC.console._status;
 	}
