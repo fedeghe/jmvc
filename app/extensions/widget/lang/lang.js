c70d2ac8cae845b0972c58e2565c7cdf93babacc
fedeghe <fedeghe@gmail.com>
2013-08-09 23:28:35 +0200

    updated lang style and clean up code

--- app/extensions/widget/lang/lang.js
+++ app/extensions/widget/lang/lang.js
@@ -1,15 +1,16 @@
 JMVC.extend('widget.langs', {
-	init : function () {
-		JMVC.head.addstyle(JMVC.vars.baseurl + JMVC.US + 'app' + JMVC.US + 'extensions' + JMVC.US + 'widget' + JMVC.US + 'lang' + JMVC.US + 'lang.css');
-	},
-	create : function (cnt) {
-		var mkp = JMVC.dom.create('ul', {'class' : 'lang'}),
-			currentlang = JMVC.vars.currentlang;
-		
 
+	//get the basic styles
+	init : function () {
+		JMVC.head.addstyle(JMVC.vars.baseurl + '/app/extensions/widget/lang/lang.css');
+	},
+
+	//create the ul with home langs links within
+	create : function (cnt) {
+		var mkp = JMVC.dom.create('ul', {'class' : 'lang'});
 		JMVC.each(JMVC.i18n, function (e, i){
 			var img = JMVC.dom.create('img', {src : JMVC.vars.baseurl + JMVC.US +'media' + JMVC.US + 'img' + JMVC.US + 'flags' + JMVC.US + i + '.gif'}),
-				a = JMVC.dom.create('a', {href : JMVC.vars.baseurl + JMVC.US + '?lang=' + i, 'class' : currentlang === i ? 'active' : '' }, img),
+				a = JMVC.dom.create('a', {'href' : JMVC.vars.baseurl + JMVC.US + '?lang=' + i, 'class' : JMVC.vars.currentlang === i ? 'active' : ''}, img),
 				li = JMVC.dom.create('li', {}, a);
 			JMVC.dom.append(mkp, li);
 		});
