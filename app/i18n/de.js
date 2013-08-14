ca8f497de1b18b11d8f1837ffd5346f1e8157726
fedeghe <fedeghe@gmail.com>
2013-08-15 00:06:36 +0200

    added german to home

---
+++ app/i18n/de.js
@@ -0,0 +1,11 @@
+JMVC.i18n.de = {
+	'ciao' : 'Hallo',
+	'come stai' : 'Wie geht es dir',
+	"sei in locale" : "sind in lokalen",
+	'pure javascript mvc framework' : 'Reine JavaScript-MVC-Framework',
+	'more':'mehr',
+	'source':'quelle',
+	'desktop' : 'desktop',
+	'mobile':'mobil',
+	'api' : 'api'
+};
