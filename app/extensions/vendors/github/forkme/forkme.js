7a6e5218a26f594bbbc6e2beb2066d6906a08656
fedeghe <fedeghe@gmail.com>
2013-08-16 00:33:53 +0200

    updated

--- app/extensions/vendors/github/forkme/forkme.js
+++ app/extensions/vendors/github/forkme/forkme.js
@@ -1,16 +1,14 @@
 JMVC.extend('github', {
-	vars : {
-		image : JMVC.vars.baseurl + '/media/img/forkme_right_gray_6d6d6d.png'
-		//image : JMVC.vars.baseurl + '/media/img/github.svg'
-	},
+	vars : {},
 	init : function () {
-		JMVC.dom.preloadImage(JMVC.github.image);
+		JMVC.github.vars.image = JMVC.vars.baseurl + '/media/img/forkme_right_gray_6d6d6d.png';
+		JMVC.dom.preloadImage(JMVC.github.vars.image);
 	},
 	forkme : function (ghname) {
 		var img = JMVC.dom.create('img', {
 				style : 'position: absolute; z-index:200; top: 0; right: 0; border: 0;'
 				,src : JMVC.github.vars.image
-				//,alt : 'Fork me on GitHub'
+				,alt : 'Fork me on GitHub'
 			}),
 			a = JMVC.dom.create('a',{
 				href : 'https://github.com/' + ghname,
