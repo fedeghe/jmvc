6e10b1b85e96e2b67c1bd2f06d1ef5c9dc02a9e4
fedeghe <fedeghe@gmail.com>
2013-08-18 01:52:48 +0200

    updated model

--- app/models/Persona.js
+++ app/models/Persona.js
@@ -1,4 +1,5 @@
-JMVC.models.Persona = function() {
-	this.name = 'Federic';
-	this.hello = function(name) {return 'Hello I`m '+(name || this.name) ;};
+JMVC.models.Persona = function(name, age) {
+	this.name = name  || 'Federic';
+	this.age = age || 4;
+	this.hello = function(n) {return 'Hello I`m '+(n || this.name) + ' and I`m ' + this.age + ' years old';};
 };
