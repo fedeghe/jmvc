651e8a4be1787fb842f21c9df1a7e7ca3ae70e05
fedeghe <fedeghe@gmail.com>
2013-08-20 01:27:17 +0200

    JMVC.each test updated

--- app/controllers/test/api/jmvc/each.js
+++ app/controllers/test/api/jmvc/each.js
@@ -3,7 +3,7 @@ JMVC.controllers.each = function() {
 		
 		JMVC.events.loadify(500);
 
-		//JMVC.head.lib('jquery');
+		JMVC.head.lib('jquery');
 		
 		this.render(function test(){
 			"use strict";
@@ -31,8 +31,8 @@ JMVC.controllers.each = function() {
 						return i + ':' + e;
 					})
 				},
-				times = 5000,
-				size = 100,
+				times = 100,
+				size = 5000,
 				jmvceach  = function (arr) {
 					var res = 0;
 					JMVC.each(arr, function (e, i) {
@@ -40,6 +40,16 @@ JMVC.controllers.each = function() {
 					});
 					return res;
 				},
+				nativeforeach = 'forEach' in Array.prototype ? 
+					function (arr) {
+						var res = 0;
+						arr.forEach(function (e, i) {
+							res += e;
+						})
+						return res;
+					}
+					:
+					function () {JMVC.test.message('no forEach implementation in that browser'); },
 				jqueryeach = function (arr) {
 					var res = 0;
 					jQuery.each(arr, function (i, e) {
@@ -47,7 +57,7 @@ JMVC.controllers.each = function() {
 					});
 					return res;
 				},
-				nativefor = function (arr) {
+				nativeforarray = function (arr) {
 					var res = 0,
 						i = 0,
 						l = arr.length;
@@ -55,17 +65,27 @@ JMVC.controllers.each = function() {
 						res += arr[i];
 					}
 					return res;
-				};
+				},
+				nativeforobj = function (obj) {
+					var res = 0,
+						i = 0;
+					for (i in obj) {
+						res += obj[i];
+					}
+					return res;
+				},
+				j = 0;
 
-			for (var j = 0; j < size; j++) {
+			for (null; j < size; j += 1) {
 				big.push(JMVC.util.rand(-1E6, 1E6));
 				obig['k' + j]  = JMVC.util.rand(-1E6, 1E6);
 			}
 
 			JMVC.test.testValue("loop(o);", function(){return loop(o); }, '(name:federico)(surname:ghedina)');
 			JMVC.test.testValue("destroy(a);", function(){return destroy(a).toString(); }, '0:zero,1:one,2:two,3:three,4:four,5:five');
-			JMVC.test.testValue("JMVC.each == nativefor;", function(){return jmvceach(big) == nativefor(big); }, true);
+			JMVC.test.testValue("JMVC.each == nativeforarray;", function(){return jmvceach(big) == nativeforarray(big); }, true);
 			//JMVC.test.testValue("JMVC.each == jQuery.each;", function(){return jmvceach(big) == jqueryeach(big); }, true);
+			JMVC.test.testValue("JMVC.each == nativeforobj;", function(){return jmvceach(obig) == nativeforobj(obig); }, true);
 			
 
 			
@@ -73,14 +93,19 @@ JMVC.controllers.each = function() {
 
 			JMVC.test.describe('<p>What about <i>JMVC.each</i> times against <strong>pure loop</strong> or <i>jQuery.each</i>?</p><a name="times"></a><h1>Times comparison</h1>here some implementation of the each function are executed '+times+' times with the same random array sized '+ size + ' with elements between -1E6 and 1E6\nand compared to a native loop implementation.');
 			
-			JMVC.test.testTime('native for (array)', nativefor, times, [big]);
+			JMVC.test.describe('With Array');
+			JMVC.test.testTime('native for (array)', nativeforarray, times, [big]);
+			JMVC.test.testTime('native forEach (array)', nativeforeach, times, [big]);
+			JMVC.test.testTime('jQuery.each (array)', jqueryeach, times, [big]);
 			JMVC.test.testTime('JMVC.each (array)', jmvceach, times, [big]);
-			//JMVC.test.testTime('jQuery.each (array)', jqueryeach, times, [big]);
-			JMVC.test.testTime('native for (obj literal)', nativefor, times, [obig]);
+
+			JMVC.test.describe('With Object literal');
+			JMVC.test.testTime('native for (obj literal)', nativeforobj, times, [obig]);
+			JMVC.test.testTime('jQuery.each (obj literal)', jqueryeach, times, [obig]);
 			JMVC.test.testTime('JMVC.each (obj literal)', jmvceach, times, [obig]);
-			//JMVC.test.testTime('jQuery.each (obj literal)', jqueryeach, times, [obig]);
-			//JMVC.test.describe('As you can see nativefor rules, jQuery sucks and JMVC sucks even more (but JMVC has true continue & break)');
-			JMVC.test.message('Avoid using any <i>each</i> function is the best choice you can take!');
+			
+			
+			JMVC.test.describe('Avoid using any <i>each</i> function is the best choice you can take!');
 
 		});
 	}
