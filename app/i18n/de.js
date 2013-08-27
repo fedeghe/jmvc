cbfa94cb192859a1adc939d883ff29e4740b15db
fedeghe <fedeghe@gmail.com>
2013-08-28 00:41:26 +0200

    translation update

--- app/i18n/de.js
+++ app/i18n/de.js
@@ -2,7 +2,7 @@ JMVC.i18n.de = {
 	'ciao' : 'Hallo',
 	'come stai' : 'Wie geht es dir',
 	"sei in locale" : "sind in lokalen",
-	'pure javascript mvc framework' : 'Reine JavaScript-MVC-Framework',
+	'pure javascript mvc framework' : 'Reine Javascript-MVC-Framework',
 	'more':'mehr',
 	'source':'quelle',
 	'desktop' : 'desktop',
