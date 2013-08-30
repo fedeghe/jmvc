4b5a02d16437ffdf6caed48630904b059693a3d9
fedeghe <fedeghe@gmail.com>
2013-08-30 23:37:33 +0200

    magic action experiment

--- app/controllers/jmvcdoc/show.js
+++ app/controllers/jmvcdoc/show.js
@@ -2,5 +2,11 @@ JMVC.controllers.show = function () {
 	this.action_index = function () {
 		JMVC.debug('yeah');
 	}
-	this.action = function (){console.debug(arguments)}
+
+
+	//check http://www.jmvc.org/jmvcdoc_show/what/name/Federico/surname/Ghedina?age=36
+	this.action = function (){
+		JMVC.debug('Checking magic action');
+		JMVC.debug(arguments);
+	}
 };
