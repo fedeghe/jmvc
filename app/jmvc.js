9cf49254dbe59553d170c36f379c713ae6cdc4e0
fedeghe <fedeghe@gmail.com>
2013-09-04 18:27:11 +0200

    manage port correctly in the baseurl

--- app/jmvc.js
+++ app/jmvc.js
@@ -60,9 +60,7 @@
      */
     var WD = W.document,
         WDL = WD.location,  // reference for current location
-        i,                  //
-        j,                  // some counters
-        l,                  //
+        i, j, l,                 // some counters
 
         /**
          * JMVC object, globalized
@@ -104,7 +102,7 @@
                      * two paths for
                      * > extensions, used as basepath by JMVC.require
                      * > test
-                     * > langs
+                     * > lang
                      */
                     PATH = {
                         /**
@@ -115,9 +113,7 @@
 
                         /**
                          * test suite path, every controller matching "test_foocontroller"
-                         * will automatically load the test suite and
-                         *  
-                         * foocontroller.js will be 
+                         * will automatically load the test suite and foocontroller.js will be 
                          * searched into the /app/controller/test directory
                          * to use test suite a require('test') is needed until TODO is done
                          * @type {string}
@@ -205,11 +201,6 @@
                      */
                     hooks = {},
 
-                    /**
-                     * a literal to store loaded lang files
-                     * @type {Object}
-                     */
-                    langs = {},
                     defaultlang = 'en',
                     currentlang = defaultlang,
 
@@ -217,9 +208,8 @@
                     // it's really next to the real value
                     time_begin = +new Date,
 
-                    //undefined string
+                    //undefined string for typeof
                     undef = 'undefined',
-                    //noop = function () {},
 
                     // getmode used in the require function
                     // ajax   : use xhr to get the source and evals
@@ -689,7 +679,6 @@
                         while (i < l) {
                             if (!JMVC.i18n[lng[i]]) {
                                 JMVC.i18n[lng[i]] = true;
-                                //langs[lng[i]] = true;
                             }
                             i += 1;
                         }
@@ -1685,7 +1674,8 @@
                             'proto' : WDL.protocol,
                             'host' : WDL.hostname,
                             'path' : WDL.pathname,
-                            'hash' : WDL.search
+                            'hash' : WDL.search,
+                            'port' : WDL.port ? ':' + WDL.port : ''
                         },
 
                         // adjust extensions
@@ -1748,7 +1738,8 @@
                         'controller_prepath' : controller_prepath,
                         'action' : action.replace(/\//g, ""),
                         'params' : params,
-                        'baseurl' : baseurl
+                        'baseurl' : baseurl,
+                        'port' : mid.port
                     };
                     //ret.controller = jmvc_normalize(ret.controller);
                     return ret;
@@ -1809,7 +1800,7 @@
                     views : {},
                     interfaces : {},
                     vars : {
-                        baseurl:    dispatched.baseurl,
+                        baseurl:    dispatched.baseurl + dispatched.port,
                         extensions : dispatched.baseurl + PATH.ext, //'/app/extensions',
                         devurl : DEV_URL,
                         produrl : PROD_URL,
@@ -1988,108 +1979,6 @@
 
 
 
-    JMVC.ajax = {
-        count : 0,
-        types : {
-            'xml' : 'text/xml',
-            'html' : 'text/html',
-            'json' : 'application/json'
-        },
-        getReq : function () {
-            var xhr,
-                IEfuckIds = ['Msxml2.XMLHTTP', 'Msxml3.XMLHTTP', 'Microsoft.XMLHTTP'],
-                i = 0,
-                len = IEfuckIds.length;
-            try {
-                xhr = new W.XMLHttpRequest();
-            } catch (e1) {
-                for (null; i < len; i += 1) {
-                    try {
-                        xhr = new ActiveXObject(IEfuckIds[i]);
-                    } catch (e2) {continue; }
-                }
-                !xhr && JMVC.debug('No way to initialize hxr');
-            }
-            JMVC.gc(IEfuckIds, i, len);
-            JMVC.ajax.count += 1;
-            return xhr;
-        },
-        call : function (options) {
-            var xhr = {
-                req : JMVC.ajax.getReq(),
-                uri : options && options.uri,
-                method : options && 'method' in options ? options.method : 'POST',
-                cback : options && 'cback' in options ? options.cback : function () {} ,
-                cb_opened : options && 'opened' in options ? options.opened : function () {},
-                cb_loading : options && 'loading' in options ? options.loading : function () {},
-                cb_error : options && 'error' in options ? options.error : function () {},
-                cb_abort : options && 'abort' in options ? options.abort : function () {},
-                sync : options && options.sync,
-                data : options && options.data || false,
-                type : (options && options.type) || JMVC.ajax.types.html,
-                cache : (options && options.cache !== undefined) ? options.cache : true,
-                targetType : options.type === 'xml' ?  'responseXML' : 'responseText',
-                timeout : options && options.timeout || 3000,
-                complete : false,
-                res : false,
-                ret : false,
-                state : false
-            };
-            xhr.req.onerror = function () {xhr.cb_error && xhr.cb_error.apply(null, arguments); };
-            xhr.req.onabort = function () {xhr.cb_abort && xhr.cb_abort.apply(null, arguments); };
-            xhr.req.onreadystatechange = function () {
-                if (xhr.req.readyState == 1) {
-                    switch (xhr.method) {
-                        case 'POST':
-                            try {
-                                xhr.req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                                xhr.req.send(xhr.data || true);
-                            } catch (e1) {}
-                        break;
-                        case 'GET':
-                            try {
-                                xhr.req.setRequestHeader("Accept", JMVC.ajax.types[xhr.type] + "; charset=utf-8");
-                                xhr.req.send(null);
-                            } catch (e2) {}
-                        break;
-                    }
-                } else if (xhr.req.readyState == 2) {
-                    console.debug('2');
-                } else if (xhr.req.readyState == 3) {
-                    console.debug('3');
-                } else if (xhr.req.readyState == 4) {
-
-                    if(xhr.req.status !== 200){
-                        xhr.req.onerror();
-                        xhr.complete = true;
-                        return;
-                    }
-
-                    xhr.complete = true;
-                    
-                    xhr.ret = xhr.type == 'json' ? new Function("return " + xhr.req[xhr.targetType])() : xhr.req[xhr.targetType];
-
-                    //cback
-                    xhr.cback && (function (r) {xhr.cback(r); })(xhr.ret);
-                    
-                    //IE leak ?????
-                    W.setTimeout(function () {
-                        JMVC.ajax.count -= 1;
-                        JMVC.purge(xhr.req);
-                    }, 50);
-
-                    return xhr.ret;
-                }
-            };
-            xhr.req.open(xhr.method, (xhr.method === 'GET') ? (xhr.uri + ((xhr.data) ? '?' + xhr.data : "")) : xhr.uri, xhr.sync);
-        },
-        post : function () {},
-        get : function () {},
-        getJson : function () {},
-        getXML : function () {}
-    };
-
-
 
     /**
     * now enhanche JMVC with some basic utility functions;
@@ -2208,12 +2097,7 @@
                 if (state === xhr.readyState) {return false; }
                 state = xhr.readyState;
 
-/*
-                if(xhr.readyState !== 4 && xhr.readyState !== 'complete' && xhr.status !== 200){
-                    //cb_error();
-                    complete = true;
-                }
- */
+
                 //JMVC.debug('called '+uri + ' ('+xhr.readyState+')');
                 if (xhr.readyState === "complete" || (xhr.readyState === 4 && xhr.status === 200)) {
                     complete = true;
@@ -2327,6 +2211,113 @@
         'getXML' : function (uri, cback) {
             return JMVC.io.ajcall(uri, {method : 'GET', sync : false, type : 'xml', cback : cback || function () {} });
         }
+    };
+
+
+    //==================================================
+    // 
+    // This is the experimental versionof ajax utilities
+    // 
+    // 
+    JMVC.ajax = {
+        count : 0,
+        types : {
+            'xml' : 'text/xml',
+            'html' : 'text/html',
+            'json' : 'application/json'
+        },
+        getReq : function () {
+            var xhr,
+                IEfuckIds = ['Msxml2.XMLHTTP', 'Msxml3.XMLHTTP', 'Microsoft.XMLHTTP'],
+                i = 0,
+                len = IEfuckIds.length;
+            try {
+                xhr = new W.XMLHttpRequest();
+            } catch (e1) {
+                for (null; i < len; i += 1) {
+                    try {
+                        xhr = new ActiveXObject(IEfuckIds[i]);
+                    } catch (e2) {continue; }
+                }
+                !xhr && JMVC.debug('No way to initialize hxr');
+            }
+            JMVC.gc(IEfuckIds, i, len);
+            JMVC.ajax.count += 1;
+            return xhr;
+        },
+        call : function (options) {
+            var xhr = {
+                req : JMVC.ajax.getReq(),
+                uri : options && options.uri,
+                method : options && 'method' in options ? options.method : 'POST',
+                cback : options && 'cback' in options ? options.cback : function () {} ,
+                cb_opened : options && 'opened' in options ? options.opened : function () {},
+                cb_loading : options && 'loading' in options ? options.loading : function () {},
+                cb_error : options && 'error' in options ? options.error : function () {},
+                cb_abort : options && 'abort' in options ? options.abort : function () {},
+                sync : options && options.sync,
+                data : options && options.data || false,
+                type : (options && options.type) || JMVC.ajax.types.html,
+                cache : (options && options.cache !== undefined) ? options.cache : true,
+                targetType : options.type === 'xml' ?  'responseXML' : 'responseText',
+                timeout : options && options.timeout || 3000,
+                complete : false,
+                res : false,
+                ret : false,
+                state : false
+            };
+            xhr.req.onerror = function () {xhr.cb_error && xhr.cb_error.apply(null, arguments); };
+            xhr.req.onabort = function () {xhr.cb_abort && xhr.cb_abort.apply(null, arguments); };
+            xhr.req.onreadystatechange = function () {
+                if (xhr.req.readyState == 1) {
+                    switch (xhr.method) {
+                        case 'POST':
+                            try {
+                                xhr.req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                                xhr.req.send(xhr.data || true);
+                            } catch (e1) {}
+                        break;
+                        case 'GET':
+                            try {
+                                xhr.req.setRequestHeader("Accept", JMVC.ajax.types[xhr.type] + "; charset=utf-8");
+                                xhr.req.send(null);
+                            } catch (e2) {}
+                        break;
+                    }
+                } else if (xhr.req.readyState == 2) {
+                    console.debug('2');
+                } else if (xhr.req.readyState == 3) {
+                    console.debug('3');
+                } else if (xhr.req.readyState == 4) {
+
+                    if(xhr.req.status !== 200){
+                        xhr.req.onerror();
+                        xhr.complete = true;
+                        return;
+                    }
+
+                    xhr.complete = true;
+                    
+                    xhr.ret = xhr.type == 'json' ? new Function("return " + xhr.req[xhr.targetType])() : xhr.req[xhr.targetType];
+
+                    //cback
+                    xhr.cback && (function (r) {xhr.cback(r); })(xhr.ret);
+                    
+                    //IE leak ?????
+                    W.setTimeout(function () {
+                        JMVC.ajax.count -= 1;
+                        JMVC.purge(xhr.req);
+                    }, 50);
+
+                    return xhr.ret;
+                }
+            };
+            xhr.req.open(xhr.method, (xhr.method === 'GET') ? (xhr.uri + ((xhr.data) ? '?' + xhr.data : "")) : xhr.uri, xhr.sync);
+        },
+        post : function () {},
+        get : function () {},
+        getJson : function () {},
+        getXML : function () {}
     };
 
     
@@ -2848,6 +2839,16 @@
             }
             return ret;
         },
+
+
+
+        'firstAncestor' : function(el, tagName) {
+            while(el = el.parentNode, el && (el.tagName != tagName.toUpperCase()));
+            return el;
+        },
+
+
+
 
         /**
          * [ description]
@@ -4132,27 +4133,19 @@
     //     _/    _/  _/        _/    _/_/  _/    _/  _/        _/    _/     
     //    _/    _/  _/_/_/_/  _/      _/  _/_/_/    _/_/_/_/  _/    _/ 
     //
-    (
-        /**
-         * [r description]
-         * @return {[type]} [description]
-         */
-        function () {
-            //
-            // before rendering, load requested extensions (must be set in the
-            // Modules var and the path/jmvc_ext.js file must be in the extensions folder)
+
             l = JMVC.modules.length;
-            if (l > 0) {
+            if (l) {
                 i = 0;
-                for (null; i < l; i += 1) {
-                    JMVC.require(JMVC.modules[i]);  
+                while (i < l) {
+                    JMVC.require(JMVC.modules[i]);
+                    i += 1;
                 }
             }
             JMVC.p.lang && JMVC.cookie.set('lang', JMVC.p.lang);
 
             JMVC.render();
-        }
-    )();
+
     
 }(this);
 
