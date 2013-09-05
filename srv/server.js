b8768e594ae9fc6cb8496386a84fe70d595e2498
fedeghe <fedeghe@gmail.com>
2013-09-05 12:16:13 +0200

    server updated, fixed config file pointer

--- srv/server.js
+++ srv/server.js
@@ -1,15 +1,19 @@
 var http = require('http'),
 	url = require('url'),
 	fs = require('fs'),
-	config = fs.existsSync('config.json') ? JSON.parse(fs.readFileSync('config.json')) : {},
+	configfile = __dirname + '/config.json',
+	config = fs.existsSync(configfile) ? JSON.parse(fs.readFileSync(configfile)) : {},
 	rx = new RegExp("^\/(" + config.free.join('|') + ")\/"),
 	port = 'port' in config ? config.port : 8080;
-
 http.createServer(function (request, response) {
-	var lookup = url.parse(decodeURI(request.url)).pathname,
- 		where = config.webroot + lookup.match(rx) ? lookup : '/' + config.accesspoint;
-	console.log(where);
- 	response.end(fs.readFileSync( where ));
+	try{
+		var lookup = url.parse(decodeURI(request.url)).pathname,
+			where = config.webroot + (lookup.match(rx) ? lookup : '/' + config.accesspoint);
+		response.end(fs.readFileSync( where ));
+	}catch(e){
+		console.log(e);
+		console.log('Server EXITED')
+		process.exit();
+	}
 }).listen(port);
-
 console.log('JMVC development server listening on port ' + port);
