0b077222523f4d740486894fc40f6c5a72bacb0e
fedeghe <fedeghe@gmail.com>
2013-10-08 09:18:27 +0200

    simple refactor

--- app/controllers/404.js
+++ app/controllers/404.js
@@ -4,20 +4,25 @@ JMVC.controllers['404'] = function () {
 	
 	this.action_msg = function () {		
 		
+		var sec2redir = 5;
+
 		JMVC.head.title('404 PAGE NOT FOUND');
 		JMVC.head.addstyle(JMVC.vars.baseurl + JMVC.US + 'media' + JMVC.US + 'css' + JMVC.US + 'core' + JMVC.US + 'jmvc.css');
-		JMVC.events.delay(function () {JMVC.head.goto(); }, 5000);
+		JMVC.events.delay(function () {JMVC.head.goto(); }, sec2redir * 1000);
 
 		var f0f = JMVC.getView('core/404'),
 			d = new Date(),
 			data = {
 				date :  d.getFullYear() + '/' + d.getMonth() + '/' + d.getDay() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds(),
 				url : JMVC.WD.referrer,
-				agent : JMVC.W.navigator.userAgent
-			};
+				agent : JMVC.W.navigator.userAgent,
+				secToRedir : sec2redir
+			},
+			cnt = this.get('cnt'),
+			act = this.get('act');
 
-		this.get('act') && (data.msg =  'action `' + this.get('act') + '` not found');
-		this.get('cnt') && (data.msg = 'controller `' + this.get('cnt') + '` not found');
+		act && (data.msg =  'action `' + this.get('act') + '` not found');
+		cnt && (data.msg = 'controller `' + this.get('cnt') + '` not found');
 
 		f0f.set(data).render();
 	};
