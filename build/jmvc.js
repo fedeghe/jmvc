79716836bf79cae9eec56bad6018790f125c6c0d
fedeghe <fedeghe@gmail.com>
2013-10-18 11:43:23 +0200

    foo changes in malta main tpl

--- build/jmvc.js
+++ build/jmvc.js
@@ -1,48 +1,48 @@
 $$src/head.js$$
 
 !function (W, undefined) {
-	'use strict';
-	var WD = W.document,	// reference for rdocument
-		WDL = WD.location,	// reference for current location
-		i, j, l,			// some counters
+    'use strict';
+    var WD = W.document,	// local reference for window.document
+        WDL = WD.location,	// local reference for current window.document.location
+        i, j, l,			// some counters 
 
-	// JMVC object, globalized
-	JMVC = W.JMVC = (
-		/*
-		* this function returns the JMVC object after doing some stuff
-		* @return {object literal} $JMVC inner object 
-		*/
-		function () {
-			'use strict';
-			$$src/core/init.js$$
-			$$src/core/innerjmvc.js$$
-			$$src/core/preload.js$$
-			$$src/core/errors.js$$
-			$$src/core/parser.js$$
-			$$src/core/event.js$$
-			$$src/core/promise.js$$
-			$$src/core/interface.js$$
-			$$src/core/controller.js$$
-			$$src/core/model.js$$
-			$$src/core/view.js$$
-			$$src/core/dispatched.js$$
-			$$src/core/outerjmvc.js$$
-			$$src/core/cleanup.js$$
-			return $JMVC;
-		}
-	)();
-	/** mandatory modules **/
-	$$src/modules/io.js$$
-	$$src/modules/util.js$$
-	$$src/modules/dom.js$$
-	$$src/modules/events.js$$
-	$$src/modules/head.js$$
-	$$src/modules/array.js$$
-	$$src/modules/string.js$$
-	$$src/modules/object.js$$
-	$$src/modules/match.js$$
-	$$src/modules/num.js$$
-	$$src/core/render.js$$
-	//
+    // JMVC object, globalized
+    JMVC = W.JMVC = (
+        /*
+        * this function returns the JMVC object after doing some stuff
+        * @return {object literal} $JMVC inner object 
+        */
+        function () {
+            'use strict';
+            $$src/core/init.js$$
+            $$src/core/innerjmvc.js$$
+            $$src/core/preload.js$$
+            $$src/core/errors.js$$
+            $$src/core/parser.js$$
+            $$src/core/event.js$$
+            $$src/core/extension.js$$
+            $$src/core/promise.js$$
+            $$src/core/interface.js$$
+            $$src/core/controller.js$$
+            $$src/core/model.js$$
+            $$src/core/view.js$$
+            $$src/core/dispatched.js$$
+            $$src/core/outerjmvc.js$$
+            $$src/core/cleanup.js$$
+            return $JMVC;
+        }
+    )();
+    /** mandatory modules **/
+    $$src/modules/io.js$$
+    $$src/modules/util.js$$
+    $$src/modules/dom.js$$
+    $$src/modules/events.js$$
+    $$src/modules/head.js$$
+    $$src/modules/array.js$$
+    $$src/modules/string.js$$
+    $$src/modules/object.js$$
+    $$src/modules/match.js$$
+    $$src/modules/num.js$$
+    $$src/core/render.js$$
+    //
 }(this);
-
