3362429a9b98e52baf5b5e98b43dbcc509060fb2
fedeghe <fedeghe@gmail.com>
2013-10-18 11:58:07 +0200

    updated…still need for new set,del,get

---
+++ app/extensions/core/mediaquery/mediaquery.js
@@ -0,0 +1,7 @@
+JMVC.extend('mediaquery', {
+	init : function () {
+		"use strict";
+		JMVC.set('mq_dsktp_lptp',"body, h1 {font-size:100px !important}");
+		JMVC.head.addstyle(JMVC.vars.extensions + 'core/mediaquery/mediaquery.css', true);
+	}
+});
