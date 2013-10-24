69c45a006d1b5cc95d0a78c0f8d7d7ae543a7a85
fedeghe <fedeghe@gmail.com>
2013-10-25 00:25:15 +0200

    JMVC.Interface sample

--- app/controllers/test/interfaces.js
+++ app/controllers/test/interfaces.js
@@ -1,23 +1,31 @@
-JMVC.controllers.interfaces = function() {
+JMVC.controllers.interfaces = function () {
 
-	this.action_index = function(){
+	this.action_index = function () {
+
 		JMVC.loadInterfaces('loader');
-		var t1 = function () {
-			this.load = function (x) {console.debug(x)};
-			this.unload = function (x) {console.debug(x)};
-		},
-		t2 = function () {
-			this.check = function (x) {console.debug(x)};
-			this.init = function (x) {console.debug(x)};
-		},
-		t3 = function () {
-			this.check = function (x) {console.debug(x)};
-			this.init = function (x) {console.debug(x)};
-			this.parse = function (x) {console.debug(x)};
-			this.inwit = function (x) {console.debug(x)};
-		};
-		console.debug(JMVC.interfaces.loader.check(t1));
-		console.debug(JMVC.interfaces.grep.check(t2))
-		console.debug(JMVC.interfaces.grep.check(t3));
+
+		var a = function () {console.debug(arguments)},
+			//
+			// three foo constructors
+ 			T1 = function () {
+				this.load = this.unload = a;
+			},
+			T2 = function () {
+				this.check = this.init = a;
+			},
+			T3 = function () {
+				this.check = 
+				this.init = 
+				this.parse = 
+				this.inwit = 
+				this.love = 
+				this.hate = 
+				this.destroy = a;
+			};
+
+		console.debug(JMVC.Interface.checkImplements(new T1, JMVC.interfaces.loader));
+		console.debug(JMVC.Interface.checkImplements(new T3, JMVC.interfaces.grep, JMVC.interfaces.mankind));
+		// this would fail
+		// console.debug(JMVC.Interface.checkImplements(new T2, JMVC.interfaces.loader))
 	};
 };
