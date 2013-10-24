d30cb6f308a37c85ff6b6d66424b6c7589ae04d5
fedeghe <fedeghe@gmail.com>
2013-10-25 00:29:35 +0200

    rewritten JMVC.Interface constructor

--- build/src/core/interface.js
+++ build/src/core/interface.js
@@ -1,48 +1,44 @@
-/*
----------
-INTERFACE
----------
-*/
-Interface = function (a) {
-    this.mthds = a;
+
+
+/**
+ * INTERFACE
+ * =========
+ * [Interface description]
+ * @param {[type]} name [description]
+ * @param {[type]} a    [description]
+ */
+Interface = function (name, a) {
+    this.name = name;
+    this.mthds = [];
+    if (!(a instanceof Array)){
+        throw new Error('An array of strings must be passed to the Interface constructor');
+    }
+    for (var i = 0, l = a.length; i < l; i++) {
+        (typeof a[i] === 'string') && this.mthds.push(a[i]);
+    }
 };
 
 /**
- * [prototype description]
- * @type {Object}
+ * [checkImplements description]
+ * @param  {[type]} obj    [description]
+ * @param  {[type]} intrfc [description]
+ * @return {[type]}        [description]
  */
-Interface.prototype = {
-    /**
-     * [addMethod description]
-     * @param {[type]} mthd [description]
-     */
-    addMethod : function (mthd) {
-        this.mthds[mthd.name] || (this.mthds[mthd.name] = mthd);
-    },
-    /**
-     * [removeMethod description]
-     * @param  {[type]} mthd [description]
-     * @return {[type]}      [description]
-     */
-    removeMethod : function (mthd) {
-        this.mthds[mthd] && (this.mthds[mthd] = null);
-    },
-    /**
-     * [check description]
-     * @param  {[type]} o [description]
-     * @return {[type]}   [description]
-     */
-    check : function (o) {
-        var m,
-            i = 0,
-            l = this.mthds.length,
-            obj = new o();
-        for (m in this.mthds) {
-            if (typeof obj[this.mthds[m]] !== 'function') {
-                return false;
+Interface.checkImplements = function (obj) {
+    var m,
+        i = 0,
+        arg = Array.prototype.slice.call(arguments);
+        l = arg.length;
+
+    while (++i < l){
+        for (m in arg[i].mthds) {
+            if (typeof obj[arg[i].mthds[m]] !== 'function') {
+                throw new Error("Function Interface.checkImplements: object "
+                  + "does not implement the " + arg[i].name
+                  + " interface. Method " + arg[i].mthds[m] + " was not found.");
             }
         }
-        obj = null;
-        return true;
     }
-};
+
+    return obj;
+};  
