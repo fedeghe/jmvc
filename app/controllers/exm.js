375f4c608e8c762d6df3af1dfdf0be9fc2260a2c
fedeghe <fedeghe@gmail.com>
2013-10-25 11:45:35 +0200

    some cleanup

--- app/controllers/exm.js
+++ app/controllers/exm.js
@@ -3,7 +3,7 @@ JMVC.controllers.exm = function () {
 	this.action_index = function () {
 		JMVC.events.loadify(1000);
 		JMVC.require('plotter', 'core/obj/bucket');
-		var M = Math,
+		var //M = Math,
 			that = this,
 			v = JMVC.getView('vacuum');
 		
@@ -15,11 +15,18 @@ JMVC.controllers.exm = function () {
 		
 		JMVC.head.addstyle(JMVC.vars.baseurl + '/media/css/logoexm.css', true, false);
 		
-		v.render(function(){
-			var newlogo = document.getElementById('extralogo');
-			var e = new JMVC.plotter.letter('e', 8, 0),
-				x = new JMVC.plotter.letter('x',8,71),
-				m = new JMVC.plotter.letter('m',22, 260);
+		v.render(function () {
+			var newlogo = document.getElementById('extralogo'),
+				e = new JMVC.plotter.letter('e', 8, 0),
+				x = new JMVC.plotter.letter('x', 8, 71),
+				m = new JMVC.plotter.letter('m', 22, 260),
+				a = newlogo.childNodes,
+				i = 1,
+				t,
+				T1 = 20,
+				T2 = 10,
+				bucket = new JMVC.bucket.create(JMVC.util.range(0, a.length - 1));
+				
 			e.line(0, 14, 0, 65, 10);
 			e.line(10, 14, 10, 65, 10);
 			e.line(59, 14, 59, 65, 10);
@@ -29,43 +36,40 @@ JMVC.controllers.exm = function () {
 			x.line(0,0,35,27,7);
 			x.line(35,27,69,0,7);
 			x.plot(newlogo);
-			var a = newlogo.childNodes;
-			var i = 1;
+
 			
-			var T1=20, T2 =10;
 			
-			var bucket = new JMVC.bucket.create(JMVC.util.range(0,a.length-1));
-			
-			var t = window.setInterval(function(){
+			t = window.setInterval(function () {
+				var trg,
+					c;
 				if (that.get('flash')) {
-					var trg = i;
+					trg = i;
 				} else {
 					if (!bucket.hasMore()) {bucket.recover(); }
 					trg =  bucket.next() || 1;/*i; JMVC.util.rand(1,a.length-1); */
 				}
 				
 				
-				try{var c = a[trg].style.color;}catch(e){JMVC.debug(trg);}
+				try {c = a[trg].style.color; } catch (e) {JMVC.debug(trg); }
 
 				window.setTimeout(
-					function(t1){
+					function (t1) {
 						a[t1].style.color = 'white';
 						a[t1].style.fontSize = '8px';
 						window.setTimeout(
-							function(t2){a[t2].style.color = c;},T1, t1
+							function (t2) {a[t2].style.color = c; }, T1, t1
 						);
-					},0, trg
+					}, 0, trg
 				);
 				if (that.get('flash')) {
-					i = (i + 1)%(a.length);
-					i = i ? i : 1;
+					i = (i + 1) % a.length;
+					i = i || 1;
 				}
 
-			},T2);
+			}, T2);
 			
 		});
 	};
-	
-}
+};
 
 
