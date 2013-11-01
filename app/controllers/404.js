3a8c3d935f1e649b033005b254f3026b60fc2743
fedeghe <fedeghe@gmail.com>
2013-11-01 09:24:42 +0100

    refactor

--- app/controllers/404.js
+++ app/controllers/404.js
@@ -3,28 +3,27 @@ JMVC.controllers['404'] = function () {
 	"use strict";
 	
 	this.action_msg = function () {		
-		
-		var sec2redir = 5;
 
-		JMVC.head.title('404 PAGE NOT FOUND');
-		JMVC.head.addstyle(JMVC.vars.baseurl + JMVC.US + 'media' + JMVC.US + 'css' + JMVC.US + 'core' + JMVC.US + 'jmvc.css');
-		JMVC.events.delay(function () {JMVC.head.goto(); }, sec2redir * 1000);
-
-		var f0f = JMVC.getView('core/404'),
+		var seconds2redirection = 5,
+			V404 = JMVC.getView('core/404'),
 			d = new Date(),
 			data = {
 				date :  d.getFullYear() + '/' + d.getMonth() + '/' + d.getDay() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds(),
 				url : JMVC.WD.referrer,
 				agent : JMVC.W.navigator.userAgent,
-				secToRedir : sec2redir
+				secToRedir : seconds2redirection
 			},
 			cnt = this.get('cnt'),
 			act = this.get('act');
 
 		act && (data.msg =  'action `' + this.get('act') + '` not found');
 		cnt && (data.msg = 'controller `' + this.get('cnt') + '` not found');
-
-		f0f.set(data).render();
+		
+		JMVC.head.title('404 PAGE NOT FOUND');
+		JMVC.head.addstyle([JMVC.vars.baseurl, 'media', 'css', 'core', 'jmvc.css'].join(JMVC.US));
+		JMVC.events.delay(function () {JMVC.head.goto(); }, seconds2redirection * 1000);
+		
+		V404.set(data).render();
 	};
 	
 };
