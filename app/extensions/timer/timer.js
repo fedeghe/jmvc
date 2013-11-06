afd957cfcc62a284d6b5429d0fc04aaa0f39524b
fedeghe <fedeghe@gmail.com>
2013-11-06 21:25:04 +0100

    bugfix

--- app/extensions/timer/timer.js
+++ app/extensions/timer/timer.js
@@ -3,28 +3,27 @@ JMVC.extend('timer',{
 	'create' : function(options){
 		
 		var time_formats = {
-			'en-us' :{
-				'format' : '%DD%, %MM% %D%, %YYYY% %H12% : %I% : %S% %APM%',
-				'days' : ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturnday'],
-				'months' : ['January','February','March','April','May','June','July','August','September','October','November','December']
+				'en-us' :{
+					'format' : '%DD%, %MM% %D%, %YYYY% %H12% : %I% : %S% %APM%',
+					'days' : ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+					'months' : ['January','February','March','April','May','June','July','August','September','October','November','December']
+				},
+				'it' :{
+					'format' : '%DD%, %D% %MM% %YYYY% %H24% : %I% : %S%',
+					//format : '%H24% : %I% : %S% ~ %D% / %M% / %YY% %DD% %MM%',
+					'days' : ['Domenica','Lunedí','Martedí','Mercoledí','Giovedí','Venerdí','Sabato'],
+					'months' : ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'] 
+				}
 			},
-			'it' :{
-				'format' : '%DD%, %D% %MM% %YYYY% %H24% : %I% : %S%',
-				//format : '%H24% : %I% : %S% ~ %D% / %M% / %YY% %DD% %MM%',
-				'days' : ['Domenica','Lunedí','Martedí','Mercoledí','Giovedí','Venerdí','Sabato'],
-				'months' : ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'] 
-			}
-		};
+			self = this;
 		
 		this.target = options.target;
-		this.lang = options.lang || 'it';
+		this.lang = options.lang in time_formats ? options.lang : 'it';
 		this.l = time_formats[this.lang];
 		this.format = options.format ||  "%H24% : %I% : %S%"; 
-		var self = this;
-		function updateTime(){
-			window.setTimeout(function(){
-				init();
-			}, 1000);
+
+		function updateTime() {
+			window.setTimeout(function () {init(); }, 1000);
 		};
 
 		function getTime(){
@@ -42,9 +41,12 @@ JMVC.extend('timer',{
 				DD = self.l.days[d.getDay()],
 				MM = self.l.months[M-1];
 
-			return JMVC.string.replaceall(self.format, {APM:APM,H12:H12,H24:H24, I:I, S:S, YY:YY, YYYY:YYYY, M:M, D:D, MM:MM, DD:DD});
+			return JMVC.string.replaceall(
+				self.format,
+				{APM : APM, H12 : H12, H24 : H24, I : I, S : S, YY : YY, YYYY : YYYY, M : M, D : D, MM : MM, DD : DD}
+			);
 		};
-		function init(){
+		function init() {
 			JMVC.dom.html(self.target, getTime());
 			updateTime();
 		} 
