097cdcbb3ca206ce44a6817700e190156ede8b1c
fedeghe <fedeghe@gmail.com>
2013-11-28 10:16:40 +0100

    refactored to use the create factory method

--- app/extensions/core/obj/bucket/bucket.js
+++ app/extensions/core/obj/bucket/bucket.js
@@ -1,42 +1,61 @@
 JMVC.extend('bucket', {
 	create : function (arr) {
 		"use strict";
-		var that = this;
-		arr = arr || [];
-
-		this.arr = JMVC.array.clone(arr);
-		this.r = false,
-		this.l = arr.length,
-		this.fix = JMVC.array.clone(this.arr);
-
-		return {
+		function bucket(a) {
+			a = a || [];
+			this.arr = JMVC.array.clone(a);
+			this.r = false,
+			this.l = a.length,
+			this.fix = JMVC.array.clone(this.arr);
+		}
+		bucket.prototype = {
+			empty : function () {
+				this.arr = [];
+				this.r = false;
+				this.l = 0;
+			},
+			fill : function (arr) {
+				this.arr = JMVC.array.clone(arr);
+				this.r = false;
+				this.l = arr.length;
+				this.fix = JMVC.array.clone(this.arr);
+			},
+			hasMore : function () {return !!this.l; },
+			merge : function (b) {
+				this.arr = [].concat.call(this.arr, b.arr);
+				this.l = this.arr.length;
+			},
 			next : function () {
-				if (that.l == 0) {return null; } 
-				that.r = that.arr.splice(JMVC.util.rand(0, that.l - 1), 1);
-				that.l -= 1;
-				return that.r.pop();
+				if (this.l == 0) {return null; } 
+				this.r = this.arr.splice(JMVC.util.rand(0, this.l - 1), 1);
+				this.l -= 1;
+				return this.r.pop();
 			},
 			recover : function () {
-				that.l = that.fix.length;
-				that.arr = JMVC.array.clone(that.fix);
+				this.l = this.fix.length;
+				this.arr = JMVC.array.clone(this.fix);
 			},
-			size : function () {return that.l; },
-			fill : function (arr) {
-				that.arr = JMVC.array.clone(arr);
-				that.r = false;
-				that.l = arr.length;
-				that.fix = JMVC.array.clone(that.arr);
+			show : function () {
+				return this.arr.toString();
 			},
-			empty : function () {
-				that.arr = [];
-				that.r = false;
-				that.l = 0;
-			},
-			hasMore : function () {return !!that.l; },
 			shuffle : function (t) {
 				if (t) while (t--) this.shuffle();
-				Array.prototype.sort.call(that.arr, function () {return Math.random() > .5; });
+				this.arr.sort(function (a, b) {return .5 - Math.random(); });
+			},
+			size : function () {return this.l; },
+			unfill : function (n) {
+				this.arr = this.arr.slice(0, n < this.l ? this.l - n : 0);
+				this.l = this.arr.length;
+			},
+			walk : function (f) {
+				var that = this;
+				for (var i = 0, l = this.l; i < l; i += 1) {
+					~function (j){
+						that.arr[j] = f(that.arr[j]);
+					}(i);
+				}
 			}
 		};
+		return new bucket(arr);
 	}
 });
