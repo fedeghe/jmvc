57129cf780860d3778313abb13875ff221233cb0
fedeghe <fedeghe@gmail.com>
2013-11-28 10:13:38 +0100

    dummy refactor

--- app/extensions/affix/affix.js
+++ app/extensions/affix/affix.js
@@ -3,58 +3,51 @@ JMVC.extend('affix', {
 	init : function () {
 		JMVC.require('core/dim', 'core/css');
 
-		JMVC.affix.vars = {
-			index : 0,
-			pool : {}
-		};
+		JMVC.affix.vars = {index : 0, pool : {}};
 		
 		JMVC.events.end(function () {
 
 			function react() {
-				var j, top, doit,
+				var j, style,
 					c = JMVC.dim.getScreenData(),
 					t = c.scrollTop;
 
 				for (j in JMVC.affix.vars.pool) {
 
-					doit = (t > JMVC.affix.vars.pool[j].top - JMVC.affix.vars.pool[j].mintop);
-					top = doit ? 
-						t + JMVC.affix.vars.pool[j].mintop
+					style = (t > JMVC.affix.vars.pool[j].top - JMVC.affix.vars.pool[j].mintop) ?
+						{position : 'fixed', top : JMVC.affix.vars.pool[j].mintop + 'px'}
 						:
-						JMVC.affix.vars.pool[j].top;
+						{position : 'absolute', top : JMVC.affix.vars.pool[j].top + 'px'};
 
-					JMVC.css.style(JMVC.affix.vars.pool[j].node, 'top', top + 'px');
-
-					JMVC.css.style(
-						JMVC.affix.vars.pool[j].node,
-						doit ? {'position':'fixed', 'top' : JMVC.affix.vars.pool[j].mintop + 'px'} : {'position':'absolute'}
-					);
+					JMVC.css.style(JMVC.affix.vars.pool[j].node, style);
 				}
 			}
 
 			JMVC.events.bind(JMVC.W, 'scroll', react);
+			
 			JMVC.events.ready(react);
 		});
 	},
 	
-	add : function (where) {
-		var tmp = new function () {
-			var a = arguments[0];
-			this.html = a.html ||'no content';
-			this.top = a.init || 100;
-			this.mintop = a.min || 10;
-			this.style = a.style || '';
-			this['class'] = a['class'] || '';
+	add : function (options) {
+
+		var tmp = new function (opt) {
+			this.html = opt.html ||'no content';
+			this.top = opt.init || 100;
+			this.mintop = opt.min || 10;
+			this.style = opt.style || '';
+			this['class'] = opt['class'] || '';
 			this.node = JMVC.dom.create('div', {
-					'style' : 'position:absolute;top:' + this.top + 'px;' + this.style,
+					//'style' : 'position:absolute;top:' + this.top + 'px;' + this.style,
+					'style' : this.style,
 					'class' : this.class
 				},
 				this.html
 			);
-		}(arguments[0]);
+		}(options);
 			
 		JMVC.affix.vars.index += 1;
 		JMVC.affix.vars.pool[JMVC.affix.vars.index] = tmp;
-		JMVC.dom.append(JMVC.dom.find(arguments[0].where) || JMVC.dom.body(), tmp.node);
+		JMVC.dom.append(JMVC.dom.find(options.where) || JMVC.dom.body(), tmp.node);
 	}
 });
