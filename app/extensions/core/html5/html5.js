f243539470053c417ae5a66c531c1c9aa3c0cb5e
fedeghe <fedeghe@gmail.com>
2013-12-04 01:06:29 +0100

    first attempt for an audio html5 façade

--- app/extensions/core/html5/html5.js
+++ app/extensions/core/html5/html5.js
@@ -1,8 +1,8 @@
 JMVC.extend('html5', {
 	init : function () {
-		this._ = {
+		JMVC.html5._ = {
 			mimemap : {
-				ogv : 'ogg' 
+				//ogv : 'ogg' 
 			}
 		}
 	},
@@ -32,20 +32,54 @@ JMVC.extend('html5', {
 		//append inner source tag
 		JMVC.dom.add(video, 'source', attrs.source);
 
-		JMVC.dom.attr(video, attrs.video);		
 		return video;
 	},
 
+	audio : function (options) {
+		/*
+		
+		<audio src="http://developer.mozilla.org/@api/deki/files/2926/=AudioTest_(1).ogg" autoplay>
+			Your browser does not support the <code>audio</code> element.
+		</audio>
+		
+		<audio controls="controls">
+			Your browser does not support the <code>audio</code> element.
+			<source src="foo.wav" type="audio/wav">
+		</audio>
+		*/
+		if (!('src' in options)) throw new Error('No src passed for video');
 
-	/*video : function (o) {
-		o.mime = o.mime || o.uri.split('.').pop();
-		o.mime = JMVC.html5.mimemap[o.mime] || o.mime;
-		o.controls = o.controls ? ' controls="controls"' :'';
-		o.autoplay = o.autoplay ? ' autoplay="autoplay"' :'';
-		var tpl = '<video width="%w%" height="%h%"%controls%%autoplay%><source src="%uri%" type="video/%mime%" /></video>';
-		return JMVC.string.replaceall(tpl, o);
-	},*/
-	audio : function () {},
+		var audio,
+			attrs = {
+				audio : {},
+				source : {}
+			};
+		//'type' in options && (attrs.source.type = 'video/' + (JMVC.html5._.mimemap[options.type] || options.type));
+
+		attrs.source.type = 'audio/' + ('type' in options ? (JMVC.html5._.mimemap[options.type] || options.type) : 'ogg');
+
+		attrs.audio.controls = options.controls ? 'controls' :'';
+		attrs.audio.autoplay = options.autoplay ? 'autoplay' :'';
+
+		var add_source = true;
+		switch (options.type) {
+			case 'ogg':
+			case 'ogv':
+				attrs.audio.src = options.src;
+				add_source = false;
+			break;
+			case 'wav':
+				attrs.source.src = options.src;
+			break;
+		}
+
+		audio = JMVC.dom.create('audio', attrs.audio);
+
+		//append inner source tag
+		add_source && JMVC.dom.add(audio, 'source', attrs.source);
+				
+		return audio;		
+	},
 	progress : function(progress, attr){
 		attr = attr || {};
 		if (!('max' in attr)){attr.max = 100;}
