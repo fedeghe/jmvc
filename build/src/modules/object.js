69875800ffbadfbe14c9c2742bec06e87026c56b
fedeghe <fedeghe@gmail.com>
2013-12-05 00:16:45 +0100

    all functions obj2XXX renamed and all now using a strategy on private _.object.walkout

--- build/src/modules/object.js
+++ build/src/modules/object.js
@@ -2,12 +2,33 @@
 OBJECT sub-module
 ---------------*/
 
-// private section
+/**
+ * [object description]
+ * @type {Object}
+ */
 _.object = {
-    
+    /**
+     * [walkout description]
+     * @param  {[type]}   o  [description]
+     * @param  {Function} fn [description]
+     * @return {[type]}      [description]
+     */
+    walkout : function (o, fn) {
+        "use strict";
+        var ret = '', j;
+        for (j in o) {
+            if (o.hasOwnProperty(j)) {
+                ret += fn(o, j, ret);
+            }
+        }
+        return ret;
+    }
 };
 
-// public section
+/**
+ * [object description]
+ * @type {Object}
+ */
 JMVC.object = {
     /**
      * Clones an object
@@ -16,6 +37,7 @@ JMVC.object = {
      * @returns cloned Object
      */
     clone : function (obj) {
+        "use strict";
         var temp,
             key;
         if (obj === null || typeof obj !== 'object') {
@@ -39,7 +61,7 @@ JMVC.object = {
      * @param  {[type]} i    [description]
      * @return {[type]}      [description]
      */
-    objCompare : function (obj1, obj2, ret, i) {
+    compare : function (obj1, obj2, ret, i) {
         "use strict";
         (ret == undefined) && (ret = true);
         if (!ret) {return 0; }
@@ -47,25 +69,14 @@ JMVC.object = {
             return false;
         }
         for (i in obj1) {
-            ret = ret && obj2[i] && this.objCompare(obj1[i], obj2[i], ret);
+            ret = ret && obj2[i] && this.compare(obj1[i], obj2[i], ret);
             if (!ret) {return false; }
         }
         for (i in obj2) {
-            ret = ret && obj1[i] && this.objCompare(obj2[i], obj1[i], ret);
+            ret = ret && obj1[i] && this.compare(obj2[i], obj1[i], ret);
             if (!ret) {return false; }
         }
         return ret;
-    },
-
-    /**
-     * [ description]
-     * @param  {[type]} obj1 [description]
-     * @param  {[type]} obj2 [description]
-     * @return {[type]}      [description]
-     */
-    objJCompare : function (obj1, obj2) {
-        "use strict";
-        return JSON.stringify(obj1) === JSON.stringify(obj2);
     },
 
     /**
@@ -74,8 +85,37 @@ JMVC.object = {
      * @param  {[type]} field) {return      (typeof obj === 'object' && obj[field] [description]
      * @return {[type]}        [description]
      */
-    inObject : function (obj, field) {
-        return (typeof obj === 'object' && obj[field]);
+    contains : function (obj, field) {
+        "use strict";
+        return (typeof obj === 'object' && filed in obj);
+    },
+
+    /**
+     * [ description]
+     * @param  {[type]} obj [description]
+     * @param  {[type]} ext [description]
+     * @return {[type]}     [description]
+     */
+    extend : function (o, ext) {
+        "use strict";
+        var obj = this.clone(o), j;
+        for (j in ext) {
+            if (ext.hasOwnProperty(j) && !(j in obj)) {
+                obj[j] = ext[j];
+            }
+        }
+        return obj;
+    },
+
+    /**
+     * [ description]
+     * @param  {[type]} obj1 [description]
+     * @param  {[type]} obj2 [description]
+     * @return {[type]}      [description]
+     */
+    jCompare : function (obj1, obj2) {
+        "use strict";
+        return JSON.stringify(obj1) === JSON.stringify(obj2);
     },
 
     /**
@@ -83,14 +123,11 @@ JMVC.object = {
      * @param  {[type]} o [description]
      * @return {[type]}   [description]
      */
-    obj2attr : function (o) {
-        var ret = '', i;
-        for (i in o) {
-            if (o.hasOwnProperty(i)) {
-                ret += ' ' + i + (o[i] ? '="' + o[i] + '"' : '');
-            }
-        }
-        return ret;
+    toAttr : function (obj) {
+        "use strict";
+        return _.object.walkout(obj, function (o, i) {
+            return ' ' + i + (o[i] ? '="' + o[i] + '"' : '');
+        });
     },
 
     /**
@@ -98,14 +135,15 @@ JMVC.object = {
      * @param  {[type]} o [description]
      * @return {[type]}   [description]
      */
-    obj2css : function (o) {
-        var ret = '', i, j;
-        for (i in o) {
-            if (o.hasOwnProperty(i)) {
-                ret += i + ' {' + JMVC.object.obj2str(o[i]) + '} ';
-            }
-        }
-        return ret;
+    toCss : function (obj) {
+        "use strict";
+        return _.object.walkout(obj, function (ob, i) {
+            return i + ' {' + _.object.walkout(ob[i], 
+                function (o, j){
+                    return j + ':' + o[j] + ';';
+                }
+            ) + '} ';
+        });
     },
 
     /**
@@ -113,24 +151,23 @@ JMVC.object = {
      * @param  {[type]} o [description]
      * @return {[type]}   [description]
      */
-    obj2qs : function (o) {
-        var ret = '', i;
-        for (i in o) {
-            if (o.hasOwnProperty(i)) {
-                ret += String((ret ? '&' : '?') + i + '=' + encodeURIComponent(o[i]));
-            }
-        }
-        return ret;
+    toQs : function (obj) {
+        "use strict";
+        return _.object.walkout(obj, function (o, i, r) {
+            return (r ? '&' : '?') + encodeURIComponent(i) + '=' + encodeURIComponent(o[i]);
+        });
     },
 
-    obj2str : function (o) {
-        var ret = '';
-        for (j in o) {
-            if (o.hasOwnProperty(j)) {
-                ret += j + ':' + o[j] + ';';
-            }
-        }
-        return ret;
+    /**
+     * [obj2str description]
+     * @param  {[type]} obj [description]
+     * @return {[type]}     [description]
+     */
+    toStr : function (obj) {
+        "use strict";
+        return _.object.walkout(obj, function (o, i) {
+            return i + ':' + o[i] + ';';
+        });
     }
 
 };
