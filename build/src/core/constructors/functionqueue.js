2a184f99e6719ac992af997fe343ee65dc84f551
fedeghe <fedeghe@gmail.com>
2013-12-16 12:30:12 +0100

    JMVC.FunctionQueue constructor added

---
+++ build/src/core/constructors/functionqueue.js
@@ -0,0 +1,27 @@
+/*---
+MONAD
+
+ok, that's not a true monad, but it's similar
+---*/
+FunctionQueue = function () {
+    this.queue = [];
+};
+FunctionQueue.prototype.add = function (f) {
+    this.queue.push(f)
+};
+FunctionQueue.prototype.run = function() {
+    var i = 0,
+        l = this.queue.length,
+        args = [].slice.call(arguments, 0),
+        f;
+
+    while (i < l){
+        f = this.queue[i++];
+        (args instanceof Array) || (args = [args]);
+        args = f.apply(null, args);
+    }
+    return args;    
+};
+FunctionQueue.prototype.reset = function () {
+    this.queue.length = 0;
+}
