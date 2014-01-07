504b6ad3d4fa29d29e2d460503d2a7da35142637
fedeghe <fedeghe@gmail.com>
2014-01-07 01:43:00 +0100

    canvas editor two modes

--- app/controllers/canvaseditor.js
+++ app/controllers/canvaseditor.js
@@ -1,50 +1,49 @@
-JMVC.controllers.canvaseditor = function (){
+JMVC.controllers.canvaseditor = function () {
+    
+    this.action_index = function (p) {
 
-    JMVC.require(
-        'canvas/editor/editor',
-        'core/screen/screen',
-        'core/lib/image/image'
-    );
-
-    this.action_index = function () {
-        
-        var screen_size,
+        var editor = p['old'] ? 'canvas/editor/editor' : 'canvas/editorNEW/editor',
+            screen_size,
             editorContainerId = 'canvasEditorContainerId';
 
+        JMVC.require(
+            editor,
+            'core/screen/screen',
+            'core/lib/image/image'
+        );
+
         JMVC.events.loadify(1000);
-
         JMVC.head.title('Canvas pure editor');
-
-        //a minimal wrapper view
+        //
+        // a minimal wrapper view
         JMVC.getView('vacuum')
-        
-            //set some style and the id
+            //
+            // set some style and the id
             .set({
-                style : 'font-family:verdana;margin:0 auto;border:0px;padding:0px',
+                style : JMVC.object.toStr({
+                    'font-family' : 'verdana',
+                    margin : '0 auto',
+                    border : '0px',
+                    padding : '0px',
+                    position : 'relative',
+                    overflow : 'hidden'
+                }),
                 id : editorContainerId
             })
-
+            //
             // render
             .render(function () {
-                //get  viewport size
+                //
+                // now it`s time to get viewport size
                 screen_size = JMVC.screen.getViewportSize();
-                
-                //start the editor
-                var editor =  new JMVC.canvas.Editor({
-
-                    //the container
+                //
+                // create initialize and start the editor
+                new JMVC.canvas.Editor({
                     node : JMVC.dom.find('#' + editorContainerId),
-
-                    //sizes
                     width : screen_size[0],
-                    height : screen_size[1]-3,
-
-                    // require.json based multirequire
-                    tools : ['canvas/editor/tools/']
-                });
-                editor.init().render();
+                    height : screen_size[1]-1
+                }).init().render();
                 
             });
     };
-
 };
