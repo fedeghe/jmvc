50ec62c6d968fd0ae3637a1f277d749c998b13eb
fedeghe <fedeghe@gmail.com>
2014-01-14 12:27:20 +0100

    I`m late You know

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -169,7 +169,7 @@ JMVC.controllers.api = function () {
 
 			JMVC.github.forkme('fedeghe');
 			
-			JMVC.widget.countdown.start('#countdown', new Date(2013, 11, 30));
+			JMVC.widget.countdown.start('#countdown', new Date(2014, 1, 30));
 			
 			JMVC.events.delay(function () {
 				for (var j = 0, l = sections.length;  j < l; j++) {
