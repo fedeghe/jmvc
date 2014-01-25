58128ebaefffe41aee2c624a67b2db0761847235
fedeghe <fedeghe@gmail.com>
2014-01-25 17:01:23 +0100

    trial interface

---
+++ app/interfaces/bee.interface.js
@@ -0,0 +1,5 @@
+JMVC.interfaces.bee = new JMVC.Interface('bee', [
+    function tee(func, obj, str) {return 'obj';},
+    function rule(func, obj, str) {return 'obj';},
+    function run(func, obj, str) {return 'obj';}
+]);
