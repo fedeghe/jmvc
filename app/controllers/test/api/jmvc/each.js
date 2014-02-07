4dcc497d4406e8bad0b9d7088acbe797bb8dfc85
fedeghe <fedeghe@gmail.com>
2014-02-08 00:29:10 +0100

    JMVC.each test modified

--- app/controllers/test/api/jmvc/each.js
+++ app/controllers/test/api/jmvc/each.js
@@ -84,24 +84,26 @@ JMVC.controllers.each = function() {
 			JMVC.test.testValue("loop(o);", function(){return loop(o); }, '(name:federico)(surname:ghedina)');
 			JMVC.test.testValue("destroy(a);", function(){return destroy(a).toString(); }, '0:zero,1:one,2:two,3:three,4:four,5:five');
 			JMVC.test.testValue("JMVC.each == nativeforarray;", function(){return jmvceach(big) == nativeforarray(big); }, true);
-			//JMVC.test.testValue("JMVC.each == jQuery.each;", function(){return jmvceach(big) == jqueryeach(big); }, true);
+			 //JMVC.test.testValue("JMVC.each == jQuery.each;", function(){return jmvceach(big) == jqueryeach(big); }, true);
 			JMVC.test.testValue("JMVC.each == nativeforobj;", function(){return jmvceach(obig) == nativeforobj(obig); }, true);
 			
 
 			
 			JMVC.test.finishAll();		
 
-			JMVC.test.describe('<p>What about <i>JMVC.each</i> times against <strong>pure loop</strong> or <i>jQuery.each</i>?</p><a name="times"></a><h1>Times comparison</h1>here some implementation of the each function are executed '+times+' times with the same random array sized '+ size + ' with elements between -1E6 and 1E6\nand compared to a native loop implementation.');
+			JMVC.test.describe('<p>What about <i>JMVC.each</i> times against <strong>pure loop</strong>' +
+				//' or <i>jQuery.each</i>' +
+				'?</p><a name="times"></a><h1>Times comparison</h1>here some implementation of the each function are executed '+times+' times with the same random array sized '+ size + ' with elements between -1E6 and 1E6\nand compared to a native loop implementation.');
 			
 			JMVC.test.describe('With Array');
 			JMVC.test.testTime('native for (array)', nativeforarray, times, [big]);
 			JMVC.test.testTime('native forEach (array)', nativeforeach, times, [big]);
-			//JMVC.test.testTime('jQuery.each (array)', jqueryeach, times, [big]);
+			 //JMVC.test.testTime('jQuery.each (array)', jqueryeach, times, [big]);
 			JMVC.test.testTime('JMVC.each (array)', jmvceach, times, [big]);
 
 			JMVC.test.describe('With Object literal');
 			JMVC.test.testTime('native for (obj literal)', nativeforobj, times, [obig]);
-			//JMVC.test.testTime('jQuery.each (obj literal)', jqueryeach, times, [obig]);
+			 //JMVC.test.testTime('jQuery.each (obj literal)', jqueryeach, times, [obig]);
 			JMVC.test.testTime('JMVC.each (obj literal)', jmvceach, times, [obig]);
 			
 			
