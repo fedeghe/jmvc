13640aa8515b6da0b3d4c65f2c81fff081e2ea10
fedeghe <fedeghe@gmail.com>
2014-02-08 00:28:28 +0100

    form clean

--- app/controllers/form.js
+++ app/controllers/form.js
@@ -1,37 +1,28 @@
 JMVC.controllers.form = function () {
 	'use strict';
 	this.action_index = function () {
-		
+
 		JMVC.events.loadify(500);
-		
+
 		JMVC.require('core/fvalidation/fvalidation');
-		
-		
+
 		var index = JMVC.getView('form'),
 			rules = {
 				'name' : 'string',
 				'option' : 'string',
 				'tarea' : 'string'
 			};
-			
-			
+
 		index.render(function () {
-			
 			var submit = JMVC.dom.find('#save');
-			
 			JMVC.events.bind(submit, 'click', function (e) {
 				!JMVC.fvalidation.validate(rules) && JMVC.events.preventDefault(e);
 			});
-			
 			JMVC.require('iscroll/iscroll');
 			JMVC.iscroll.create(JMVC.dom.find('#tarea'), 'urle', 2);
-			
 		});
 	};
-	
-	
-	
-	
+
 	this.action_validation = function () {
 		JMVC.events.loadify(500);
 		JMVC.require('core/validation/validation');
@@ -41,14 +32,17 @@ JMVC.controllers.form = function () {
 		index.render(function () {
 			var submit = JMVC.dom.find('#save'),
 				validator = JMVC.validation.create();
-			
-			//validator.add(rules);
-			
+
+			validator.add(JMVC.dom.find('#name'), {'required' : true, 'type' : 'integer', 'min' : 3, 'max' : 5});
+			validator.add(JMVC.dom.find('#option'), {'required' : true, msg : 'these dicks'});
+			validator.add(JMVC.dom.find('#tarea'), {'required' : true});
+			validator.add(JMVC.dom.find('#chk'), {'required' : true});
+
+
 			JMVC.events.bind(submit, 'click', function (e) {
-				validator.check();
-				JMVC.events.preventDefault(e);
+				validator.validate() || JMVC.events.preventDefault(e);
 			});
-			
+
 			JMVC.require('iscroll/iscroll');
 			JMVC.iscroll.create(JMVC.dom.find('#tarea'), 'urle', 2);
 
