a47417e3f60a239bd540512138f1613611ef1a19
fedeghe <fedeghe@gmail.com>
2014-03-18 12:05:23 +0100

    some int<>binary conversion functions attempt added

--- build/src/modules/num.js
+++ build/src/modules/num.js
@@ -10,8 +10,17 @@ JMVC.num = {
 	pFloat : function (f) {return 1 * f; },
 	pInt : function (i) {return i >> 0; },
 	mRound : function (n) {return (n + 0.5) >> 0; },
-	mFloor : function (n) {(n > 0 ? n : n + 1) >> 0; },
-	mCeil : function (n) {return (n + (n > 0 && !!(n % 1))) >> 0; },
-	num : function (n) {return parseFloat(n.toFixed(10), 10); }
+    //mFloor : function (n) {return ~~n; },
+	mFloor : function (n) {return n >> 0; },
+    mCeil : function (n) {return (n + (n > 0 && !!(n % 1))) >> 0; },
+	//mCeil : function (n) {return (n >> 0) + (n > 0 ? 1 : -1); },
+	num : function (n) {return parseFloat(n.toFixed(10), 10); },
+    dec2Bin : function (dec){
+        if(dec > 0) return dec.toString(2)>>>0;
+        dec = Math.abs(dec);
+        var res = dec ^ parseInt((new Array(dec.toString(2).length+1)).join("1"),2);
+        return (res+1).toString(2)>>>0;
+    },
+    bin2Dec : function (b) {return parseInt(b>>>0, 2); }
 };
 //-----------------------------------------------------------------------------
