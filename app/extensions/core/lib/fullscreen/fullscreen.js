bac57d122371675822d779a7930191a0247deec0
fedeghe <fedeghe@gmail.com>
2014-03-21 01:21:27 +0100

    fullscreen extension

---
+++ app/extensions/core/lib/fullscreen/fullscreen.js
@@ -0,0 +1,26 @@
+JMVC.extend('fullscreen', {
+    up : function (element) {
+        var ret = false;
+        if(element.requestFullscreen) {
+            ret = element.requestFullscreen();
+        } else if(element.mozRequestFullScreen) {
+            ret = element.mozRequestFullScreen();
+        } else if(element.webkitRequestFullscreen) {
+            ret = element.webkitRequestFullscreen();
+        } else if(element.msRequestFullscreen) {
+            ret = element.msRequestFullscreen();
+        }
+        return ret;
+    },
+    down : function () {
+        var ret = false;
+        if(document.exitFullscreen) {
+            ret = document.exitFullscreen();
+        } else if(document.mozCancelFullScreen) {
+            ret = document.mozCancelFullScreen();
+        } else if(document.webkitExitFullscreen) {
+            ret = document.webkitExitFullscreen();
+        }
+        return ret;
+    }
+});
