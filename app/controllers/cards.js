51cb534c25e8c05a67bdf988db3f925d044e6e7b
fedeghe <fedeghe@gmail.com>
2014-04-08 00:19:53 +0200

    cards controller updated for view the array.shuffle results

--- app/controllers/cards.js
+++ app/controllers/cards.js
@@ -1,16 +1,19 @@
 JMVC.controllers.cards = function () {
+    'use strict';
     this.action_index = function () {
+
+        var howmany = JMVC.p.n || 10;
 
         JMVC.require('games/deck/deck');
 
         JMVC.getView('vacuum')
             .set({
-                'style' : 'margin:20px;width:370px;',
+                'style' : 'margin:20px;width:442px;',
                 'id': 'container'
             })
             .render(function () {
-                JMVC.deck.initDecks(2);
-                JMVC.deck.shuffle(10);
+                JMVC.deck.initDecks(1);
+                JMVC.deck.shuffle(howmany);
 
                 JMVC.dom.append(JMVC.dom.find('#container'), JMVC.deck.getDecks());
             });
