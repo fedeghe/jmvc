1346ad5fc033c9c87fd67e5d9556fb7b96a1129b
fedeghe <fedeghe@gmail.com>
2014-04-10 00:25:06 +0200

    label modified

--- app/i18n/it.js
+++ app/i18n/it.js
@@ -2,7 +2,7 @@ JMVC.i18n.it = {
 	'ciao' : 'Ciao',
 	'come stai' : 'Come stai',
 	"sei in locale" : "Sei su localhost",
-	'pure javascript mvc framework' : 'Framework javascript puro',
+	'pure javascript mvc framework' : 'MVC lato browser in puro javascript',
 	'more':'piú info',
 	'source':'sorgente',
 	'desktop' : 'desktop',
