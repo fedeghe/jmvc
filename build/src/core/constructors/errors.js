9a934b4ad809eada15d1360c52c9051e183c4ace
fedeghe <fedeghe@gmail.com>
2014-04-15 01:37:26 +0200

    errors updated

--- build/src/core/constructors/errors.js
+++ build/src/core/constructors/errors.js
@@ -1,34 +1,46 @@
 /*----
 ERRORS
-------
+------ 
 
 specific classes that will extend the built-in Error Onject
 */
 Errors = {
-    'Network' : function (msg) {
+    Network : function (msg) {
         this.name = 'Network';
         this.msg = msg || this.name + ' error';
     },
-    'BadParams' : function (msg) {
+    BadParams : function (msg) {
         this.name = 'BadParams';
         this.msg = msg || this.name + ' error';
     },
-    'BadName' : function (msg) {
+    BadName : function (msg) {
         this.name = 'BadName';
         this.msg = msg || this.name + ' error';
     },
-    'BadImplement' : function (msg) {
+    BadImplement : function (msg) {
         this.name = 'BadImplement';
         this.msg = msg || this.name + ' error';
     },
-    'ControllerNotFound' : function (msg) {
+    ControllerNotFound : function (msg) {
         this.name = 'ControllerNotFound';
         this.msg = msg ||  this.name + ' error';
     },
-    'ActionNotFound' : function (msg) {
+    ActionNotFound : function (msg) {
         this.name = 'ActionNotFound';
         this.msg = msg ||  this.name + ' error';
     }
 };
-jmvc.multi_inherit(Errors, Error);
+
+jmvc.multi_inherit(Errors, W.Error);
+
+
+
+
+//
+Errors.notify = function (err) {
+    var msg = '[ERROR ' + err.name + ' : ' + err.msg;
+    JMVC.debug(msg) && alert(msg);
+    return false;
+};
+
 //-----------------------------------------------------------------------------
