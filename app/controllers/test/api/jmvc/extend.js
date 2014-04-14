fa24a32c71a19031d70b519d2febf4972f2f0d0f
fedeghe <fedeghe@gmail.com>
2014-04-15 01:29:59 +0200

    JMVC.extend test

--- app/controllers/test/api/jmvc/extend.js
+++ app/controllers/test/api/jmvc/extend.js
@@ -12,6 +12,8 @@ JMVC.controllers.extend = function() {
             JMVC.test.startAll();
             JMVC.test.describe('Extend JMVC');
 
+            JMVC.test.message('no param');
+
             JMVC.test.code("JMVC.extend('JMVC.test.purpose', {\n"+
                     "   pow : function (n, e) {\n"+
                     "       return Math.pow(n, e);\n"+
@@ -20,6 +22,14 @@ JMVC.controllers.extend = function() {
                     "       return 1 << n;\n"+
                     "   }\n"+
                 "});");
+
+            //JMVC.check(function () {JMVC.extend(); });
+
+            JMVC.test.testException(
+                "JMVC.extend throws JMVC.Errors.BadParams if the signature is not respected",
+                function () {JMVC.extend(); },
+                JMVC.Errors.BadParams
+            );
 
             
             JMVC.extend('test.purpose', {
@@ -32,12 +42,9 @@ JMVC.controllers.extend = function() {
                 sqrt : function (n) {
                     return Math.sqrt(n);
                 }
-            }, JMVC);
+            });
         
             
-
-            
-
             JMVC.test.testValue("JMVC.test.purpose.pow(4, 3);", function(){return JMVC.test.purpose.pow(4, 3); }, 64);
             JMVC.test.testValue("JMVC.test.purpose.pow2(4);", function(){return JMVC.test.purpose.pow2(4); }, 16);
             JMVC.test.testValue("JMVC.test.purpose.sqrt(5);", function(){return JMVC.test.purpose.sqrt(5).toFixed(2); }, 2.24);
