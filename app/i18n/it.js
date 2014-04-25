e5a67769dd82acb0050180692312967f642d0f0a
fedeghe <fedeghe@gmail.com>
2014-04-25 11:20:42 +0200

    Wording changed

--- app/i18n/it.js
+++ app/i18n/it.js
@@ -2,7 +2,7 @@ JMVC.i18n.it = {
 	'ciao' : 'Ciao',
 	'come stai' : 'Come stai',
 	"sei in locale" : "Sei su localhost",
-	'pure javascript mvc framework' : 'MVC lato browser in puro javascript',
+	'pure javascript mvc framework' : 'MVC lato client in puro javascript',
 	'more':'piú info',
 	'source':'sorgente',
 	'desktop' : 'desktop',
