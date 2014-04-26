89ce61b13ec7c60496bbe6492695b73a1e7fd43e
fedeghe <fedeghe@gmail.com>
2014-04-27 00:16:31 +0200

    some comments added

--- build/src/core/init.js
+++ build/src/core/init.js
@@ -1,64 +1,76 @@
-/*--
-INIT
---*/
-//
-// the returning object created in that function,
-// global JMVC will take the $JMVC ref
-var $JMVC,
-    //
+/*- INIT -*/
+
+// store starting time, that's not truly the starting time but 
+// it's really next to the real value
+var time_begin = +new Date(),
+
+    // the returning object created in that function,
+    // global JMVC will take the $JMVC ref
+    $JMVC,
+
     // version (vars.json)
     JMVC_VERSION = '$version$',
-    //
+
     // review (vars.json)
     JMVC_REVIEW = '$review$',
-    //
+
     // review (vars.json)
     JMVC_DATE = '__DATE__',
-    //
+
     // review (vars.json)
     JMVC_TIME = '__TIME__',
-    //
+
     // experimental (ignore it)
     JMVC_PACKED = '$packed$', //'.min' 
-    //
+
     // inner jmvc literal, will contain almost all the functions used to 
     // compose the $JMVC object and thus the returning JMVC
-    // @type {Object}
     jmvc = {},
-    //
+
     // url separator
     US = '/',
-    //
-    // in some cases is useful to automatically distinguish between a
-    // developing url and production url
-    // will be returned in a var container accessible from the JMVC object
-    // through JMVC.vars.baseurl & JMVC.vars.devurl
+
+    /*
+    in some cases is useful to automatically distinguish between a
+    developing url and production url
+    will be returned in a var container accessible from the JMVC object
+    through JMVC.vars.baseurl & JMVC.vars.devurl
+    */
     DEV_URL = WDL.protocol + US + US + 'www.jmvc.dev',
     PROD_URL = WDL.protocol + US + US + 'www.jmvc.org',
     DEV_URLstatic = WDL.protocol + US + US + 'static.jmvc.dev',
     PROD_URLstatic = WDL.protocol + US + US + 'static.jmvc.org',
-    //
-    // paths for
-    // extensions: used as basepath by JMVC.require
-    // test: tests
-    // lang: lang files
+
+    /**
+     * paths for
+     * extensions: used as basepath by JMVC.require
+     * test: tests
+     * lang: lang files
+     * @type {Object}
+     */
     PATH = {
-        //
-        // extensions path, used as base path in the JMVC.require function
-        // @type {string}
+
+        /**
+         * extensions path, used as base path in the JMVC.require function
+         * @type {[type]}
+         */
         ext  : US + 'app' + US + 'extensions' + US,
-        //
-        // test suite path, every controller matching "test_foocontroller"
-        // will automatically load the test suite and
-        //  
-        // foocontroller.js will be 
-        // searched into the /app/controller/test directory
-        // to use test suite a require('test') is needed until TODO is done
-        // @type {string}
+
+        /**
+         * test suite path, every controller matching "test_foocontroller"
+         * will automatically load the test suite and
+         *
+         * foocontroller.js will be 
+         * searched into the /app/controller/test directory
+         * to use test suite a require('test') is needed until TODO is done
+         * @type {[type]}
+         */
         test : US + 'app' + US + 'testsuite' + US,
-        //
-        // path for lang files, loaded with the JMVC.lang function
-        // @type {string}
+
+        /**
+         * path for lang files, loaded with the JMVC.lang function
+         * @type {[type]}
+         */
         lang : US + 'app' + US + 'i18n' + US
     },
     //
@@ -68,8 +80,8 @@ var $JMVC,
         'view' : '.html',
         'interface' : '.interface' + (JMVC_PACKED || '') + '.js'
     },
-    //
-    /**
+
+    /*
      * all these extensions can be used just after the action
      * @type {Array}
      */
@@ -83,32 +95,28 @@ var $JMVC,
         controller : 'index',
         action : 'index'
     },
-    //
-    // dispather function result
+
     /**
+     * dispather function result
      * here will be stored relevant results returned from the dispather function
      * used to parse the current url for getting all is needed to now how to get
      * the right response
      */
     dispatched,
-    //
-    /**
-     * MVC basic constructors
-     */
+
+    /* MVC basic constructors */
     Controller,
     Model,
     View,
     Interface,
-    //
+
     /**
      * the parser object, used for replacing all available placeholders
      * (views, views variables, chunks, snippets)
      */
     Parser,
-    //
-    /**
-     * some useful constructors 
-     */
+
+    // Some useful constructors
     Pipe,
     Event,
     Promise,
@@ -116,36 +124,23 @@ var $JMVC,
     Channel,
     Extension,
     FunctionQueue,
-    //
-    /**
-     * in case some modules need to be always loaded here's the place to set them
-     * @type {Array}
-     */
+
+    // in case some modules need to be always loaded here's the place to set them
     Modules = ['vendors/google/analytics/analytics', 'core/cookie/cookie'],
-    //
-    /**
-     * preloader
-     */
+
+    // preloader
     preload,
-    //
-    /**
-     * hooks literal used to execute callbacks as far as some relevant event are fired
-     * starting fron the request and ending with the document rendering end
-     * @type {Object}
-     */
+
+    // hooks literal used to execute callbacks as far as some relevant event are fired
+    // starting fron the request and ending with the document rendering end
     hooks = {},
-    //
-    /**
-     * a literal to store loaded lang files
-     * @type {Object}
-     */
+
+    // a literal to store loaded lang files
     defaultlang = 'en',
+
     currentlang = defaultlang,
-    //
-    // store starting time, that's not truly the starting time but 
-    // it's really next to the real value
-    time_begin = +new Date(),
-    //
+
+
     //undefined string for typeof
     undef = 'undefined',
     //
