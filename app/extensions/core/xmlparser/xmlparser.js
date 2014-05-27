d8cd3d16360aa621017923513248b38a2f2b8ecd
fedeghe <fedeghe@gmail.com>
2014-05-28 00:48:37 +0200

    fixed a bug on xmparser

--- app/extensions/core/xmlparser/xmlparser.js
+++ app/extensions/core/xmlparser/xmlparser.js
@@ -1,6 +1,6 @@
 JMVC.extend('xmlparser', {
 
-	// takes the contant of
+	// takes the content of
 	// the xml to be parsed
 	//
 	load : function (txt, or_is_xml) {
@@ -11,7 +11,10 @@ JMVC.extend('xmlparser', {
 
 		if (or_is_xml == undefined) {
 		//clean up a bit
-			txt = txt.replace(/\n/g, "").replace(/[\t ]+\</g,"<").replace(/\>[\t ]+\</g,"").replace(/\>[\t ]+$/g, ">");
+			txt = txt.replace(/\n/g, "")
+				.replace(/[\t ]+\</g,"<")
+				.replace(/\>[\t ]+\</g,"")
+				.replace(/\>[\t ]+$/g, ">");
 
 			this.xmlDoc = false;
 
@@ -32,12 +35,14 @@ JMVC.extend('xmlparser', {
 		
 		
 		this.nodeExtractor = function (t) {return t; };
-		this.pointerNode = false;
 
 		//set the extractor function or get the fTH node
 		this.extractor = function (f, reset_extractor) {
 			if (typeof f === 'number') {
-				return this.pointerNode.childNodes.length > f ? this.nodeExtractor(this.pointerNode.childNodes[f]) : false;
+				return this.pointerNode.childNodes.length > f ?
+					this.nodeExtractor(this.pointerNode.childNodes[f])
+					:
+					false;
 			}
 			if (typeof f === 'function' && (!this.nodeExtractor || reset_extractor)) {
 				this.nodeExtractor = f;
@@ -62,7 +67,9 @@ JMVC.extend('xmlparser', {
 		
 		
 		this.pointer = function(node){
-			if(node){this.pointerNode = node;}
+			if (node) {
+				this.pointerNode = node;
+			}
 			return this.pointerNode;
 		}
 		
@@ -70,13 +77,12 @@ JMVC.extend('xmlparser', {
 			return this.xmlDoc.childNodes[0];
 		};
 		
-		this.toJson = function(){
-//			Create the return object
-			var obj = {}, xml = that.xmlDoc;
+		this.toJson = function (xml) {
+			var obj = {};
+			xml = xml ||that.pointerNode;
 
-//			element
+
 			if (xml.nodeType == 1) {
-//				do attributes
 				if (xml.attributes.length > 0) {
 				obj["@attributes"] = {};
 					for (var j = 0; j < xml.attributes.length; j++) {
@@ -85,31 +91,37 @@ JMVC.extend('xmlparser', {
 					}
 				}
 			}
-//			text
+
 			else if (xml.nodeType == 3) {
-				obj = xml.nodeValue;
+				obj = xml.nodeValue;//.replace(/\n/g, '').replace(/^\s*/, '').replace(/\s*$/, '');
 			}
 
-//			do children
 			if (xml.hasChildNodes()) {
 				for(var i = 0; i < xml.childNodes.length; i++) {
 					var item = xml.childNodes.item(i),
 						nodeName = item.nodeName,
-						old;
-					if (typeof obj[nodeName] == "undefined") {
-						obj[nodeName] = that.toJson(item);
+						old, tmp;
+
+					tmp = this.toJson(item);
+
+					if (typeof(obj[nodeName]) == "undefined") {
+						!!tmp && (obj[nodeName] = tmp);
 					} else {
-						if (typeof(obj[nodeName].length) == "undefined") {
+
+						if (typeof(obj[nodeName].push) == "undefined") {
 							old = obj[nodeName];
 							obj[nodeName] = [];
 							obj[nodeName].push(old);
 						}
-						obj[nodeName].push(that.toJson(item));
+						
+						!!tmp && obj[nodeName].push(tmp);
 					}
 				}
 			}
 			return obj;
-		}
+		};
+		// by default is the root
+		this.pointerNode = this.root();
 	},
 	
 	
@@ -129,49 +141,5 @@ JMVC.extend('xmlparser', {
 	_tag : function(node, tag, n){
 		var nodes = node.getElementsByTagName(tag);
 		return n<nodes.length ? nodes[n] : false;
-	},
-	
-//	Thanks to the great David Walsh
-//	url : http://davidwalsh.name/convert-xml-json
-	toJson : function(xml){ 
-//		Create the return object
-		var obj = {};
-
-//		element
-		if (xml.nodeType == 1) {
-//			do attributes
-			if (xml.attributes.length > 0) {
-				obj["@attributes"] = {};
-				for (var j = 0; j < xml.attributes.length; j++) {
-					var attribute = xml.attributes.item(j);
-					obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-				}
-			}
-		}
-//		text
-		else if (xml.nodeType == 3) {
-			obj = xml.nodeValue;
-		}
-		
-//		do children
-		if (xml.hasChildNodes()) {
-			for(var i = 0; i < xml.childNodes.length; i++) {
-				var item = xml.childNodes.item(i),
-					nodeName = item.nodeName,
-					old;
-
-				if (typeof(obj[nodeName]) == "undefined") {
-					obj[nodeName] = JMVC.xmlparser.toJson(item);
-				} else {
-					if (typeof(obj[nodeName].length) == "undefined") {
-						old = obj[nodeName];
-						obj[nodeName] = [];
-						obj[nodeName].push(old);
-					}
-					obj[nodeName].push(JMVC.xmlparser.toJson(item));
-				}
-			}
-		}
-		return obj;
 	}
 });
