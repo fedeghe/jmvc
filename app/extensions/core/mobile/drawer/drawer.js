7fa40e6dc98d54856908adc4a9c616dcd15c3456
fedeghe <fedeghe@gmail.com>
2014-07-19 13:51:46 +0200

    bad first attempt for a drawer

--- app/extensions/core/mobile/drawer/drawer.js
+++ app/extensions/core/mobile/drawer/drawer.js
@@ -16,7 +16,20 @@ JMVC.extend('mobile.drawer', function () {
             i = 0,
             l = list.length,
             drwID = '' + JMVC.util.uniqueid,
-            dim = JMVC.screen.getViewportSize();
+            dim = JMVC.screen.getViewportSize(),
+            body = document.body,
+            offset = 40,
+            //
+            // wrap
+            wrap = JMVC.dom.wrap(body);
+
+        JMVC.css.style(wrap, {
+            zIndex : 100,
+            position:'relative'
+            //,overflow : 'hidden'
+        });
+
+        JMVC.css.unselectable(body);
 
         for (var tmp; i < l; i++) {
             tmp = {
@@ -36,65 +49,87 @@ JMVC.extend('mobile.drawer', function () {
                 
                 var node = this,
                     n = node.getNode(drwID);
-                JMVC.events.drag.on(document.body, {
-                    // start : function (e, data) { 
-                    //     console.dir(data);
-                    //     var n = JMVC.dom.create('div', {'style': 'width:3px;height:3px;background-color:black;position:absolute;top:'+data.start[1]+'px;left:'+data.start[0]+'px;'}, '&nbsp;');
-                    //     JMVC.dom.append(document.body, n);
-                    // },
+
+                JMVC.events.drag.on(body, {
                     move : function (e, data) { 
-                        
-                        if (!visible && data.orientation == 'e') {
-                            
-                            JMVC.css.style(n, 'left', (-dim.width + data.distance) + 'px');
-                        }
-                        if (visible && data.orientation == 'o') {
-                            
-                            JMVC.css.style(n, 'left', (-data.distance) + 'px');
+                        var left = JMVC.events.coord(e)[0];
+
+                        if (left > dim.width - offset) {
+                            JMVC.events.preventDefault(e);
+                            return false;
+                        } else if (!visible && data.orientation == 'e') {
+                            JMVC.css.style(n, 'width', left + 'px');
+                            JMVC.css.style(wrap, 'left', left + 'px');
+                        } else if (visible && data.orientation == 'o') {
+                            JMVC.css.style(n, 'width', left + 'px');
+                            JMVC.css.style(wrap, 'left', left + 'px');
                         }
                     },
                     end : function (e, data) {
+                        var left = JMVC.events.coord(e)[0];
+
                         if (!visible && data.orientation == 'e') {
-                            if (data.distance > dim.width/2) {
-                                JMVC.css.style(n, 'left', '0px');
+                            if (left > dim.width/2) {
+
+                                JMVC.css.style(n, 'width', (dim.width - offset) + 'px');
+                                JMVC.css.style(wrap, 'left', (dim.width - offset) + 'px');
                                 visible = true; 
                             } else {
-                                JMVC.css.style(n, 'left', -dim.width + 'px');   
+                            
+                                JMVC.css.style(n, 'width', '0px');
+                                JMVC.css.style(wrap, 'left', '0px');
                                 visible = false;
                             }
                         } else if (visible && data.orientation == 'o') {
-                            if (data.distance > dim.width/2) {
-                                JMVC.css.style(n, 'left', -dim.width + 'px');
+                            if (left < dim.width/2) {
+                            
+                                JMVC.css.style(n, 'width', '0px');
+                                JMVC.css.style(wrap, 'left', '0px');
                                 visible = false; 
                             } else {
-                                JMVC.css.style(n, 'left', '0px');   
+                                JMVC.css.style(n, 'width', (dim.width - offset) + 'px');
+                                JMVC.css.style(wrap, 'left', (dim.width - offset) + 'px');
                                 visible = true;
                             }
                         } else {
-                            JMVC.css.style(n, 'left', (visible ? 0 : -dim.width) + 'px');
-                        } 
-                        
+                            
+                            JMVC.css.style(n, 'width', (visible ? dim.width - offset : 0) + 'px');
+                            JMVC.css.style(wrap, 'left', (visible ? dim.width - offset : 0) + 'px');
+                        }
                     }
                 });
-                
-                
             },
             content : [{
+                target : wrap,
                 attrs : {'class':'JMVC-drawer-hamburger round8 resp_mobi'},
-                style  : {color : 'white', fontSize:'20px', padding:'4px 8px'},
+                style  : {
+                    color : 'white',
+                    fontSize:'20px',
+                    padding:'4px 8px',
+                    position:'absolute',
+                    left : (- parseInt(JMVC.css.style(document.body, 'paddingLeft'), 10)) + 'px',
+                    top : (- parseInt(JMVC.css.style(document.body, 'paddingTop'), 10)) + 'px'
+                },
                 html : JMVC.core.widgzard.htmlspecialchars('>'),
                 cb : function () {
                     var node = this;
-                    JMVC.events.bind(node, 'click', function () {
+                    JMVC.events.on(node, 'click', function () {
                         var n = node.getNode(drwID),
-                            st = parseInt(JMVC.css.style(n, 'left'), 10);
-                        if (!st) {
+                            
+                            st = parseInt(JMVC.css.style(n, 'width'), dim.width + 'px');
+
+                        
+                        if (st) {
                             visible = false;
-                            JMVC.css.style(n, 'left', -dim.width + 'px');    
+                            
+                            JMVC.css.style(n, 'width', '0px');    
+                            JMVC.css.style(wrap, 'left', '0px');    
                             
                         } else {
                             visible = true;
-                            JMVC.css.style(n, 'left', '0px');                            
+                                                       
+                            JMVC.css.style(n, 'width', (dim.width - offset) + 'px');                            
+                            JMVC.css.style(wrap, 'left', (dim.width - offset) + 'px');                            
                             
                         }
                         
@@ -103,18 +138,21 @@ JMVC.extend('mobile.drawer', function () {
                     
                 }
             },{
+                target : wrap,
                 wid : drwID,
                 attrs : {'class': 'JMVC-drawer resp_mobi respfixed'},
                 style : {
                     position: 'fixed',
                     top : '0px',
-                    left: '-100%',
-                    bottom: '0px',
+                    left:'0px',
+                    minHeight:'100%',
                     overflow : 'scroll',
-                    padding : '10px',
-                    width: (dim.width - 40 ) + 'px'
+                    //width: (dim.width - 40 ) + 'px',
+                    width : '0px',
+                    zIndex : 1
                 },
                 content : [{
+                    attrs : {'class':'cnt'},
                     tag : 'ul',
                     content : cnt
                 }]
@@ -135,7 +173,6 @@ JMVC.extend('mobile.drawer', function () {
     }
 
     Drawer.prototype.render = function () {
-        
         JMVC.core.widgzard.render(this.tpl);
     };
 
