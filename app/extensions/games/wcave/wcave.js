ab5aa8f90ba431d1b9d78feb49f5906e37514a69
fedeghe <fedeghe@gmail.com>
2014-07-19 13:52:22 +0200

    wcave build (irrelevant)

--- app/extensions/games/wcave/wcave.js
+++ app/extensions/games/wcave/wcave.js
@@ -5,7 +5,7 @@ JMVC.head.addStyle(JMVC.vars.baseurl + '/app/extensions/games/wcave/wcave.css');
 JMVC.head.addStyle(JMVC.vars.baseurl + '/app/extensions/games/wcave/freckle.css', true);
 /**
  * 
- * wcave build n.101
+ * wcave build n.118
  * 
  * @version: 1.4.13
  * @date : 25-04-2013
