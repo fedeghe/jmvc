bebea0f10694e8e62032bb94e2d07ccbaa2686e6
fedeghe <fedeghe@gmail.com>
2014-07-31 00:28:41 +0200

    autoload predefined modules only if no JMVCshut

--- build/src/core/render.js
+++ build/src/core/render.js
@@ -2,14 +2,15 @@
 (function () {
     var i = 0,
         l = JMVC.modules.length;
-    
-    while (i < l) {
-        JMVC.require(JMVC.modules[i]);
-        i += 1;
-    }
-    
-    if (JMVC.p.lang) {
-        JMVC.cookie.set('lang', JMVC.p.lang);
+    if (!W.JMVCshut) {
+        while (i < l) {
+            JMVC.require(JMVC.modules[i]);
+            i += 1;
+        }
+        
+        if (JMVC.p.lang) {
+            JMVC.cookie.set('lang', JMVC.p.lang);
+        }
     }
 
     try {
@@ -21,6 +22,5 @@
     } catch (e) {
         return JMVC.Errors.notify(e);
     } 
-
 })();
 //==========================================
