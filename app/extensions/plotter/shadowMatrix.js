ab9bfcac483ba95f6ef206599c38b05382b377d9
fedeghe <fedeghe@gmail.com>
2014-10-23 22:14:54 +0200

    shadow matrix experimental first stage

---
+++ app/extensions/plotter/shadowMatrix.js
@@ -0,0 +1,241 @@
+(function (){
+
+    var _constructor = function (opts) {
+
+
+            this.scale = opts.scale || 1;
+            this.className = opts['className'] || 'shadowMatrix';
+            this.colorMap = opts.colorMap || {};
+            this.matrix = opts.matrix || opts.frames[0];
+            this.frames = opts.frames || [];
+            
+            this.size = [this.matrix.length, this.matrix[0].split(',').length];
+
+            this.tpl = {
+                position : 'relative',
+                width: '0px',
+                height: '0px',
+                margin : (this.size[0] * this.scale) + 'px,' + (this.size[1] * this.scale) + 'px',
+                'box-shadow':null,
+                '-webkit-box-shadow' : null,
+                '-moz-box-shadow' : null
+            };
+            this.pointTpl = '%left%px %top%px 0px ' + this.scale + 'px %color%';
+        },
+        proto = _constructor.prototype;
+    
+
+
+    
+
+
+    proto.animate = function (opts, ms) {
+        var n = this.frames.length,
+            self = this,
+            i = 0;
+        window.setInterval(function () {
+            self.matrix = self.frames[i]
+            self.draw({node : opts.node});
+            i = (i + 1) % n;
+        }, ms);
+    };
+
+    
+
+
+    proto.draw = function (opts) {
+        this.node || (this.node = opts.node || (function (){alert('target node missing!!!'); })());
+        this.node.innerHTML = '';
+
+        var out = [],
+            self = this,
+            ret,
+            i = 0,
+            j = 0,
+            l = 0,
+            rows, el, prms;
+        for (null; i < this.size[0]; i++) { 
+            rows = this.matrix[i].split(',');           
+            for (l = rows.length, j = 0; j < l; j++) {
+                if (this.colorMap[rows[j]] instanceof Array && this.colorMap[rows[j]][0] instanceof Function) {
+                    prms = this.colorMap[rows[j]][1] || {};
+                    this.node.appendChild(this.colorMap[rows[j]][0].call(this, i, j, prms));
+                } else {
+                    el = this.colorMap[rows[j]] || '#000';
+                    out.push(JMVC.string.replaceAll(this.pointTpl,{
+                        left: j * this.scale * 2,
+                        top : i * this.scale * 2 ,
+                        color : this.colorMap[rows[j]] || '#000'
+                    }));
+                }
+            }
+        }
+        this.tpl['box-shadow'] = this.tpl['-webkit-box-shadow'] = this.tpl['-moz-box-shadow'] = out.join(',');
+        JMVC.css.style(this.node, this.tpl);
+    };
+
+    proto.redraw = function (mat){
+        if (mat) this.matrix = mat;
+        this.draw();
+    };
+
+
+    proto.mirror = function () {
+        var m = [],
+            i = 0,
+            tmp;
+        while (i < this.size[0]) {
+            tmp = this.matrix[i].split(',')
+            tmp.reverse();
+            m.push(tmp.join(','));
+            i++;
+        }
+        this.matrix = m;
+        this.redraw();
+        return this;
+    };
+
+
+
+    JMVC.shadowMatrix = function (opts) {
+        return new _constructor(opts);
+    }
+
+    JMVC.shadowMatrix.getMatrixFromImage = function (opts) {
+        var img,
+            w, h,
+            cnv, ctx,
+            res = {
+                scale : opts.size || 1,
+                matrix : [],
+                colorMap : {}
+            },
+            tmpMatrix = [],
+            sampling = opts.sampling || 1,
+            getPxData = function () {
+                return ctx.getImageData(0, 0, cnv.width, cnv.height);
+            },
+            getPxFun = function () {
+                var pixels = getPxData(),
+                    w = pixels.width,
+                    h = pixels.height,
+                    px = pixels.data;
+
+                return function (r, c) {
+                    if (r < 0 || r >= h || c < 0 || c >= w) {
+                        return [0, 0, 0, 0];
+                    }
+                    var i = 4 * (r * w + c);
+                    return [
+                        px[i],
+                        px[i + 1],
+                        px[i + 2],
+                        px[i + 3]
+                    ];
+                }
+            },
+            px,
+            // a promise to proceed only when
+            // the image is loaded
+            // &&
+            // the matrix is ready
+            promiseLoadAndDone = new JMVC.Promise(),
+            proceed = function (){
+                var i, r, c, h, w, row, compon,
+                    tmpColorMap,
+                    colorIndex;
+                //
+                //  create shoulder canvas with the image
+                cnv = document.createElement('canvas');
+                w = cnv.width = img.width;
+                h = cnv.height = img.height;
+                cnv.style.display = 'none';
+                //JMVC.WDB.appendChild(cnv);
+                ctx = cnv.getContext("2d");
+                ctx.drawImage(img, 0, 0);
+
+                pxF = getPxFun();
+                
+                //
+                //  write matrix and collect colorMap
+                for (r = 0, c = 0, tmpColorMap = [], colorIndex = 0; r < h; r++) {
+                    row = [];
+                    for (c = 0; c < w; c++) {
+                        compon = pxF(r, c);
+                        color = 'rgba(' + compon.join(',') + ')';
+                        if (!tmpColorMap[color]){
+                            tmpColorMap[color] = colorIndex++;
+                        }
+                        row.push(tmpColorMap[color]);
+                    }
+                    res.matrix.push(row.join(','))
+                }
+
+                // 
+                // invert to obtain colorMap
+                for (i in tmpColorMap) {
+                    res.colorMap[tmpColorMap[i]] = i;
+                }
+
+                //
+                // resolve the promise, passing res to all thens
+                promiseLoadAndDone.done(res);
+            };
+
+        if (opts.imgUrl) {
+            //
+            // create image from url
+            img = new Image();
+            //
+            // proceed when loaded
+            img.onload = proceed;
+            img.src = opts.imgUrl;
+
+        } else if (opts.img) {
+
+            img = opts.img;
+            proceed();
+        }
+
+        //
+        // return it to allow then execution
+        return promiseLoadAndDone;
+    };
+
+
+
+    JMVC.shadowMatrix.triUL = function (i, j, prms) {
+        var n = JMVC.dom.create('div');
+        JMVC.css.style(n, {
+            position:'absolute',
+            width:'0px',
+            height:'0px',
+            borderBottom : (2 * this.scale) + 'px solid ' + this.colorMap[prms.color],
+            borderLeft : (2 * this.scale) + 'px solid transparent',
+            top: (this.scale * (2 * i -1)) + 'px',
+            left:(this.scale * (2 * j -1)) + 'px'
+        });
+        return n;
+    };
+
+    JMVC.shadowMatrix.triUR = function(i, j, prms) {
+        var n = JMVC.dom.create('div');
+        JMVC.css.style(n, {
+            position:'absolute',
+            width:'0px',
+            height:'0px',
+            borderBottom : (2 * this.scale) + 'px solid ' + this.colorMap[prms.color],
+            borderRight : (2 * this.scale) + 'px solid transparent',
+            top: ((2 * i -1) * this.scale) + 'px',
+            left:(this.scale * (2 * j -1)) + 'px'
+        });
+        return n;
+    };
+
+})();
+
+/*
+JMVC.require('plotter/shadowMatrix2');
+var t = new JMVC.shadowMatrix('Federico');
+t.hello();
+ */
