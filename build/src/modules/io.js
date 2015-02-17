c6407119b6c34ce9a59724aef53d4cb6a660e83a
fedeghe <fedeghe@gmail.com>
2015-02-17 23:24:18 +0100

    XDomainRequest added

--- build/src/modules/io.js
+++ build/src/modules/io.js
@@ -9,17 +9,29 @@ _.io = {
      * Façade for getting the xhr object
      * @return {object} the xhr
      */
-    getxhr : function () {
-        return ('XMLHttpRequest' in W) ?
-            function () {JMVC.io.xhrcount += 1; return new W.XMLHttpRequest(); }
-            :
-            ('ActiveXObject' in W) ?
-            function () {JMVC.io.xhrcount += 1; return new W.ActiveXObject('Microsoft.XMLHTTP'); }
-            :
-            function () {
-                JMVC.debug('No way to initialize XHR');       
-            };
-    }(),
+    getxhr : function (o) {
+        var xhr,
+            IEfuckIds = ['Msxml2.XMLHTTP', 'Msxml3.XMLHTTP', 'Microsoft.XMLHTTP'],
+            xdr = typeof W.XDomainRequest !== 'undefined' && document.all && !(navigator.userAgent.match(/opera/i)),
+            len = IEfuckIds.length,
+            i = 0;
+
+        if (xdr && o.cors) {
+            xhr = new W.XDomainRequest();
+        } else {
+            try {
+                xhr = new W.XMLHttpRequest();
+            } catch (e1) {
+                for (null; i < len; i += 1) {
+                    try {
+                        xhr = new W.ActiveXObject(IEfuckIds[i]);
+                    } catch (e2) {continue; }
+                }
+                !xhr && alert('No way to initialize XHR');
+            }
+        }
+        return xhr;
+    },
     /**
      * [ description]
      * @param  {[type]} uri     [description]
@@ -27,23 +39,20 @@ _.io = {
      * @return {[type]}         [description]
      */
     ajcall : function (uri, options) {
-        var xhr = _.io.getxhr(),
+        var xhr = _.io.getxhr(options),
+            xdr = typeof W.XDomainRequest !== 'undefined' && document.all && !(navigator.userAgent.match(/opera/i)),
             method = (options && options.method) || 'POST',
             cback = options && options.cback,
-
             cb_opened = (options && options.opened) || function () {},
             cb_loading = (options && options.loading) || function () {},
             cb_error = (options && options.error) || function () {},
-            cb_abort = (options && options.abort) || function () {return false; },
-            cb_loadstart = (options && options.loadstart) || function () {},
-            cb_timeout = (options && options.timeout) || function () {},
-            
+            cb_abort = (options && options.abort) || function () {},
             sync = options && options.sync,
-            data = (options && options.data) || false,
+            data = (options && options.data) || {},
             type = (options && options.type) || 'text/html',
             cache = (options && options.cache !== undefined) ? options.cache : true,
             targetType = type === 'xml' ?  'responseXML' : 'responseText',
-            timeout = options && options.timeout || 3000,
+            timeout = options && options.timeout || 10000,
             complete = false,
             res = false,
             ret = false,
@@ -56,90 +65,121 @@ _.io = {
         }
         data = JMVC.object.toQs(data).substr(1);
 
-        xhr.onreadystatechange = function () {
-            
-            var tmp;
-            if (state === xhr.readyState) {
-                return false;
-            }
-            state = xhr.readyState;
-            //if (xhr.readyState === 'complete' || (xhr.readyState === 4 && xhr.status === 200)) {
-            if (xhr.readyState === 4){
-                if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
-                    complete = true;
-                    if (cback) {
-                        res = xhr[targetType];
-                        (function () {cback(res); })(res);
+        if (xdr && options.cors) {
+                // xhr is actually a xdr
+                //alert('only XDR');
+                xhr.open(method, (method === 'GET') ? (uri + ((data) ? '?' + data: '')) : uri);
+
+                xhr.onerror = cb_error;
+                xhr.ontimeout = function () {};
+                xhr.onprogress = function () {};
+                xhr.onload = function (r) {
+                    // cback((targetType === 'responseXML') ? r.target[targetType].childNodes[0] : r.target[targetType]);
+                    cback(xhr.responseText);
+                };
+                xhr.timeout = 3000;
+                var tmp = {
+                    xml : 'text/xml',
+                    html : 'text/html',
+                    json : 'application/json'
+                }[type] || 'text/html';
+
+                xhr.contentType = tmp;
+                window.setTimeout(function () {
+                    xhr.send();    
+                }, 20);
+                
+
+
+            } else {
+                xhr.onreadystatechange = function () {
+                    var tmp;
+                    if (state === xhr.readyState) {
+                        
+                        return false;
                     }
-                    ret = xhr[targetType];
-                    //IE leak ?????
-                    W.setTimeout(function () {
-                        JMVC.io.xhrcount -= 1;
-                        //JMVC.purge(xhr);
-                    }, 50);
-                    return ret;
-                } else {
-                    cb_error(xhr);
-                    return false;
-                }
-            } else if (xhr.readyState === 3) {
-                //loading data
-                cb_loading(xhr);
-            } else if (xhr.readyState === 2) {
-                //headers received
-                cb_opened(xhr);
-            } else if (xhr.readyState === 1) {
-                cb_loadstart(xhr);
-                switch (method) {
-                case 'POST':
-                    try {
-                        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                        xhr.send(data || true);
-                    } catch (e1) {}
-                    break;
-                case 'GET':
-                    try {
-                        tmp = {
-                            xml : 'text/xml',
-                            html : 'text/html',
-                            json : 'application/json'
-                        }[type] || 'text/html';
-                        xhr.setRequestHeader('Accept', tmp + '; charset=utf-8');
-                        xhr.send(null);
-                    } catch (e2) {}
-                    break;
-                default :
-                    xhr.send(null);
-                    break;
-                }
+                    state = xhr.readyState;
+                    
+                    if (xhr.readyState === 'complete' || (~~xhr.readyState === 4 && ~~xhr.status === 200)) {
+                        complete = true;
+                        if (cback) {
+                            res = xhr[targetType];
+                            (function () {cback(res); })(res);
+                        }
+                        ret = xhr[targetType];
+                        //IE leak ?????
+                        
+                        W.setTimeout(function () {
+                            xhr = null;
+                        }, 50);
+                        return ret;
+                    } else if (xhr.readyState === 3) {
+                        //loading data
+                        cb_loading(xhr);
+                    } else if (xhr.readyState === 2) {
+                        //headers received
+                        cb_opened(xhr);
+                    } else if (xhr.readyState === 1) {
+                        switch (method) {
+                        case 'POST':
+                            try {
+                                xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                                xhr.send(data || true);
+                            } catch (e1) {}
+                            break;
+                        case 'GET':
+                            try {
+                                tmp = {
+                                    xml : 'text/xml',
+                                    html : 'text/html',
+                                    json : 'application/json'
+                                }[type] || 'text/html';
+
+                                xhr.setRequestHeader('Accept', tmp + '; charset=utf-8');
+                                xhr.send(null);
+                            } catch (e2) {}
+                            break;
+                        default :
+                            alert(method);
+                            xhr.send(null);
+                            break;
+                        }
+                    }
+                    return true;
+                };
+                xhr.onerror = function () {
+                    
+                    cb_error && cb_error.apply(null, arguments);
+                };
+                xhr.onabort = function () {
+
+                    cb_abort && cb_abort.apply(null, arguments);
+                };
+                //open request
+                xhr.open(method, (method === 'GET') ? (uri + ((data) ? '?' + data: '')) : uri, sync);
+
+                //thread abortion
+                W.setTimeout(function () {
+                    if (!complete) {
+                        complete = true;
+                        xhr.abort();
+                    }
+                }, timeout);
+                try {
+                    return (targetType === 'responseXML') ? xhr[targetType].childNodes[0] : xhr[targetType];
+                } catch (e3) {}
+
+
             }
+
+
+
+
+            
+
+
+
             return true;
-        };
-
-        xhr.onerror = function () {
-            cb_error && cb_error.apply(null, arguments);
-        };
-        xhr.onabort = function () {
-            cb_abort && cb_abort.apply(null, arguments);
-        };
-
-        // open request
-        //
-        xhr.open(method, (method === 'GET') ? (uri + ((data) ? '?' + data: '')) : uri, sync);
-        
-        // thread abortion
-        //
-        W.setTimeout(function () {
-            if (!complete) {
-                cb_timeout(xhr);
-                complete = true;
-                xhr.abort();
-            }
-        }, timeout);
-        try {
-            return (targetType === 'responseXML') ? xhr[targetType].childNodes[0] : xhr[targetType];
-        } catch (e3) {}
-        return true;
     }
 };
 
