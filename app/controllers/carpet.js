3972357f676268e51e9d3b43b0a05b13e21e0eba
fedeghe <fedeghe@gmail.com>
2015-06-01 10:57:08 +0200

    fixed some params in divrot

--- app/controllers/carpet.js
+++ app/controllers/carpet.js
@@ -1,7 +1,8 @@
 JMVC.controllers.carpet = function () {
 	'use strict';
+
 	/* default action */
-	this.action_index = function () {
+	this.action_index = function (f) {
 
 		/* get lib & view */
 		JMVC.require(
@@ -14,19 +15,31 @@ JMVC.controllers.carpet = function () {
 
 
 		var index = JMVC.getView('zero'),
+			size = null,
 			jmap = false,
 			dims = JMVC.screen.getViewportSize(),
-			Ww = dims.width,
-			Wh = dims.height,
+			Ww = size || dims.width,
+			Wh = size || dims.height,
 			that = this;
 
 		/* top = left = 100 from default css */
 
 		index.render(function () {
 			/* cssmargin5 * sides2 +border1 */
-			jmap = JMVC.carpet.create(JMVC.dom.body(), Ww - 11, Wh - 11, 3);
+			jmap = JMVC.carpet.create(JMVC.dom.body(), {
+				w : Ww - 11,
+				h : Wh - 11,
+				s : 3,
+				tileWidth : 300,
+				tileHeight: 300
+				// ,enabled : {
+				// 	oriz : false
+				// }
+			});
 
 			that.set('jmap', jmap);
+
+			f && typeof f === 'function' && f();
 
 			if (JMVC.p.debug === 'true') {
 				jmap.beforeAdd(function (i) {console.debug('adding ID: ' + i); });
@@ -35,15 +48,16 @@ JMVC.controllers.carpet = function () {
 				jmap.afterRemove(function (i) {console.debug('removed ID: ' + i); });
 			}
 			
-			jmap.enableSpeed(4.2);
+			// jmap.enableSpeed(4.2);
 		});
 
 	};
 
 	this.action_logo = function () {
+
 		var that = this;
 		this.action_index(function () {
-			//console.debug(that);
+			
 			var jmap = that.get('jmap');
 			jmap.beforeAdd(function (i) {console.debug('adding ID: ' + i); });
 			jmap.afterAdd(function (i) {console.debug('added ID: ' + i); });
@@ -51,5 +65,6 @@ JMVC.controllers.carpet = function () {
 			jmap.afterRemove(function (i) {console.debug('removed ID: ' + i); });
 			JMVC.debug(jmap);
 		});
+
 	};
 };
