d076f7382a53cfdad7492291f12b6e7530f549f1
fedeghe <fedeghe@gmail.com>
2015-08-30 22:03:46 +0200

    jsonify rewritten completely

--- app/extensions/core/xmlparser/xmlparser.js
+++ app/extensions/core/xmlparser/xmlparser.js
@@ -7,7 +7,11 @@ JMVC.extend('xmlparser', {
 
 		var that = this,
 			xmlDoc,
-			parser;
+			parser,
+			clean = function (d) {
+				return d.replace(/>\n</g, '').replace(/\t/g, '').replace(/^\s*/g, '').replace(/\s*$/g, '');
+				// return d.replace(/\n/gm, '').replace(/\t/gm, '').replace(/^\s*/g, '').replace(/\s*$/g, '');
+			};
 
 		if (or_is_xml == undefined) {
 		//clean up a bit
@@ -93,7 +97,10 @@ JMVC.extend('xmlparser', {
 			}
 
 			else if (xml.nodeType == 3) {
-				obj = xml.nodeValue;//.replace(/\n/g, '').replace(/^\s*/, '').replace(/\s*$/, '');
+				//obj = xml.nodeValue;
+				// obj = xml.nodeValue.replace(/\n/g, '').replace(/^\s*/, '').replace(/\s*$/, '');
+				// obj = xml.nodeValue.replace(/>\n</g, '').replace(/^\s*/, '').replace(/\s*$/, '');
+				obj = clean(xml.nodeValue);
 			}
 
 			if (xml.hasChildNodes()) {
@@ -106,6 +113,144 @@ JMVC.extend('xmlparser', {
 
 					if (typeof(obj[nodeName]) == "undefined") {
 						!!tmp && (obj[nodeName] = tmp);
+					} else {
+
+						if (typeof(obj[nodeName].push) == "undefined") {
+							old = obj[nodeName];
+							obj[nodeName] = [];
+							obj[nodeName].push(old);
+						}
+						
+						!!tmp && obj[nodeName].push(tmp);
+					}
+				}
+			}
+			return obj;
+		};
+
+		/*
+		================================
+		1 	ELEMENT_NODE
+		2 	ATTRIBUTE_NODE
+		3 	TEXT_NODE
+		4 	CDATA_SECTION_NODE
+		5 	ENTITY_REFERENCE_NODE
+		6 	ENTITY_NODE
+		7 	PROCESSING_INSTRUCTION_NODE
+		8 	COMMENT_NODE
+		9 	DOCUMENT_NODE
+		10 	DOCUMENT_TYPE_NODE
+		11 	DOCUMENT_FRAGMENT_NODE
+		12 	NOTATION_NODE
+		================================
+		*/
+		this.jsonify = function (xml) {
+			var obj = {};
+			xml = xml ||that.pointerNode;
+
+			
+			// ELEMENT_NODE
+			// nodeName : the element name
+			// ndoeValue : null
+			// 	
+			if (xml.nodeType == 1) {
+				obj.name = 	xml.nodeName;
+				if (xml.attributes.length > 0) {
+				obj["@attributes"] = {};
+					for (var j = 0; j < xml.attributes.length; j++) {
+						var attribute = xml.attributes.item(j);
+						obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+					}
+				}
+			}
+
+			// node that 2 has been skipped since they are attributes, already done
+
+			// TEXT_NODE 3
+			// nodeName : #text
+			// nodeValue : content of node
+			// 
+			else if (xml.nodeType == 3) {
+				// obj.value = xml.nodeValue;
+				// obj = xml.nodeValue.replace(/\n/g, '').replace(/^\s*/, '').replace(/\s*$/, '');
+				obj = clean(xml.nodeValue);
+			// CDATA_SECTION_NODE 4
+			// nodeName : #cdata-section
+			// nodeValue : content of node
+			// 
+			} else if (xml.nodeType == 4) {
+				obj.value = clean(xml.nodeValue);
+
+			// ENTITY_REFERENCE 5
+			// nodeName : entity reference name
+			// nodeValue : null
+			// 
+			} else if (xml.nodeType == 5) {
+				obj.value = clean(xml.nodeValue);
+
+			// ENTITY_NODE 6
+			// nodeName : entity node
+			// nodeValue : null
+			// 
+			} else if (xml.nodeType == 6) {
+				obj.name = clean(xml.nodeName);
+			
+			// PROCESSING_INSTRUCTION_NODE 7
+			// nodeName : target
+			// nodeValue : content of node
+			// 
+			} else if (xml.nodeType == 7) {
+				obj.value = clean(xml.nodeValue);
+
+			// COMMENT_NODE 8
+			// nodeNAme : #comment
+			// nodeValue : comment text
+			// 
+			} else if (xml.nodeType == 8) {
+				obj.value = xml.data;
+
+			// DOCUMENT_NODE 9
+			// nodeName : #document
+			// nodeValue : null
+			// 
+			} else if (xml.nodeType == 9) {
+				obj.value = clean(xml.nodeValue);
+
+			// DOCUMENT_TYPE_NODE 10
+			// nodeName : doctype name
+			// nodeValue : null
+			// 
+			} else if (xml.nodeType == 10) {
+				obj.value = clean(xml.nodeValue);
+
+			// DOCUMENT_FRAGMENT_NODE 11
+			// nodeName : #document fragment
+			// nodeValue : null
+			// 
+			} else if (xml.nodeType == 11) {
+				obj.value = clean(xml.nodeValue);
+
+			// NOTATION_NODE 12
+			// nodeName : notation name
+			// nodeValue : null
+			// 	
+			} else if (xml.nodeType == 12) {
+				obj.value = clean(xml.nodeValue);
+
+			}
+
+			if (xml.hasChildNodes()) {
+				for(var i = 0; i < xml.childNodes.length; i++) {
+					var item = xml.childNodes.item(i),
+						nodeName = item.nodeName,
+						old, tmp;
+
+					tmp = this.jsonify(item);
+
+					if (typeof(obj[nodeName]) == "undefined") {
+
+						!!tmp && (obj[nodeName] = tmp);
+
 					} else {
 
 						if (typeof(obj[nodeName].push) == "undefined") {
