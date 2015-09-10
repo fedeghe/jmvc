5b170680b95c9f05d72746e63813b2a0206c41a6
fedeghe <fedeghe@gmail.com>
2015-09-10 23:30:22 +0200

    fixed parameter encoding

--- src/src/core/dispatched.js
+++ src/src/core/dispatched.js
@@ -65,14 +65,16 @@ dispatched = (function () {
         // splitting an empty string give an array with one empty string
         // 
         els = mid.search.substr(1).split('&');
+        
 
         for (i = 0, len = els.length; i < len; i += 1) {
             lab_val = els[i].split('=');
 
             // do not override extra path params
             // 
-            !params[lab_val[0]] && (params[lab_val[0]] = W.decodeURIComponent(lab_val[1]));
-            // !params[lab_val[0]] && (params[lab_val[0]] = lab_val[1]);
+            // !params[lab_val[0]] && (params[lab_val[0]] = decodeURIComponent(lab_val[1]));
+            // !params[lab_val[0]] && (params[lab_val[0]] = W.decodeURI(lab_val[1]));
+            !params[lab_val[0]] && (params[lab_val[0]] = lab_val[1]);
         }
     }
 
