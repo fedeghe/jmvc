099ea6a4f79d8bb12175224d6e28fcf63d7c17f2
fedeghe <fedeghe@gmail.com>
2015-11-16 23:37:03 +0100

    build

--- app/jmvc.js
+++ app/jmvc.js
@@ -6,12 +6,12 @@
  * JMVC : A pure Javascript MVC framework
  * ======================================
  *
- * @version :  3.7 (rev. 3) build: 4040
+ * @version :  3.7 (rev. 3) build: 4081
  * @copyright : 2015, Federico Ghedina <fedeghe@gmail.com>
  * @author : Federico Ghedina <fedeghe@gmail.com>
  * @url : http://www.jmvc.org
- * @file : built with Malta v.2.0.6 & a love heap
- *         glued with 41 files on 15/9/2015 at 9:25:54
+ * @file : built with Malta v.2.1.3 & a love heap
+ *         glued with 41 files on 27/10/2015 at 13:32:56
  *
  * All rights reserved.
  *
@@ -114,10 +114,10 @@
                 JMVC_REVIEW = '3',
             
                 // review (vars.json)
-                JMVC_DATE = '15/9/2015',
+                JMVC_DATE = '27/10/2015',
             
                 // review (vars.json)
-                JMVC_TIME = '9:25:54',
+                JMVC_TIME = '13:32:56',
             
                 // experimental (ignore it)
                 JMVC_PACKED = '', //'.min' 
@@ -1447,6 +1447,7 @@
                      */
                     attach : function (listener) {
                         this.listeners.push(listener);
+                        return this;
                     },
                     /**
                      * [ description]
@@ -1459,6 +1460,7 @@
                         while (i < l) {
                             this.listeners[i++](this.sender, args);
                         }
+                        return this;
                     }
                 };
                 return {
@@ -3052,6 +3054,8 @@
     			len = IEfuckIds.length,
     			i = 0;
     
+    		
+    
     		if (xdr && o.cors) {
     			xhr = new W.XDomainRequest();
     		} else {
@@ -3066,6 +3070,9 @@
     				!xhr && alert('No way to initialize XHR');
     			}
     		}
+    		// f i x
+    		// xhr && (xhr.withCredentials = true);
+    
     		return xhr;
     	},
     
@@ -3238,12 +3245,11 @@
     					// 
     					if (!hasFiles) {
     						
-    						_.io.setCookiesHeaders(xhr);
+    						// _.io.setCookiesHeaders(xhr);
     						_.io.setHeaders(xhr, type, limitSize);
     
     					} else {
                             // if files are in, no heders must be sent
-    						//_.io.setMultipartHeader(xhr);
     						_.io.setHeaders(xhr, 'json', limitSize);
                         }
     
@@ -3511,6 +3517,11 @@
         getType : function (o) {
             return ({}).toString.call(o).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
         },
+    
+        isMobile : (function () {
+            var ua = navigator.userAgent || navigator.vendor || window.opera;
+            return /android|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(ad|hone|od)|iris|kindle|lge |maemo|meego.+mobile|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino|playbook|silk/i.test(ua) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(ua.substr(0, 4));
+        })(),
     
         typeOf : function(o) {
             if (o === null) {
@@ -4353,16 +4364,17 @@
             afterFreeMem && JMVC.events.free(el);
     
             var parent;
+            
             typeof el === 'string' && (el = JMVC.dom.find(el));
     
             if (JMVC.util.isArray(el)) {
-                for (var i  = 0, l = el.length; i < l; i++) {
-                    JMVC.dom.remove(el[i]);
+                for (var i  = 0, l = el.length, r = true; i < l; i++) {
+                    r &= JMVC.dom.remove(el[i]);
                 }
-                return true;
+                return r;
             }
             parent = el.parentNode;
-            parent.removeChild(el);
+            parent && parent.removeChild(el);
             return parent;
         },
     
@@ -4416,6 +4428,25 @@
             JMVC.dom.insertAfter(coming, going);
             JMVC.dom.remove(going) && (coming.style.display = display);
         },
+    
+    
+    
+        /**
+         * [ description]
+         * @param  {[type]} elem        [description]
+         * @param  {[type]} addingClass [description]
+         * @return {[type]}             [description]
+         */
+        setClass : function (elem, classes) {
+            if (JMVC.util.isArray(elem)) {
+                for (var i = 0, l = elem.length; i < l; i++) {
+                    JMVC.dom.setClass(elem[i], classes);
+                }
+                return;
+            }    
+            elem.className = classes;
+        },
+    
     
         /**
          * Swap two existing nodes
@@ -4715,6 +4746,24 @@
     //
     // PUBLIC section
     JMVC.events = {
+    
+        avoidVerticalScroll : function () {
+            _.events.bind(window, 'touchmove', function (e) {
+                e.preventDefault();
+                _.events.kill(e);
+            });/*
+            _.events.bind(window,'touchstart', function() {
+                var top = el.scrollTop,
+                    totalScroll = el.scrollHeight,
+                    currentScroll = top + el.offsetHeight
+                if(top === 0) {
+                    el.scrollTop = 1
+                } else if(currentScroll === totalScroll) {
+                    el.scrollTop = top - 1
+                }
+            })*/
+        },
+    
         /**
          * [ description]
          * @param  {[type]}   el   [description]
@@ -5711,6 +5760,14 @@
          */
         beResponsive: function(on) {
             JMVC.dom[typeof on === 'undefined' || !!on ? 'addClass' : 'removeClass'](JMVC.WDB, 'resp');
+        },
+    
+        /**
+         * [fontAwesome description]
+         * @return {[type]} [description]
+         */
+        fontAwesome : function () {
+            JMVC.head.addStyle("https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css");
         },
     
         /**
@@ -6772,6 +6829,7 @@
          * @return {[type]}      [description]
          */
         reduce: function(o, fn) {
+            'use strict';
             var ret = '',
                 j;
             for (j in o) {
@@ -6795,6 +6853,7 @@
          * @returns cloned Object
          */
         _clone: function(obj) {
+            'use strict';
             var temp,
                 key;
             if (obj === null || typeof obj !== 'object') {
@@ -6810,6 +6869,7 @@
         },
         // http://stackoverflow.com/questions/728360/most-elegant-way-to-clone-a-javascript-object
         clone: function(obj) {
+            'use strict';
             var self = JMVC.object,
                 copy,
                 i, l;
@@ -6856,7 +6916,7 @@
          * @return {[type]}      [description]
          */
         compare: function(obj1, obj2, ret, i) {
-            
+            'use strict';
             if (typeof ret === 'undefined') {
                 ret = true;
             }
@@ -6890,6 +6950,7 @@
          * @return {[type]}        [description]
          */
         contains: function(obj, field) {
+            'use strict';
             return (typeof obj === 'object' && field in obj);
         },
     
@@ -6900,6 +6961,7 @@
          * @return {[type]}   [description]
          */
         digForKey : function (o, k) {
+            'use strict';
             return _.common.digFor('key', o, k);
         },
     
@@ -6910,6 +6972,7 @@
          * @return {[type]}   [description]
          */
         digForValue : function (o, k) {
+            'use strict';
             return _.common.digFor('value', o, k);
         },
     
@@ -6920,6 +6983,7 @@
          * @return {[type]}     [description]
          */
         extend: function(o, ext, force) {
+            'use strict';
             var obj = JMVC.object.clone(o),
                 j;
             for (j in ext) {
@@ -6935,6 +6999,7 @@
          * @return {[type]} [description]
          */
         fromQs : function () {
+            'use strict';
             var els = document.location.search.substr(1).split('&'),
                 i, len, tmp, out = {};
             
@@ -6947,7 +7012,12 @@
             }
             return out;
         },
-    
+        isEmpty : function (o) {
+            'use strict';
+            var i;
+            for (i in o) return false;
+            return true;
+        },
         /**
          * [ description]
          * @param  {[type]} obj1 [description]
@@ -6955,6 +7025,7 @@
          * @return {[type]}      [description]
          */
         jCompare: function(obj1, obj2) {
+            'use strict';
             return typeof JSON !== 'undefined' ? JSON.stringify(obj1) === JSON.stringify(obj2) : obj1 == obj2;
         },
         /**
@@ -6963,6 +7034,7 @@
          * @return {[type]}     [description]
          */
         keys: function(obj) {
+            'use strict';
             var res = [],
                 i;
             for (i in obj) {
@@ -6977,6 +7049,7 @@
          * @return {[type]}     [description]
          */
         order: function(obj) {
+            'use strict';
             var k = [],
                 i, j, l,
                 out = {};
@@ -6997,6 +7070,7 @@
          * @return {[type]}        [description]
          */
         orderBy : function (obj, key , versus) {
+            'use strict';
             function f(v) {
                 var tov = typeof v;
                 if (tov == 'number') {
@@ -7017,6 +7091,7 @@
          * @return {[type]}   [description]
          */
         toAttr: function(obj) {
+            'use strict';
             return _.object.reduce(obj, function(o, i) {
                 return ' ' + i + (o[i] ? '="' + o[i] + '"' : '');
             });
@@ -7027,6 +7102,7 @@
          * @return {[type]}   [description]
          */
         toCss: function(obj, straight) {
+            'use strict';
             return _.object.reduce(obj, function(ob, i) {
                 return !!straight ?
                     i + '{' + ob[i] + '} ' :
@@ -7043,6 +7119,7 @@
          * @return {[type]}   [description]
          */
         toQs: function(obj) {
+            'use strict';
             return _.object.reduce(obj, function(o, i, r) {
                 return ((r ? '&' : '?') + encodeURIComponent(i) + '=' + encodeURIComponent(o[i])).replace(/\'/g, '%27');
             });
@@ -7053,6 +7130,7 @@
          * @return {[type]}     [description]
          */
         toStr: function(obj) {
+            'use strict';
             return _.object.reduce(obj, function(o, i) {
                 return i + ':' + o[i] + ';';
             });
@@ -7063,6 +7141,7 @@
          * @return {[type]}     [description]
          */
         values: function(obj) {
+            'use strict';
             var res = [],
                 i;
             for (i in obj) {
