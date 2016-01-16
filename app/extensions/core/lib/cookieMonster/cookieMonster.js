a429218ec7e5072b9854763e388f53647046c907
fedeghe <fedeghe@gmail.com>
2016-01-16 23:45:42 +0100

    cookie monster extension

--- app/extensions/core/lib/cookieMonster/cookieMonster.js
+++ app/extensions/core/lib/cookieMonster/cookieMonster.js
@@ -1,6 +1,12 @@
 JMVC.core.widgzard && JMVC.hook({"onAfterRender" : function(cnt){
 	var Wid = JMVC.core.widgzard,
-		done = JMVC.cookie.get('EUlaw');
+		done = JMVC.cookie.get('EUlaw'),
+		trans = {
+			it : 'JMVC.org utilizza i cookie per garantire di fornirvi la migliore esperienza di navigazione. Se si continua senza modificare le impostazioni, supporremo che acconsentite a ricevere tutti i cookie da questo sito. Se si desidera modificare le preferenze, è possibile farlo seguendo le istruzioni <a href="http://www.aboutcookies.org/Default.aspx?page=1">qui</a>',
+			de : 'JMVC.org verwendet Cookies, um sicherzustellen, dass wir Ihnen die beste Erfahrung auf unserer Website. Wenn Sie ohne Änderung Ihrer Einstellungen fortfahren, werden wir annehmen, dass Sie glücklich, dass alle Cookies von dieser Website zu empfangen. Wenn Sie möchten, um Ihre Einstellungen Sie ändern, kann dies durch folgende Sie den Anweisungen <a href="http://www.aboutcookies.org/Default.aspx?page=1">hier</a> zu tun',
+			en : 'JMVC.org uses cookies to ensure that we give you the best experience on our website. If you continue without changing your settings, we will assume that you are happy to receive all cookies from this website. If you would like to change your preferences you may do so by following the instructions <a href="http://www.aboutcookies.org/Default.aspx?page=1">here</a>',
+			jp : 'JMVC.orgは、私たちはあなたの当社のウェブサイト上で最高の経験を与えることを保証するためにクッキーを使用しています。あなたは、設定を変更せずに続行した場合、我々はあなたが受け取るために満足していると仮定します。このサイトからすべてのクッキーをしました。あなたが設定を変更したい場合は、ここで<a href="http://www.aboutcookies.org/Default.aspx?page=1">の指示に</a>従うことによってそれを行うことが'
+		}
 
 	!done && Wid.render({
 		content : [{
@@ -11,61 +17,83 @@ JMVC.core.widgzard && JMVC.hook({"onAfterRender" : function(cnt){
 				padding : '10px',
 				margin : '1%',
 				left:'0px',
-				backgroundColor:'#ddd',
+				backgroundColor:'rgba(100,100,100,0.8)',
 				width:'98%',
-				zIndex:999
+				zIndex:999,
+				fontSize : '14px',
+				color:'white',
+				fontFamily:'Verdana, sans'
 			},
-			attrs : {'class':'round8 group'},
+			attrs : {'class':'round8 group respfixed'},
 			content : [{
 				attrs : {'class':'floatl'},
 				style : {
-					width:'95%'
+					width:'95%',
+					lineHeight:'25px'
 				},
-				html : 'This site uses cookies'
+				html : trans[JMVC.vars.currentlang]
 			},{
-				attrs : {'class':'floatr round8'},
+				attrs : {'class':'floatr'},
 				style : {
 					width:'5%',
-					textAlign : 'center',
-					color:'white',
-					backgroundColor:'black',
-					width : '20px',
-					height : '20px',
-					fontSize : '20px',
-					lineHeight:'18px',
-					cursor : 'pointer'
+					textAlign:'right'
 				},
-				html : '&times;',
-				cb : function () {
-					var self = this,
-						$elf = self.node;
-					JMVC.events.on($elf, 'click', function (){
-						JMVC.dom.remove(self.getNode('cookieEU').node);
-					});
-					self.done();
-				}
-			},{
-				attrs : {
-					'class':'floatr round8'
-				},
-				style : {
-					marginRight : '10px',
-					color:'green',
-					cursor : 'pointer'
-				},
-				html : 'ACCEPT',
-				
-				cb : function () {
-					var self = this,
-						$elf = self.node;
-					JMVC.events.on($elf, 'click', function (){
-						JMVC.dom.remove(self.getNode('cookieEU').node);
-						var oneYearMilliseconds = 1000 * 60 * 60 * 24 * 365;
-						JMVC.cookie.set('EUlaw', 'approved', oneYearMilliseconds, '/', document.location.host.replace('www.', ''));
-						done = true;
-					});
-					this.done();
-				}
+				content : [{
+					attrs : {'class':'round8 respfixed'},
+					style : {
+						display : 'inline-block',
+						textAlign : 'center',
+						color:'red',
+						backgroundColor:'white',
+						width : '24px',
+						height : '24px',
+						fontSize : '20px',
+						lineHeight:'18px',
+						padding:'2px',
+						cursor : 'pointer',
+						marginLeft : '5px'
+					},
+					html : '&times;',
+					cb : function () {
+						var self = this,
+							$elf = self.node;
+						JMVC.events.on($elf, 'click', function (){
+							JMVC.dom.remove(self.getNode('cookieEU').node);
+						});
+						self.done();
+					}
+				},{
+					attrs : {
+						'class': 'round8 respfixed'
+					},
+					style : {
+						display : 'inline-block',
+						textAlign : 'center',
+						color:'green',
+						backgroundColor:'white',
+						width : '24px',
+						height : '24px',
+						fontSize : '20px',
+						lineHeight:'18px',
+						cursor : 'pointer',
+						padding:'2px',
+						marginTop:'5px',
+						marginLeft : '5px'
+					},
+					html : '&#10003;',
+					
+					cb : function () {
+						var self = this,
+							$elf = self.node;
+						JMVC.events.on($elf, 'click', function (){
+							JMVC.dom.remove(self.getNode('cookieEU').node);
+							var oneYearMilliseconds = 1000 * 60 * 60 * 24 * 365;
+							JMVC.cookie.set('EUlaw', 'approved', oneYearMilliseconds, '/', document.location.host.replace('www.', ''));
+							done = true;
+						});
+						this.done();
+					}
+				}]
 			}]
 
 		}]
