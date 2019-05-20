027fd1cf3e5955cd4d476904806d9da541c236ac
fedeghe <fedeghe@gmail.com>
2019-05-20 22:22:30 +0200

    tabs style to less

--- source/app/extensions/widget/tabs/tabs.js
+++ source/app/extensions/widget/tabs/tabs.js
@@ -6,13 +6,14 @@
 JMVC.extend('tabs', {
 	counter : 0,
 	vert_css : false,
-	oriz_css : false,
-
-	init : function () {},
-
+    oriz_css : false,
+    
+    style: '/app/extensions/widget/tabs/style.css',
+    init: function () {
+        JMVC.head.addStyle(JMVC.vars.baseurl + JMVC.tabs.style, true);
+    },
 	tab : function (options) {
 		JMVC.tabs.counter += 1;
-		
 		options = options || {};
 
 		var bid = '_' + JMVC.tabs.counter,
@@ -32,16 +33,15 @@ JMVC.extend('tabs', {
 		};
 
 		this.current = null;
-
 		this.render = function (idcontainer, idul, show) {
-
 			//utility function to hide all tabs
 			function hideall () {
 				for (var i = 0, l = that.elements['div'].length; i < l; i += 1) {
 					JMVC.css.style(that.elements['div'][i], 'display', 'none');
 					JMVC.dom.switchClass(that.elements['li'][i], 'sel', 'unsel');
 				}
-			}
+            }
+            
 			function verticalize (str) {
 				return str.split('').join('<br />');
 			}
@@ -98,22 +98,13 @@ JMVC.extend('tabs', {
 				JMVC.dom.append(container, that.els2render[i]);
 			}
 
-			//add an end event to inject a css that take care of vertical dimensions for min-height of tabs
-
-			
-			
-			
-			
 			//set bindings
 			for(i = 0, l = that.elements['li'].length; i < l; i += 1){
 				JMVC.events.on(that.elements['li'][i], 'click', function(e){
-
 					//anchor up
 					document.location.hash = anchorName;
-
 					//hide all tabs
 					hideall();
-
 					var target = JMVC.events.eventTarget(e),
 						//show tab
 						id_orig = JMVC.dom.attr(target, 'id'),
@@ -131,80 +122,16 @@ JMVC.extend('tabs', {
 
 			JMVC.events.delay(function () {JMVC.tabs.fixheight(); }, 0);
 			return ids2return;
-		};
-		
-	},
+		};	
+    },
+    
 	fixheight : function () {
-		
 		var tc = JMVC.dom.find('.tabcontainer_v');
-
-	
 		JMVC.each(tc, function (el, i) {
 			var ulid = JMVC.dom.attr(el, 'data-ul'),
 				ul = JMVC.dom.find('#' + ulid),
 				h = ~~(JMVC.css.height(ul));
-			if (h) {
-				JMVC.css.style(el, 'minHeight', h + 'px');
-			}
+			h && JMVC.css.style(el, 'minHeight', h + 'px');
 		});
-	},
-	render : function () {
-		
-		JMVC.events.end(function () {
-			var border3 = '-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;',
-				colors = {
-					oriz : {
-						base : '#aaa',
-						selected : '#fff',
-						hover : '#ddd',
-						border : '#888',
-						text : '#000',
-						textselected : '#000',
-						texthover : '#000',
-						containerbg : '#fff',
-						textcontent : '#000' 
-					},
-					vert : {
-						base : '#aaa',
-						selected : '#fff',
-						hover : '#ddd',
-						border : '#888',
-						text : '#000',
-						textselected : '#000',
-						texthover : '#000',
-						containerbg : '#fff',
-						textcontent : '#000' 
-					}
-				},
-				sizes = {
-					oriz : {border : 2, round : 5},
-					vert : {border : 2, round : 5}
-				},
-				styles = {
-					'ul.tablabels_o':'overflow:hidden; list-style:none;margin:0px;padding:0px;',
-					'ul.tablabels_o li':'color:' + colors.oriz.text + ';float:left; line-height:1;font-size:100%;padding:0px;margin:0px;border:0px;background-color:' + colors.oriz.base + ';padding:5px; cursor:pointer;margin-left:5px;'+
-						'border:solid ' + sizes.oriz.border + 'px ' + colors.oriz.border + ';-moz-border-radius-topleft: ' + sizes.oriz.round + 'px;-moz-border-radius-topright:' + sizes.oriz.round + 'px;-moz-border-radius-bottomleft:0px;-moz-border-radius-bottomright:0px;'+
-						'-webkit-border-top-left-radius:' + sizes.oriz.round + 'px;-webkit-border-top-right-radius:' + sizes.oriz.round + 'px;-webkit-border-bottom-left-radius:0px;-webkit-border-bottom-right-radius:0px;'+
-						'border-top-left-radius:' + sizes.oriz.round + 'px;border-top-right-radius:' + sizes.oriz.round + 'px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;',
-					'ul.tablabels_o li:hover':'color:' + colors.oriz.texthover + ';background-color:' + colors.oriz.hover + ';',
-					'ul.tablabels_o li.sel':'color:' + colors.oriz.textselected + ';border:' + sizes.oriz.border + 'px solid ' + colors.oriz.border + ';border-bottom:none; background-color:' + colors.oriz.selected + '; z-index:10;position:relative;bottom:-1px; ',
-					'ul.tablabels_o li.unsel':'opacity:0.5',
-					'ul.tablabels_o li.unsel:hover':'opacity:0.8',
-					'div.tabcontainer_o' : 'color:' + colors.oriz.textcontent +';background-color:' + colors.oriz.containerbg +';' + border3 + 'line-height:1;font-size:100%;padding:0px;margin:0px;border:0px;border:' + sizes.oriz.border + 'px solid ' + colors.oriz.border + ';position:relative;top:-3px;z-index:5;padding:5px;',
-					///////////////////////////////////////////////////////////////////////
-					'ul.tablabels_v':'color:' + colors.vert.text + ';float:left;list-style:none;margin:0px;padding:0px;',
-					'ul.tablabels_v li':'display:block; text-align:right;line-height:1;font-size:100%;padding:0px;margin:0px;border:0px;background-color:' + colors.vert.base + ';padding:5px; cursor:pointer;margin-top:5px;left:' + sizes.vert.round + 'px;padding-right:' + 2 * sizes.vert.round + 'px;position:relative;' +
-							'border:solid ' + sizes.vert.border + 'px ' + colors.vert.border + ';-moz-border-radius-topleft: ' + sizes.vert.round + 'px;-moz-border-radius-topright:0px;-moz-border-radius-bottomleft:' + sizes.vert.round + 'px;-moz-border-radius-bottomright:0px;'+
-							'-webkit-border-top-left-radius:' + sizes.vert.round + 'px;-webkit-border-top-right-radius:0px;-webkit-border-bottom-left-radius:' + sizes.vert.round + 'px;-webkit-border-bottom-right-radius:0px;'+
-							'border-top-left-radius:' + sizes.vert.round + 'px;border-top-right-radius:0px;border-bottom-left-radius:' + sizes.vert.round + 'px;border-bottom-right-radius:0px;  border-right:none; left:1px',
-					'ul.tablabels_v li:hover':'color:' + colors.vert.texthover + ';background-color:' + colors.vert.hover + ';',
-					'ul.tablabels_v li.sel':'color:' + colors.vert.textselected + ';border:' + sizes.vert.border + 'px solid ' + colors.vert.border + ';border-right:none; background-color:' + colors.vert.selected + '; z-index:10;position:relative;left:1px',
-					'ul.tablabels_v li.unsel':'opacity:0.5',
-					'ul.tablabels_v li.unsel:hover':'opacity:0.8',
-					'div.tabcontainer_v' : 'color:' + colors.vert.textcontent +';background-color:' + colors.vert.containerbg +';' + border3 + 'min-height:250px;overflow:hidden; position:relative;line-height:1;font-size:100%;padding:0px;margin:0px;border:0px;border:' + sizes.vert.border + 'px solid ' + colors.vert.border + ';left:-1px;z-index:5; padding:5px;'
-				};
-			
-			JMVC.head.addStyle(JMVC.object.toCss(styles, true), true, true);			
-		});
-	}
+    }
 });
