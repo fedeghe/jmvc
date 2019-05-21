e54150daeef8d5a9f607fc7d6d9f2a1f3ae4591d
fedeghe <fedeghe@gmail.com>
2019-05-21 21:50:05 +0200

    small refactor canvaseditor start script

--- source/app/controllers/canvaseditor.js
+++ source/app/controllers/canvaseditor.js
@@ -1,25 +1,17 @@
+JMVC.require(
+    'canvas/editor/editor',
+    'core/lib/image/image'
+);
+
 JMVC.controllers.canvaseditor = function () {
     'use strict';
+    
+    var editorContainerId = 'canvasEditorContainerId';
+
     this.action_index = function (p) {
-
         JMVC.events.loadify(1000);
-
         JMVC.head.title('Canvas editor');
-
-        var screen_size,
-            editorContainerId = 'canvasEditorContainerId';
-
-        JMVC.require(
-            'canvas/editor/editor',
-            'core/lib/image/image'
-        );
-        
-        //
-        // a minimal wrapper view
         JMVC.getView('vacuum')
-
-            // set some style and the id
-            // 
             .set({
                 style : JMVC.object.toStr({
                     'font-family' : 'verdana',
@@ -32,20 +24,12 @@ JMVC.controllers.canvaseditor = function () {
                 }),
                 id : editorContainerId
             })
-
-            // render
-            //
             .render(function () {
-
-                // create initialize and start the editor
-                //
                 new JMVC.canvas.Editor({
                     node : JMVC.dom.find('#' + editorContainerId)
                     //,width: 300
                     //,height: 400
                 });
             });
-            
-        //window.setTimeout(function() {JMVC.screen.launchFullscreen();}, 5000);
     };
 };
