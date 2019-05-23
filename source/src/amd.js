7a49735f17b002774c3edfd67234aa816d679c64
fedeghe <fedeghe@gmail.com>
2019-05-23 23:09:08 +0200

    lints on

--- source/src/amd.js
+++ source/src/amd.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
 // AMD friendly, if shutup mode
-// 
+//
 if (typeof JMVCshut !== 'undefined') {
     (function (m) {
-        typeof define === "function"
-        && define.amd
-        && define(function () {return JMVC; });
+        typeof define === 'function' &&
+            define.amd &&
+            define(function () { return JMVC; });
     })(JMVC);
 }
