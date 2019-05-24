e4b9c7b593980ace1a7b843efbc31761d53aea70
fedeghe <fedeghe@gmail.com>
2019-05-24 11:30:36 +0200

    and on

--- source/src/modules/minishim.js
+++ source/src/modules/minishim.js
@@ -1,18 +1,18 @@
+/* eslint-disable no-undef */
 _.shimthags = 'canvas,audio,video,source,track,embed,datalist,keygen,output,header,nav,section,main,article,aside,footer,details,summary,figure,figcaption,mark,time,bdi,wbr,dialog,meter,progress,ruby,rt,rp';
 
 // http://stackoverflow.com/questions/13897173/how-to-detect-if-an-html5-valid-doctype-is-present-in-the-document-using-javascr
 if (document.doctype !== null) {
-
     var node = document.doctype,
-        doctype_string = "<!DOCTYPE " +
+        doctypeString = '<!DOCTYPE ' +
             node.name +
             (node.publicId ? ' PUBLIC"' + node.publicId + '"' : '') +
             (!node.publicId && node.systemId ? ' SYSTEM' : '') +
             (node.systemId ? ' "' + node.systemId + '"' : '') +
-        ">", t, l;
+        '>', t, l;
 
-    if ('<!DOCTYPE html>' !== doctype_string) {
-        t = _.shimthags.split(','),
+    if (doctypeString !== '<!DOCTYPE html>') {
+        t = _.shimthags.split(',');
         l = t.length;
         while (l--) {
             document.createElement(t[l]);
