4cc90417d97ae945b62e85e4509ce2a30490328c
fedeghe <fedeghe@gmail.com>
2019-05-24 08:19:23 +0200

    more lintz

--- source/src/modules/bom.js
+++ source/src/modules/bom.js
@@ -1,12 +1,11 @@
 JMVC.bom = {
-    document : JMVC.WD,
-    frames : JMVC.W.frames,
-    history : JMVC.W.history,
-    location : JMVC.W.location,
-    navigator : JMVC.W.navigator,
-
+    document: JMVC.WD,
+    frames: JMVC.W.frames,
+    history: JMVC.W.history,
+    location: JMVC.W.location,
+    navigator: JMVC.W.navigator,
     // here or in JMVC.head? bah
-    qs : function (qs) {
+    qs: function (qs) {
         JMVC.bom.location.search = JMVC.object.toQs(qs);
-    } 
+    }
 };
--- source/src/modules/cookie.js
+++ source/src/modules/cookie.js
@@ -1,128 +1,128 @@
 // type : LIB
-// 
+//
 _.cookie = {};
 
-JMVC.cookie =  {
+JMVC.cookie = {
+    enabled: true,
+    cookie_nocookiesaround: false,
+    initCheck: function () {
+        'use strict';
+        return JMVC.W.navigator.cookieEnabled;
+    },
+    /**
+     * [set description]
+     * @param {[type]} name    [description]
+     * @param {[type]} value   [description]
+     * @param {[type]} expires [description]
+     * @param {[type]} path    [description]
+     * @param {[type]} domain  [description]
+     * @param {[type]} secure  [description]
+     */
 
-	enabled : true,
+    set: function (name, value, expires, path, domain, secure) {
+        'use strict';
+        if (!JMVC.cookie.enabled) return false;
+        this.cookie_nocookiesaround = false;
+        var today = new Date(),
+            expiresDate = new Date(today.getTime() + expires);
+        // expires && (expires = expires * 1000 * 60 * 60 * 24);
+        JMVC.WD.cookie = name +
+            '=' + JMVC.W.escape(value) +
+            (expires ? ';expires=' + expiresDate.toGMTString() : '') +
+            (path ? ';path=' + path : '') +
+            (domain ? ';domain=' + domain : '') +
+            (secure ? ';secure' : '');
+        return true;
+    },
 
-	cookie_nocookiesaround : false,
+    /**
+     * [get description]
+     * @param  {[type]} check_name [description]
+     * @return {[type]}            [description]
+     */
 
-	initCheck : function () {
-		"use strict";
-		return JMVC.W.navigator.cookieEnabled;
-	},
-	
-	/**
-	 * [set description]
-	 * @param {[type]} name    [description]
-	 * @param {[type]} value   [description]
-	 * @param {[type]} expires [description]
-	 * @param {[type]} path    [description]
-	 * @param {[type]} domain  [description]
-	 * @param {[type]} secure  [description]
-	 */
-	set : function (name, value, expires, path, domain, secure) {
-		"use strict";
-		if (!JMVC.cookie.enabled) return false;
-		this.cookie_nocookiesaround = false;
-		var today = new Date(),
-			expires_date = new Date(today.getTime() + expires);
-		// expires && (expires = expires * 1000 * 60 * 60 * 24);
-		JMVC.WD.cookie = name +
-			"=" + JMVC.W.escape(value) +
-			(expires ? ";expires=" + expires_date.toGMTString() : "") +
-			(path ? ";path=" + path : "") +
-			(domain ? ";domain=" + domain : "") +
-			(secure ? ";secure" : "");
-		return true;
-	},
+    get: function (checkName) {
+        'use strict';
+        var allCookies = JMVC.WD.cookie.split(';'),
+            tempCookie = '',
+            cookieName = '',
+            cookieValue = '',
+            cookieFound = false,
+            i = 0,
+            l = allCookies.length;
 
-	/**
-	 * [get description]
-	 * @param  {[type]} check_name [description]
-	 * @return {[type]}            [description]
-	 */
-	get : function (check_name) {
-		"use strict";
-		var a_all_cookies = JMVC.WD.cookie.split(';'),
-			a_temp_cookie = '',
-			cookie_name = '',
-			cookie_value = '',
-			b_cookie_found = false,
-			i = 0,
-			l = a_all_cookies.length;
-		if (!JMVC.cookie.enabled) return false;
-		for (null; i < l; i += 1) {
-			a_temp_cookie = a_all_cookies[i].split('=');
-			cookie_name = a_temp_cookie[0].replace(/^\s+|\s+$/g, '');
-			if (cookie_name === check_name) {
-				b_cookie_found = true;
-				a_temp_cookie.length > 1 && (cookie_value = JMVC.W.unescape(a_temp_cookie[1].replace(/^\s+|\s+$/g, '')));
-				return cookie_value;
-			}
-			a_temp_cookie = null;
-			cookie_name = '';
-		}
-		return b_cookie_found;
-	},
+        if (!JMVC.cookie.enabled) return false;
+        for (null; i < l; i += 1) {
+            tempCookie = allCookies[i].split('=');
+            cookieName = tempCookie[0].replace(/^\s+|\s+$/g, '');
+            if (cookieName === checkName) {
+                cookieFound = true;
+                tempCookie.length > 1 && (cookieValue = JMVC.W.unescape(tempCookie[1].replace(/^\s+|\s+$/g, '')));
+                return cookieValue;
+            }
+            tempCookie = null;
+            cookieName = '';
+        }
+        return cookieFound;
+    },
+    /**
+     * [del description]
+     * @param  {[type]} name   [description]
+     * @param  {[type]} path   [description]
+     * @param  {[type]} domain [description]
+     * @return {[type]}        [description]
+     */
+    del: function (name, path, domain) {
+        'use strict';
+        if (!JMVC.cookie.enabled) return false;
+        var ret = false;
+        if (this.get(name)) {
+            JMVC.WD.cookie = name + '=' +
+                (path ? ';path=' + path : '') +
+                (domain ? ';domain=' + domain : '') +
+                ';expires=Thu, 01-Jan-1970 00:00:01 GMT';
+            ret = true;
+        }
+        return ret;
+    },
 
-	/**
-	 * [del description]
-	 * @param  {[type]} name   [description]
-	 * @param  {[type]} path   [description]
-	 * @param  {[type]} domain [description]
-	 * @return {[type]}        [description]
-	 */
-	del : function (name, path, domain) {
-		"use strict";
-		if (!JMVC.cookie.enabled) return false;
-		var ret = false;
-		if (this.get(name)) {
-			JMVC.WD.cookie = name + "=" + (path ? ";path=" + path : "") + (domain ? ";domain=" + domain : "") + ";expires=Thu, 01-Jan-1970 00:00:01 GMT";
-			ret = true;
-		}
-		return ret;
-	},
+    /**
+     * [delall description]
+     * @return {[type]} [description]
+     */
+    delall: function () {
+        'use strict';
+        if (!JMVC.cookie.enabled) return false;
+        var thecookie = JMVC.WD.cookie.split(';'),
+            i = 0,
+            l = thecookie.length,
+            nome;
+        for (null; i < l; i += 1) {
+            nome = thecookie[i].split('=');
+            this.del(nome[0], false, false);
+        }
+        this.cookie_nocookiesaround = true;
+        return true;
+    },
 
-	/**
-	 * [delall description]
-	 * @return {[type]} [description]
-	 */
-	delall : function () {
-		"use strict";
-		if (!JMVC.cookie.enabled) return false;
-		var thecookie = JMVC.WD.cookie.split(";"),
-			i = 0,
-			l = thecookie.length,
-			nome;
-		for (null; i < l; i += 1) {
-			nome = thecookie[i].split('=');
-			this.del(nome[0], false, false);
-		}
-		this.cookie_nocookiesaround = true;
-		return true;
-	},
-
-	/**
-	 * [getall description]
-	 * @return {[type]} [description]
-	 */
-	getall : function () {
-		"use strict";
-		if (!JMVC.cookie.enabled) return false;
-		if (JMVC.WD.cookie === '') {
-			return [];
-		}
-		return this.cookie_nocookiesaround ?
-			[]
-			:
-			JMVC.each(
-				JMVC.WD.cookie.split(';'),
-				function (i) {
-					var t = i.split('=');
-					return {name : t[0], value : t[1]};
-				}
-			);
-	}
+    /**
+     * [getall description]
+     * @return {[type]} [description]
+     */
+    getall: function () {
+        'use strict';
+        if (!JMVC.cookie.enabled) return false;
+        if (JMVC.WD.cookie === '') {
+            return [];
+        }
+        return this.cookie_nocookiesaround
+            ? []
+            : JMVC.each(
+                JMVC.WD.cookie.split(';'),
+                function (i) {
+                    var t = i.split('=');
+                    return { name: t[0], value: t[1] };
+                }
+            );
+    }
 };
