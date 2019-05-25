e59e0ff42a25a26ab96f5e9df75c25e3c35ca70f
fedeghe <fedeghe@gmail.com>
2019-05-25 22:41:23 +0200

    eslint fwd

--- source/app/components/contactBasic.js
+++ source/app/components/contactBasic.js
@@ -1,78 +1,79 @@
+// eslint-disable-next-line no-unused-vars
 var ___ = {
-    tag: "li",
-    style : {
-        height:"inherit",
-        width:"inherit",
-        backgroundColor : "#PARAM{bgcolor|white}",
+    tag: 'li',
+    style: {
+        height: 'inherit',
+        width: 'inherit',
+        backgroundColor: '#PARAM{bgcolor|white}'
     },
-    attrs : {'class':'round8'},
-    content : [{
+    attrs: { 'class': 'round8' },
+    content: [{
 
-        attrs : {
-            "class" : "innerwrap"
+        attrs: {
+            'class': 'innerwrap'
         },
-        style : {
-            height:"inherit",
-            width:"100%"
+        style: {
+            height: 'inherit',
+            width: '100%'
         },
         data: {
-            submitUrl: "#PARAM{submitUrl}",
+            submitUrl: '#PARAM{submitUrl}',
             id: JMVC.util.uniqueid + ''
         },
         content: [{
-            tag: "form",
+            tag: 'form',
             attrs: {
-                action: "#PARAM{submitUrl}",
-                method: "post",
-                onSubmit: "return false;"
+                action: '#PARAM{submitUrl}',
+                method: 'post',
+                onSubmit: 'return false;'
             },
             content: [{
-                tag:"div",
+                tag: 'div',
                 content: [{
-                    tag: "label",
-                    attrs: {"class":"blk"},
-                    html: "Name"
-                },{
-                    tag: "input",
+                    tag: 'label',
+                    attrs: { 'class': 'blk' },
+                    html: 'Name'
+                }, {
+                    tag: 'input',
                     attrs: {
-                        type: "text"
+                        type: 'text'
                     },
                     cb: function () {
                         this.node.id = this.climb(3).data.id + '_name';
                         this.done();
                     }
                 }]
-            },{
-                tag: "div",
+            }, {
+                tag: 'div',
                 content: [{
-                    tag: "label",
-                    attrs: {"class":"blk"},
-                    html: "Email"
-                },{
-                    tag: "input",
+                    tag: 'label',
+                    attrs: { 'class': 'blk' },
+                    html: 'Email'
+                }, {
+                    tag: 'input',
                     attrs: {
-                        type: "text"
+                        type: 'text'
                     },
                     cb: function () {
                         this.node.id = this.climb(3).data.id + '_email';
                         this.done();
                     }
                 }]
-            },{
-                tag: "div",
+            }, {
+                tag: 'div',
                 content: [{
-                    tag: "input",
+                    tag: 'input',
                     attrs: {
-                        "class":"blk",
-                        type : "submit",
-                        value : "Submit"
+                        'class': 'blk',
+                        type: 'submit',
+                        value: 'Submit'
                     }
                 }]
             }],
-            cb : function () {
+            cb: function () {
                 var self = this,
                     valid = function (f) {
-                        var ret = f.value != '';
+                        var ret = f.value !== '';
                         if (ret) {
                             f.style.border = '';
                         } else {
@@ -81,36 +82,32 @@ var ___ = {
                         return ret;
                     };
 
-
-
                 this.node.addEventListener('submit', function () {
-
                     var nameF = document.getElementById(self.parent.data.id + '_name'),
                         emailF = document.getElementById(self.parent.data.id + '_email');
 
-
-                    (valid(nameF) & valid(emailF))
-                    &&
-                    JMVC.io.post(self.parent.data.submitUrl, function (r) {
-
+                    valid(nameF) &&
+                    valid(emailF) &&
+                    JMVC.io.post(
+                        self.parent.data.submitUrl,
+                        function (r) {
                             JMVC.core.widgzard.render({
-                                target : self.parent.node,
-                                content : [{
-                                    html : r,
-                                    style : {color:'red', padding:'20px', textAlign:'center'}
+                                target: self.parent.node,
+                                content: [{
+                                    html: r,
+                                    style: { color: 'red', padding: '20px', textAlign: 'center' }
                                 }]
                             }, true);
-
                         },
                         false,
                         {
-                            name : nameF.value,
-                            email : emailF.value,
+                            name: nameF.value,
+                            email: emailF.value
                         }
                     );
-                },false);
+                }, false);
                 this.done();
             }
         }]
     }]
-}
+};
