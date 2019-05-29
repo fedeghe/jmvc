11e2043dd2a0ecce626f246b0337c244883bdea3
fedeghe <fedeghe@gmail.com>
2019-05-29 23:18:39 +0200

    some

--- source/app/extensions/vendors/gravatar/gravatar.js
+++ source/app/extensions/vendors/gravatar/gravatar.js
@@ -1,35 +1,27 @@
 JMVC.require('core/extendedUtils');
 JMVC.extend('vendors/gravatar', {
-
-    baseUrl : 'http://www.gravatar.com',
-
-    getInfo : function (email) {
-        var md5 = JMVC.utils.md5(email),
-            url = JMVC.string.replaceAll(
-                '%baseurl%/%md5%.json',
-                {
-                    baseurl : JMVC.vendors.gravatar.baseUrl,
-                    md5 : md5
-                }
-            );
-        // http://www.gravatar.com/d3a4c55b1dd4dc0f9728b19a1e537b52.json
-        return url;
+    baseUrl: 'http://www.gravatar.com',
+    getInfo: function (email) {
+        var md5 = JMVC.utils.md5(email);
+        return JMVC.string.replaceAll(
+            '%baseurl%/%md5%.json', {
+                baseurl: JMVC.vendors.gravatar.baseUrl,
+                md5: md5
+            }
+        );
     },
-    getGravatar : function (email, size) {
-        size = ~~size || 100; 
-        var md5 = JMVC.utils.md5(email),
-            url = JMVC.string.replaceAll(
-                '%baseurl%/avatar/%md5%.jpg?s=%size%',
-                {
-                    baseurl : JMVC.vendors.gravatar.baseUrl,
-                    md5 : md5,
-                    size : size
-                }
-            );
-
-        //http://www.gravatar.com/avatar/d3a4c55b1dd4dc0f9728b19a1e537b52.jpg?s=200
-        return url;
+    getGravatar: function (email, size) {
+        size = ~~size || 100;
+        var md5 = JMVC.utils.md5(email);
+        return JMVC.string.replaceAll(
+            '%baseurl%/avatar/%md5%.jpg?s=%size%',
+            {
+                baseurl: JMVC.vendors.gravatar.baseUrl,
+                md5: md5,
+                size: size
+            }
+        );
     }
-})
+});
 
-//http://en.gravatar.com/avatar/d3a4c55b1dd4dc0f9728b19a1e537b52.jpg?s=200
+// http://en.gravatar.com/avatar/d3a4c55b1dd4dc0f9728b19a1e537b52.jpg?s=200
