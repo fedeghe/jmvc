ce35d0abe5b66327452f798eb32cddb206ca822c
fedeghe <fedeghe@gmail.com>
2019-05-30 10:44:25 +0200

    lintz

--- source/app/extensions/widget/gauge/gauge.js
+++ source/app/extensions/widget/gauge/gauge.js
@@ -3,26 +3,24 @@ JMVC.head.addStyle(JMVC.vars.baseurl + '/app/extensions/widget/gauge/gauge.css',
 JMVC.nsCheck('JMVC.widget');
 
 JMVC.widget.Gauge = function () {
-    this.node = JMVC.dom.create('div', {'class':'JMVCgauge'});
-    var wrap = JMVC.dom.add(this.node, 'div', {'class':'JMVCgaugeWrap'});
-    this.bar = JMVC.dom.add(wrap, 'div', {'class':'JMVCgaugeBar'});
+    this.node = JMVC.dom.create('div', { 'class': 'JMVCgauge' });
+    var wrap = JMVC.dom.add(this.node, 'div', { 'class': 'JMVCgaugeWrap' });
+    this.bar = JMVC.dom.add(wrap, 'div', { 'class': 'JMVCgaugeBar' });
     this.val = 0;
 };
 
 JMVC.widget.Gauge.prototype.set = function (v) {
     this.val = (v % 101) || this.val;
-    JMVC.css.style(this.bar, {width : this.val + '%'});
+    JMVC.css.style(this.bar, { width: this.val + '%' });
     JMVC.dom.html(this.bar, this.val + ' %');
-}
+};
 
 JMVC.widget.Gauge.prototype.get = function () {
     return ~~this.val;
-}
+};
 
 JMVC.widget.Gauge.prototype.render = function (p) {
-    return (p && JMVC.dom.isNode(p)) ?
-        JMVC.dom.html(p, this.node)
-        :
-        this.node;
-}
-
+    return (p && JMVC.dom.isNode(p))
+        ? JMVC.dom.html(p, this.node)
+        : this.node;
+};
