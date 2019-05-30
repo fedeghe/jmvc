93e3745c37d75b72947a96221cc7b3226a071f3f
fedeghe <fedeghe@gmail.com>
2019-05-30 11:33:23 +0200

    lintz

--- source/app/extensions/widget/snow/snow.js
+++ source/app/extensions/widget/snow/snow.js
@@ -7,183 +7,168 @@ JMVC.snow.jSnow(document.body, {str:'M@7730', mode:'rot'});
 JMVC.require('core/screen/screen');
 
 JMVC.extend('snow', {
+    jSnow: function (target, options) {
+        options = options || {};
+        // number of flake sources
+        var strenght = options.stren || 1,
+            // what snows down
+            snowflake = options.str || 'flakes',
+            // how many flakes per second
+            // ti = options.ti || 1,
+            // flake size
+            // size = options.size || 80,
+            color = options.color || 'white',
+            bodyDim = target === 'body'
+                ? JMVC.screen.getViewportSize()
+                : {
+                    width: JMVC.num.getNum(JMVC.css.width(target)),
+                    height: JMVC.num.getNum(JMVC.css.height(target))
+                },
 
+            // x = false,
+            speeds = [5, 20, 21, 22, 23],
+            sizes = [2, 20],
+            phases = [20, 40],
+            amplitudes = [10, 20],
+            directions = [-5, 5],
+            opacities = [0.1, 1],
 
-	jSnow : function(target, options){
+            bodyWidth = bodyDim.width - 20,
+            bodyHeight = bodyDim.height - 20,
 
-		options = options || {};
+            // count = 0,
 
-		//number of flake sources
-		var strenght = options.stren || 1,
-			//what snows down
-			snowflake = options.str || 'flakes',
-			//how many flakes per second
-			ti = options.ti || 1,
-			//flake size
-			size = options.size || 80,
-			color = options.color || 'white',
+            containerId = 'snow_container',
+            // snowflakez = {},
 
-			body_dim = target == "body" ? 
-					JMVC.screen.getViewportSize()
-					:
-					{width : JMVC.num.getNum(JMVC.css.width(target), height : JMVC.num.getNum(JMVC.css.height(target))},
-			
-			x = false,
-			speeds = [ 5, 20, 21, 22, 23],
-			sizes = [2,20],
-			phases = [20,40],
-			amplitudes = [10,20],
-			directions = [-5,5],
-			opacities = [0.1,1],
+            baseDiv = JMVC.dom.create('div', {
+                id: containerId,
+                style: 'position:absolute; display:block; top:0px; left:0px;'
+            }),
 
-			body_width = body_dim.width - 20,
-			body_height = body_dim.height - 20,
+            // flake = JMVC.dom.create('span'),
+            rotation = (snowflake === 'flakes'),
+            rotString = options.mode === 'rot',
+            rotFlakes = ['*', '#', '+'],
+            rotIncr = [-1, 1],
 
-			count = 0,
+            Snow = function () {
+                JMVC.dom.append(JMVC.dom.find(target), baseDiv);
+                this.flake_sources = [];
 
-			container_id = 'snow_container',
-			snowflakez = { },
+                this.start = function () {
+                    for (var i = 0, l = this.flake_sources.length; i < l; i += 1) {
+                        this.flake_sources[i].append_flake();
+                    }
+                };
+                this.init = function () {
+                    for (var i = 0; i < strenght; i++) {
+                        this.flake_sources.push(new FlakeSource(this));
+                    }
+                    this.start();
+                };
+                this.init();
+            },
 
-			base_div = JMVC.dom.create('div', {id:container_id, style:"position:absolute;display:block;top:0px; left:0px;"}),
+            FlakeSource = function (parent) {
+                var that = this,
+                    rn = JMVC.util.rand(500, 1000);
 
-			flake = JMVC.dom.create('span'),
-			rotation = (snowflake ==='flakes'),
-			rot_string = options.mode=='rot',
-			rot_flakes = new Array('*','#','+'),
-			rot_incr = new Array(-1,1),
+                this.append_flake = function () {
+                    var opa = JMVC.util.rand(opacities[0] * 10, opacities[1] * 10) / 10,
+                        siz = Math.round(sizes[0] + opa * (sizes[1] - sizes[0])),
+                        speed = speeds[JMVC.util.rand(0, speeds.length - 1)],
+                        f = new Fla({
+                            parent: this,
+                            position: JMVC.util.rand(10, bodyWidth - 10),
+                            // speed : parent.utility.rand(speeds[0], speeds[1]),
+                            speed: speed / 5,
+                            phase: JMVC.util.rand(phases[0], phases[1]),
+                            amplitude: JMVC.util.rand(amplitudes[0], amplitudes[1]),
+                            direction: JMVC.util.rand(directions[0], directions[1]),
 
-			Snow = function(){
+                            // opacity : parent.utility.rand(opacities[0]*10, opacities[1]*10) /10,
+                            // size : parent.utility.rand(sizes[0], sizes[1]),
+                            opacity: opa,
+                            size: siz,
 
-				JMVC.dom.append(JMVC.dom.find(target), base_div);
+                            mainobj: parent,
+                            rot: 0,
+                            rotIncr: rotIncr[JMVC.util.rand(0, 1)],
+                            rot_fact: JMVC.util.rand(1, 20)
+                        });
+                    window.setTimeout(
+                        function () {
+                            that.append_flake();
+                            window.setTimeout(
+                                function () {
+                                    JMVC.dom.append(baseDiv, f.el);
+                                    f.stylize();
+                                    f.animator.animate();
+                                },
+                                rn
+                            );
+                        },
+                        rn
+                    );
+                };
+            },
 
-				this.flake_sources = [];
+            Fla = function (opts) {
+                var that = this;
+                this.parent = opts.parent;
 
-				this.start = function () {
-					for (var i = 0, l = this.flake_sources.length; i < l; i += 1) {
-						this.flake_sources[i].append_flake();
-					}
-				};
+                this.el = JMVC.dom.create('span');
+                this.stylize = function () {
+                    JMVC.css.style(that.el, {
+                        color: color,
+                        fontSize: opts.size + 'px',
+                        position: 'absolute',
+                        opacity: opts.opacity,
+                        top: '1px',
+                        left: opts.position + 'px',
+                        zIndex: 500
+                    });
+                    JMVC.dom.html(that.el, rotation ? rotFlakes[JMVC.util.rand(0, rotFlakes.length - 1)] : snowflake);
+                };
+                this.animator = new Animator(this.el, opts);
+            },
 
-				this.init = function(){
-					for(var i  =0; i<strenght; i++){
-						this.flake_sources.push(new Flake_source(this));
-					}
-					this.start();
-				}
-				this.init();
-			},
+            path = function (y, amp, phase) {
+                return Math.ceil(amp * Math.cos(y / phase));
+            },
 
-			Flake_source = function (parent) {
+            move = function (obj, x, y) {
+                JMVC.css.style(obj, { left: x + 'px', top: y + 'px' });
+            },
 
-				var that = this,
-					rn = JMVC.util.rand(500,1000);
+            Animator = function (f, opts) {
+                var that = this;
+                this.animate = function () {
+                    // var top = f.css('top');
+                    var y = parseInt(JMVC.css.style(f, 'top'), 10),
+                        t, p;
+                    opts.position += opts.direction / 8;
+                    if (y < bodyHeight - opts.size) {
+                        p = opts.position + path(y + 1, opts.amplitude, opts.phase);
+                        if (p < 0 || p > bodyWidth - opts.size) {
+                            clearTimeout(t);
+                            JMVC.dom.remove(f);
+                            return;
+                        }
 
-				this.append_flake = function(){
-					var opa = JMVC.util.rand(opacities[0]*10, opacities[1]*10) /10;
-					var siz = Math.round(sizes[0] + opa*(sizes[1]-sizes[0]));
-					//var speed = Math.round(sizes[0] + opa*(sizes[1]-sizes[0]));
-					var speed =  speeds[JMVC.util.rand(0, speeds.length - 1)];
-					
-					var f = new fla({
-						parent:this,
-						position: JMVC.util.rand(10, body_width-10),
-						//speed : parent.utility.rand(speeds[0], speeds[1]),
-						speed : speed/5,
-						phase : JMVC.util.rand(phases[0], phases[1]),
-						amplitude : JMVC.util.rand(amplitudes[0], amplitudes[1]),
-						direction : JMVC.util.rand(directions[0], directions[1]),
+                        if (rotation || rotString) {
+                            opts.rot = (opts.rot + opts.rotIncr * opts.rot_fact) % 360;
 
-						//opacity : parent.utility.rand(opacities[0]*10, opacities[1]*10) /10,
-						//size : parent.utility.rand(sizes[0], sizes[1]),
-						opacity:opa,
-						size:siz,
-
-						mainobj: parent,
-						rot : 0,
-						rot_incr : rot_incr[JMVC.util.rand(0, 1)],
-						rot_fact : JMVC.util.rand(1, 20)
-					});
-					window.setTimeout(
-						function(){
-							that.append_flake();
-							window.setTimeout(
-								function(){
-									JMVC.dom.append(base_div, f.el);
-									f.stylize();
-									f.animator.animate();
-								},
-								rn
-							);
-						},
-						rn
-					);
-				};
-			},
-
-			fla = function (opts) {
-				var that = this;
-				this.parent = opts.parent;
-
-				this.el = JMVC.dom.create('span');
-				this.stylize = function () {
-					JMVC.css.style(that.el, {
-						color : color,
-						fontSize : opts.size + 'px',
-						position : 'absolute',
-						opacity : opts.opacity,
-						top : '1px',
-						left : opts.position + 'px',
-						zIndex : 500
-					})
-					JMVC.dom.html(that.el, rotation? rot_flakes[JMVC.util.rand(0, rot_flakes.length - 1)] : snowflake);
-				};
-				this.animator = new animator(this.el, opts);
-			},
-
-			path = function (y, amp, phase) {
-				return Math.ceil(amp * Math.cos(y / phase));
-			},
-
-			move = function (obj, x, y) {
-				JMVC.css.style(obj, {left : x + 'px', top : y + 'px' });
-			},
-
-			animator = function (f, opts) {
-				var that = this;
-
-				this.animate = function () {
-					//var top = f.css('top');
-					var y = parseInt(JMVC.css.style(f, 'top'), 10);
-
-
-
-					opts.position += opts.direction / 8;
-					var t;
-					if (y < body_height-opts.size) {
-						var p = opts.position + path(y + 1, opts.amplitude, opts.phase)
-						if (p < 0 || p > body_width - opts.size) {
-							clearTimeout(t);
-							JMVC.dom.remove(f);
-							return;
-						}
-
-						if (rotation || rot_string) {
-							opts.rot=(opts.rot+opts.rot_incr*opts.rot_fact)%360;
-
-							JMVC.css.rotate(f, opts.rot);
-						}
-						move(f, p, y + opts.speed );
-						t = window.setTimeout( function () {that.animate(); }, 40 );
-					} else {
-						JMVC.dom.remove(f);
-					}
-
-				}
-
-			};
-
-		new Snow();
-	}
-}); //end extend
-	
-	
+                            JMVC.css.rotate(f, opts.rot);
+                        }
+                        move(f, p, y + opts.speed);
+                        t = window.setTimeout(function () { that.animate(); }, 40);
+                    } else {
+                        JMVC.dom.remove(f);
+                    }
+                };
+            };
+        return new Snow();
+    }
+}); // end extend
--- source/app/extensions/widget/snow/snow/snow.js
+++ source/app/extensions/widget/snow/snow/snow.js
@@ -1,85 +1,81 @@
 JMVC.head.addStyle(JMVC.vars.baseurl + '/app/extensions/widget/snow/snow.css', true);
 
-JMVC.require('core/screen/screen');	
+JMVC.require('core/screen/screen');
 //
 JMVC.extend('snow', {
-	vars : {
-		flakes : [],
-		l : 0,
-		trg : null
-	},
-	
-	start : function (trg, opts) {
+    vars: {
+        flakes: [],
+        l: 0,
+        trg: null
+    },
+    start: function (trg, opts) {
+        JMVC.snow.vars.trg = JMVC.dom.find(trg);
+        JMVC.css.style(JMVC.snow.vars.trg, 'position', 'relative');
+        JMVC.snow.vars.l = (opts && opts.length) || 100;
 
-		JMVC.snow.vars.trg = JMVC.dom.find(trg);
-		JMVC.css.style(JMVC.snow.vars.trg, 'position', 'relative');
-		JMVC.snow.vars.l = opts && opts.length || 100;
-		
-		var bodysize = JMVC.screen.bodySize();
+        var bodysize = JMVC.screen.bodySize(),
+            i = 0;
 
-		function Flake () {
-			var left = JMVC.util.rand(0, bodysize[0] - 20),
+        function Flake () {
+            var left = JMVC.util.rand(0, bodysize[0] - 20),
                 top = JMVC.util.rand(0, bodysize[1] - 20),
                 opa = JMVC.util.rand(3, 8) / 10;
-			this.s = JMVC.util.rand(5,40);
-			this.node = JMVC.dom.create(
-				'span',
-				{
-					'class':'flake',
-					'style':'line-height:' + this.s + 'px;height:' + this.s + 'px;font-size:' + this.s + 'px;top:' + top + 'px;left:' + left + 'px;opacity:' + opa
-				},
-				'&bull;'
-			);
-			this.amplitude = JMVC.util.rand(0.1, 2) *0.5;
-			this.speed = 0.5 + Math.random()*.5;
-			
-			this.length = JMVC.util.rand(10, 20);
-			this.cursor = 0;
-		}
-		
-		function getFlake() {
-			var f = new Flake();
-			JMVC.snow.vars.flakes.push(f);
-			JMVC.dom.append(JMVC.snow.vars.trg, f.node);
-		}
-		
-		function loop() {
-			var newr = [];
-			for(var i = 0, l = JMVC.snow.vars.flakes.length; i < l; i += 1) {
-				var r = move(JMVC.snow.vars.flakes[i]);
-				if (r) {
-					newr.push(JMVC.snow.vars.flakes[i]);
-				}
-			}
-			JMVC.snow.vars.flakes = newr;
-		}
+            this.s = JMVC.util.rand(5, 40);
+            this.node = JMVC.dom.create(
+                'span',
+                {
+                    'class': 'flake',
+                    'style': 'line-height:' + this.s + 'px;height:' + this.s + 'px;font-size:' + this.s + 'px;top:' + top + 'px;left:' + left + 'px;opacity:' + opa
+                },
+                '&bull;'
+            );
+            this.amplitude = JMVC.util.rand(0.1, 2) * 0.5;
+            this.speed = 0.5 + Math.random() * 0.5;
+            this.length = JMVC.util.rand(10, 20);
+            this.cursor = 0;
+        }
 
-		function move(e) {
-			var el = e.node,
-				top = JMVC.num.getFloat(JMVC.css.style(el, 'top')),
-				left = JMVC.num.getFloat(JMVC.css.style(el ,'left'));
+        function getFlake () {
+            var f = new Flake();
+            JMVC.snow.vars.flakes.push(f);
+            JMVC.dom.append(JMVC.snow.vars.trg, f.node);
+        }
 
-			//get back
-			if(top > bodysize[1]-60){
-				top = 0;
-			}
-			
-			left = left + e.amplitude * Math.sin(top / e.length);
-			
-			if(left > bodysize[0] -60){
-				e.node.style.display = 'none';
-			} else {
-				e.node.style.display = 'block';
-			} 
-			JMVC.css.style(el,{'top': (top + e.speed)+'px', 'left': left+ 'px'});
-			return true;
-		}
-		
-		for(var i = 0; i < JMVC.snow.vars.l; i += 1){
-			getFlake();
-		}
-		window.setInterval(function(){loop();}, 10);
-	}
+        function loop () {
+            var newr = [],
+                i = 0, r,
+                l = JMVC.snow.vars.flakes.length;
+            for (null; i < l; i += 1) {
+                r = move(JMVC.snow.vars.flakes[i]);
+                r && newr.push(JMVC.snow.vars.flakes[i]);
+            }
+            JMVC.snow.vars.flakes = newr;
+        }
+
+        function move (e) {
+            var el = e.node,
+                top = JMVC.num.getFloat(JMVC.css.style(el, 'top')),
+                left = JMVC.num.getFloat(JMVC.css.style(el, 'left'));
+
+            // get back
+            if (top > bodysize[1] - 60) {
+                top = 0;
+            }
+
+            left = left + e.amplitude * Math.sin(top / e.length);
+
+            if (left > bodysize[0] - 60) {
+                e.node.style.display = 'none';
+            } else {
+                e.node.style.display = 'block';
+            }
+            JMVC.css.style(el, { 'top': (top + e.speed) + 'px', 'left': left + 'px' });
+            return true;
+        }
+
+        for (null; i < JMVC.snow.vars.l; i += 1) {
+            getFlake();
+        }
+        window.setInterval(function () { loop(); }, 10);
+    }
 });
-
-
