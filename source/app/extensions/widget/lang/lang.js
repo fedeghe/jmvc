7624f6019a1e9c0926b1270b825ccad915314de2
fedeghe <fedeghe@gmail.com>
2019-05-30 10:45:41 +0200

    more lintz

--- source/app/extensions/widget/lang/lang.js
+++ source/app/extensions/widget/lang/lang.js
@@ -1,25 +1,26 @@
 JMVC.extend('widget.langs', {
 
-	//get the basic styles
-	init : function () {
-		JMVC.head.addStyle(JMVC.vars.baseurl + '/app/extensions/widget/lang/lang.css');
-	},
+    // get the basic styles
+    init: function () {
+        JMVC.head.addStyle(JMVC.vars.baseurl + '/app/extensions/widget/lang/lang.css');
+    },
 
-	//create the ul with home langs links within
-	create : function (cnt) {
-		
-		var mkp = JMVC.dom.create('ul', {'class' : 'lang'});
+    // create the ul with home langs links within
+    create: function (cnt) {
+        var mkp = JMVC.dom.create('ul', { 'class': 'lang' });
 
-		JMVC.each(JMVC.i18n.langs, function (e, i){
-			var img = JMVC.dom.create('img', {
-					src : JMVC.vars.baseurl + JMVC.US +'media' + JMVC.US + 'img' + JMVC.US + 'flags' + JMVC.US + i + '.gif',
-					width : 16,
-					height: 11
-				}),
-				a = JMVC.dom.create('a', {'href' : JMVC.vars.baseurl + JMVC.US + '?lang=' + i, 'class' : JMVC.vars.currentlang === i ? 'active' : ''}, img);
-			JMVC.dom.append(mkp, JMVC.dom.create('li', {}, a));
-		});
-
-		JMVC.dom.append(cnt, mkp);
-	}
-})
+        JMVC.each(JMVC.i18n.langs, function (e, i) {
+            var img = JMVC.dom.create('img', {
+                    src: JMVC.vars.baseurl + JMVC.US + 'media' + JMVC.US + 'img' + JMVC.US + 'flags' + JMVC.US + i + '.gif',
+                    width: 16,
+                    height: 11
+                }),
+                a = JMVC.dom.create('a', {
+                    'href': JMVC.vars.baseurl + JMVC.US + '?lang=' + i,
+                    'class': JMVC.vars.currentlang === i ? 'active' : ''
+                }, img);
+            JMVC.dom.append(mkp, JMVC.dom.create('li', {}, a));
+        });
+        JMVC.dom.append(cnt, mkp);
+    }
+});
