2c56d56dc7a7cc63fed1321231c940b79d1bc8c3
fedeghe <fedeghe@gmail.com>
2019-05-31 08:48:01 +0200

    lintz

--- source/app/extensions/canvas/editor/panels/fullscreenManager.js
+++ source/app/extensions/canvas/editor/panels/fullscreenManager.js
@@ -1,12 +1,9 @@
 JMVC.canvas.Editor.getFullscreenManager = function (instance) {
-
-    var self = instance,
-        panel = new JMVC.canvas.Editor.Panel();
-
+    var panel = new JMVC.canvas.Editor.Panel();
     return {
-        launch : function () {
+        launch: function () {
             JMVC.screen.launchFullscreen();
         },
-        panel : panel
+        panel: panel
     };
 };
