7bc71b7d7730c1e11a91e9ca1181ad9af6e44d32
fedeghe <fedeghe@gmail.com>
2019-06-03 22:15:34 +0200

    ddd lintz

--- source/app/extensions/core/lib/math/ddd/ddd.js
+++ source/app/extensions/core/lib/math/ddd/ddd.js
@@ -1,38 +1,36 @@
 // type : CONSTRUCTORS
 //
-
-
 JMVC.extend('ddd', {
-	Point : function (x, y, z) {
-		this.x  = x;
-		this.y  = y;
-		this.z  = z;
-	},
-	Line : function (p1, p2) {
-		this.p1 = p1;
-		this.p2 = p2;
-	},
-	Triangle : function (p1, p2, p3) {
-		this.p1 = p1;
-		this.p2 = p2;
-		this.p3 = p3;
-	}
+    Point: function (x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    },
+    Line: function (p1, p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    },
+    Triangle: function (p1, p2, p3) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
 });
 
 JMVC.prototipize(
-	JMVC.ddd.Point, {
-		distance2Point : function (p) {
-			return Math.sqrt(
-				(this.x -p.x)*(this.x -p.x) +
-				(this.y -p.y)*(this.y -p.y) +
-				(this.z -p.z)*(this.z -p.z)
-			);
-		},
-		distance2Line : function () {
-			
-		}
-		
-	}
+    JMVC.ddd.Point, {
+        distance2Point: function (p) {
+            return Math.sqrt(
+                (this.x - p.x) * (this.x - p.x) +
+                (this.y - p.y) * (this.y - p.y) +
+                (this.z - p.z) * (this.z - p.z)
+            );
+        },
+        distance2Line: function () {
+
+        }
+
+    }
 );
-	
-JMVC.debug(JMVC.ddd.Point.distance)
+
+JMVC.debug(JMVC.ddd.Point.distance);
