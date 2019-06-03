649be9b550ee55d5e70ce51bab450494370fa27f
fedeghe <fedeghe@gmail.com>
2019-06-03 07:22:17 +0200

    some more lintz

--- source/app/extensions/core/lib/game/game.js
+++ source/app/extensions/core/lib/game/game.js
@@ -1,59 +1,58 @@
+/* eslint-disable no-undef */
 // type : FACTOY_METHOD
 //
 
 JMVC.extend('game', {
+    create: function (init, draw, update, fps) {
+        var self = this;
 
-	create : function (init, draw, update, fps) {
+        this.init = init || JMVC.noop;
+        this.draw = draw || JMVC.noop;
+        this.update = update || JMVC.noop;
+        this.fps = fps || 50;
+        this.intervalId = false;
 
-		var self = this;
+        this.run = (function () {
+            var loops = 0,
+                skipTicks = 1000 / JMVC.game.fps,
+                maxFrameSkip = 10,
+                nextGameTick = +new Date();
+            return function () {
+                loops = 0;
+                while (+new Date() > nextGameTick && loops < maxFrameSkip) {
+                    self.update();
+                    nextGameTick += skipTicks;
+                    loops += 1;
+                }
+                self.draw();
+            };
+        })();
 
-		this.init = init || JMVC.noop;
-		this.draw = draw || JMVC.noop;
-		this.update = update || JMVC.noop;
-		this.fps  = fps || 50;
-		this.intervalId = false;
+        (function () {
+            if (JMVC.W.webkitRequestAnimationFrame) {
+                JMVC.W.onEachFrame = function (cb) {
+                    (function _cb_ () { cb(); webkitRequestAnimationFrame(_cb_); })();
+                };
+            } else if (JMVC.W.mozRequestAnimationFrame) {
+                JMVC.W.onEachFrame = function (cb) {
+                    (function _cb_ () { cb(); mozRequestAnimationFrame(_cb_); })();
+                };
+            } else {
+                JMVC.W.onEachFrame = function (cb) {
+                    setInterval(cb, 1000 / 60);
+                };
+            }
+        })();
 
-		this.run = (function() {
-			var loops = 0,
-				skipTicks = 1000 / JMVC.game.fps,
-				maxFrameSkip = 10,
-				nextGameTick = +new Date();
-			return function (){
-				loops = 0;
-				while ( +new Date() > nextGameTick && loops < maxFrameSkip) {
-					self.update();
-					nextGameTick += skipTicks;
-					loops += 1;
-				}
-				self.draw();
-			};
-		})();
+        JMVC.W.onEachFrame(self.run);
 
-		(function() {
-			if (JMVC.W.webkitRequestAnimationFrame) {
-				JMVC.W.onEachFrame = function (cb) {
-					(function _cb_() { cb(); webkitRequestAnimationFrame(_cb_); })();
-				};
-			} else if (JMVC.W.mozRequestAnimationFrame) {
-				JMVC.W.onEachFrame = function (cb) {
-					(function _cb_() {cb(); mozRequestAnimationFrame(_cb_); })();
-				};
-			} else {
-				JMVC.W.onEachFrame = function (cb) {
-					setInterval(cb, 1000 / 60);
-				}
-			}
-		})();
-
-		JMVC.W.onEachFrame(self.run);		
-
-		return {
-			'start' : function(){
-				self.intervalId = setInterval(self.run, 0);
-			},
-			'stop' : function () {
-				clearInterval(self.intervalId);
-			}
-		};
-	}
+        return {
+            'start': function () {
+                self.intervalId = setInterval(self.run, 0);
+            },
+            'stop': function () {
+                clearInterval(self.intervalId);
+            }
+        };
+    }
 });
--- source/app/extensions/core/lib/grind/grind.js
+++ source/app/extensions/core/lib/grind/grind.js
@@ -1,28 +1,23 @@
 // type : LIB
 //
-
-
 /**
  * [description]
  * @return {[type]} [description]
  */
 JMVC.extend('grind', function () {
+    'use strict';
 
-    "use strict";
-    
     JMVC.head.addStyle(JMVC.vars.extensions + 'core/lib/grind/grind.min.css');
 
-    function render(config, callback, pro) {
-        
+    function render (config, callback, pro) {
         var mainTarget = config.target,
             gotContent = 'content' in config,
             promises = pro || [],
             resolve = function () {
                 promises.length && promises.shift();
-                if (promises.length == 0) {
+                if (promises.length === 0) {
                     if (typeof callback === 'function') {
-
-                        // prevent inner Grind to call again their root when is a leaf of a 
+                        // prevent inner Grind to call again their root when is a leaf of a
                         // parent Grind
                         if (!mainTarget._called) {
                             callback.call();
@@ -30,46 +25,39 @@ JMVC.extend('grind', function () {
                         }
                     }
                 }
-            };
+            },
+            i, l;
 
         if (gotContent) {
             config.content.map = {};
             config.content.getNode = function (id) {
                 return id in config.content.map ? config.content.map[id] : false;
-            }
+            };
         }
 
-        (JMVC.dom.isNode(mainTarget) ?
-            mainTarget
-            :
-            JMVC.dom.find(mainTarget)).innerHTML = '';
+        (JMVC.dom.isNode(mainTarget)
+            ? mainTarget
+            : JMVC.dom.find(mainTarget)).innerHTML = '';
 
-        for (var i = 0, l = config.content.length; i < l; i++) {
-
-            (function recur(item, parent) {
-
+        for (i = 0, l = config.content.length; i < l; i++) {
+            (function recur (item, parent) {
                 var tag,
-                    j = 0;
+                    j = 0, k, h;
 
                 if (!parent) {
-                    parent = (JMVC.dom.isNode(mainTarget) ?
-                        mainTarget
-                        :
-                        JMVC.dom.find(item.target || mainTarget)
-                    ) || document.body;                
+                    parent = (JMVC.dom.isNode(mainTarget)
+                        ? mainTarget
+                        : JMVC.dom.find(item.target || mainTarget)
+                    ) || document.body;
                 }
 
-                if (item == 'clearer') {
-
-                    tag = document.createElement("br");
+                if (item === 'clearer') {
+                    tag = document.createElement('br');
                     tag.className = 'clearer';
                     parent.appendChild(tag);
-                    
                 } else {
-                    
-                    tag = document.createElement(item.tag || "div");
-                    
-                    if (typeof item.attrs !== 'undefined') { 
+                    tag = document.createElement(item.tag || 'div');
+                    if (typeof item.attrs !== 'undefined') {
                         for (j in item.attrs) {
                             if (j !== 'class') {
                                 tag.setAttribute(j, item.attrs[j]);
@@ -78,23 +66,23 @@ JMVC.extend('grind', function () {
                             }
                         }
                     }
-                    if (typeof item.style !== 'undefined') { 
+                    if (typeof item.style !== 'undefined') {
                         for (j in item.style) {
-                            tag.style[j.replace(/^float$/,'cssFloat')] = item.style[j];
+                            tag.style[j.replace(/^float$/, 'cssFloat')] = item.style[j];
                         }
                     }
 
-                    if (typeof item.html !== 'undefined') { 
+                    if (typeof item.html !== 'undefined') {
                         tag.innerHTML = item.html;
                     }
-                    
+
                     parent.appendChild(tag);
 
-                    //save a reference back to json
+                    // save a reference back to json
                     //
                     item.node = tag;
 
-                    if (typeof item.cb === "function") {
+                    if (typeof item.cb === 'function') {
                         tag.promise = JMVC.Promise.create();
                         tag.promise.then(resolve);
                         promises.push(tag.promise);
@@ -102,30 +90,27 @@ JMVC.extend('grind', function () {
                     }
 
                     if (item.content) {
-                        for (var k = 0, h = item.content.length; k < h; k++) {
+                        for (k = 0, h = item.content.length; k < h; k++) {
                             recur(item.content[k], tag);
                         }
                     }
 
                     if ('grindID' in item) {
-                       config.content.map[item.grindID] = tag;
+                        config.content.map[item.grindID] = tag;
                     }
                 }
             })(config.content[i]);
-        }   
+        }
         !promises.length && resolve();
-        
     }
 
-
     return {
-        render: function(conf, cb, bodyClass) {
+        render: function (conf, cb, bodyClass) {
             bodyClass && JMVC.dom.addClass(document.body, bodyClass);
             render(conf, cb);
         },
-        colorize : function () {
-            JMVC.head.addStyle(JMVC.vars.extensions + 'core/lib/grind/color.css');  
+        colorize: function () {
+            JMVC.head.addStyle(JMVC.vars.extensions + 'core/lib/grind/color.css');
         }
     };
-
 });
