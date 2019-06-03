9c9404eb30e945133526886833e50f6b1803d86d
fedeghe <fedeghe@gmail.com>
2019-06-03 23:04:14 +0200

    more lintz

--- source/app/extensions/core/mobile/devicemotion.js
+++ source/app/extensions/core/mobile/devicemotion.js
@@ -1,32 +1,30 @@
 JMVC.extend('mobile/devicemotion', {
-	deviceOrientation : function (f, absolute) {
-		
-		JMVC.events.on(window, 'deviceorientation', function (e) {
-			e.absolute = typeof absolute !== 'undefined'
-						&& (e.absolute = absolute);
-			f({
-				alpha : e.alpha,
-				beta : e.beta,
-				gamma : e.gamma,
-				e : e
-			});
-		});
-	},
-	deviceMotion : function (f) {
-		JMVC.events.on(window, 'devicemotion', function (e) {
-			f({
-				accX : e.acceleration.x,
-				accY : e.acceleration.y,
-				accZ : e.acceleration.z,
-				accGX : e.accelerationIncludingGravity.x,
-				accGY : e.accelerationIncludingGravity.y,
-				accGZ : e.accelerationIncludingGravity.z,
-				accAlpha : e.rotationRate.alpha,
-				accBeta : e.rotationRate.beta,
-				accGamma : e.rotationRate.gamma,
-				interval : e.interval,
-				e : e
-			});
-		});
-	}
+    deviceOrientation: function (f, absolute) {
+        JMVC.events.on(window, 'deviceorientation', function (e) {
+            e.absolute = typeof absolute !== 'undefined' ? absolute : false;
+            f({
+                alpha: e.alpha,
+                beta: e.beta,
+                gamma: e.gamma,
+                e: e
+            });
+        });
+    },
+    deviceMotion: function (f) {
+        JMVC.events.on(window, 'devicemotion', function (e) {
+            f({
+                accX: e.acceleration.x,
+                accY: e.acceleration.y,
+                accZ: e.acceleration.z,
+                accGX: e.accelerationIncludingGravity.x,
+                accGY: e.accelerationIncludingGravity.y,
+                accGZ: e.accelerationIncludingGravity.z,
+                accAlpha: e.rotationRate.alpha,
+                accBeta: e.rotationRate.beta,
+                accGamma: e.rotationRate.gamma,
+                interval: e.interval,
+                e: e
+            });
+        });
+    }
 });
