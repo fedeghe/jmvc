e667fb5419d271258a0627e8a661611839341952
fedeghe <fedeghe@gmail.com>
2019-06-03 22:15:13 +0200

    math lint

--- source/app/extensions/core/lib/math/math.js
+++ source/app/extensions/core/lib/math/math.js
@@ -1,17 +1,16 @@
 // type : LIB
 //
 
-
-JMVC.extend('math',{
-	math : JMVC.M, 
-	isSquare : function (n) {
-		var tmp = JMVC.M.sqrt(n);
-		return tmp === ~~tmp;
-	},
-	squareX : function (n, s) {
-		return JMVC.M.pow(n, 1/s);
-	},
-	dirac : function (i) {
-		return ((i % 2) ? -1 : 1);
-	}
+JMVC.extend('math', {
+    math: JMVC.M,
+    isSquare: function (n) {
+        var tmp = JMVC.M.sqrt(n);
+        return tmp === ~~tmp;
+    },
+    squareX: function (n, s) {
+        return JMVC.M.pow(n, 1 / s);
+    },
+    dirac: function (i) {
+        return ((i % 2) ? -1 : 1);
+    }
 });
