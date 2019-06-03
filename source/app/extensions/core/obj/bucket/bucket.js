b71b1f0933041fa8585b3331a90004ce22a77b2a
fedeghe <fedeghe@gmail.com>
2019-06-03 23:04:24 +0200

    more lintz

--- source/app/extensions/core/obj/bucket/bucket.js
+++ source/app/extensions/core/obj/bucket/bucket.js
@@ -2,73 +2,71 @@
 //
 
 JMVC.extend('bucket', {
-	create : function (arr) {
-		"use strict";
-		function bucket(a) {
-			a = a || [];
-			this.arr = JMVC.array.clone(a);
-			this.r = false,
-			this.l = a.length,
-			this.fix = JMVC.array.clone(this.arr);
-			this._current = null;
-			
-		}
-		bucket.prototype = {
+    create: function (arr) {
+        function Bucket (a) {
+            a = a || [];
+            this.arr = JMVC.array.clone(a);
+            this.r = false;
+            this.l = a.length;
+            this.fix = JMVC.array.clone(this.arr);
+            this._current = null;
+        }
+        Bucket.prototype = {
 
-			current : function () {
-				return this._current;
-			},
-			empty : function () {
-				this.arr = [];
-				this.r = false;
-				this.l = 0;
-			},
-			fill : function (arr) {
-				this.arr = JMVC.array.clone(arr);
-				this.r = false;
-				this.l = arr.length;
-				this.fix = JMVC.array.clone(this.arr);
-			},
-			hasMore : function () {return !!this.l; },
-			merge : function (b) {
-				this.arr = [].concat.call(this.arr, b.arr);
-				this.l = this.arr.length;
-			},
-			next : function () {
+            current: function () {
+                return this._current;
+            },
+            empty: function () {
+                this.arr = [];
+                this.r = false;
+                this.l = 0;
+            },
+            fill: function (arr) {
+                this.arr = JMVC.array.clone(arr);
+                this.r = false;
+                this.l = arr.length;
+                this.fix = JMVC.array.clone(this.arr);
+            },
+            hasMore: function () { return !!this.l; },
+            merge: function (b) {
+                this.arr = [].concat.call(this.arr, b.arr);
+                this.l = this.arr.length;
+            },
+            next: function () {
+                if (this.l === 0) { return null; }
+                this.r = this.arr.splice(JMVC.util.rand(0, this.l - 1), 1);
+                this.l -= 1;
 
-				if (this.l == 0) {return null; } 
-				this.r = this.arr.splice(JMVC.util.rand(0, this.l - 1), 1);
-				this.l -= 1;
-
-				this._current = this.r.pop();
-				return this._current;
-			},
-			recover : function () {
-				this.l = this.fix.length;
-				this.arr = JMVC.array.clone(this.fix);
-			},
-			show : function () {
-				return this.arr.toString();
-			},
-			shuffle : function (t) {
-				if (t) while (t--) this.shuffle();
-				this.arr = JMVC.array.shuffle(this.arr);
-				//this.arr.sort(function (a, b) {return .5 - Math.random(); });
-			},
-			size : function () {return this.l; },
-			unfill : function (n) {
-				this.arr = this.arr.slice(0, n < this.l ? this.l - n : 0);
-				this.l = this.arr.length;
-			},
-			walk : function (f) {
-				var that = this;
-				for (var i = 0, l = this.l; i < l; i += 1) {
-					~function (j){
-						that.arr[j] = f(that.arr[j]);
-					}(i);
-				}
-			}
-		};
-		return new bucket(arr);
-	}
+                this._current = this.r.pop();
+                return this._current;
+            },
+            recover: function () {
+                this.l = this.fix.length;
+                this.arr = JMVC.array.clone(this.fix);
+            },
+            show: function () {
+                return this.arr.toString();
+            },
+            shuffle: function (t) {
+                if (t) while (t--) this.shuffle();
+                this.arr = JMVC.array.shuffle(this.arr);
+                // this.arr.sort(function (a, b) {return .5 - Math.random(); });
+            },
+            size: function () { return this.l; },
+            unfill: function (n) {
+                this.arr = this.arr.slice(0, n < this.l ? this.l - n : 0);
+                this.l = this.arr.length;
+            },
+            walk: function (f) {
+                var that = this,
+                    i = 0;
+                for (null; i < this.l; i += 1) {
+                    (function (j) {
+                        that.arr[j] = f(that.arr[j]);
+                    })(i);
+                }
+            }
+        };
+        return new Bucket(arr);
+    }
 });
