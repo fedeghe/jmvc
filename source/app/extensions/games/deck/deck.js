d91fea4b482c03d12da841d2be2ac2dbad7d084b
fedeghe <fedeghe@gmail.com>
2019-06-05 21:37:48 +0200

    lintz

--- source/app/extensions/games/deck/deck.js
+++ source/app/extensions/games/deck/deck.js
@@ -1,25 +1,23 @@
 JMVC.extend('deck', function () {
     JMVC.require('core/obj/bucket/bucket');
-    
     JMVC.head.addStyle([JMVC.vars.baseurl, 'app', 'extensions', 'games', 'deck', 'deck.css'].join(JMVC.US), false, false);
-    // 
     var seeds = {
-            c : '&#9829;',
-            q : '&#9830;',
-            f : '&#9827;',
-            p : '&#9824;'
+            c: '&#9829;',
+            q: '&#9830;',
+            f: '&#9827;',
+            p: '&#9824;'
         },
         els = [null, 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
         bucket = JMVC.bucket.create();
 
-    function getCard(s, n) {
-        var crd = JMVC.dom.create('span', {'class': 'card sm_' + s});
-        JMVC.dom.add(crd, 'span', {'class': 'sm'}, seeds[s]);
-        JMVC.dom.add(crd, 'span', {'class': 'num'}, els[n]);
+    function getCard (s, n) {
+        var crd = JMVC.dom.create('span', { 'class': 'card sm_' + s });
+        JMVC.dom.add(crd, 'span', { 'class': 'sm' }, seeds[s]);
+        JMVC.dom.add(crd, 'span', { 'class': 'num' }, els[n]);
         return crd;
     }
 
-    function initDecks(n) {
+    function initDecks (n) {
         var deck = [],
             i,
             j, jl;
@@ -34,18 +32,17 @@ JMVC.extend('deck', function () {
         bucket.fill(deck);
     }
 
-    function shuffle(n) {
+    function shuffle (n) {
         n = n || 1;
         while (n--) {
             bucket.shuffle();
         }
     }
-    // 
     return {
-        getCard : getCard,
-        initDecks : initDecks,
-        getDecks : function () {return bucket.arr; },
-        shuffle : shuffle,
-        next : function () {return bucket.next(); }
+        getCard: getCard,
+        initDecks: initDecks,
+        getDecks: function () { return bucket.arr; },
+        shuffle: shuffle,
+        next: function () { return bucket.next(); }
     };
 });
