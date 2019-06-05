6a907ea404f013e7ce096c947ed24c0134460e5c
fedeghe <fedeghe@gmail.com>
2019-06-05 21:37:26 +0200

    lintz

--- source/app/extensions/core/validation/messages/en.js
+++ source/app/extensions/core/validation/messages/en.js
@@ -1,9 +1,9 @@
 {
-    'nonempty' : 'field should not be empty',
-    'color' : 'please enter a color [RRGGBB]',
-    'string' : 'field should contain a string',
-    'integer' : 'field should contain an integer',
-    'email' : 'field should contain a valid email address',
-    'checked' : 'field should be checked',
-    'url' : 'field should contain a valid url'
+    'nonempty': 'field should not be empty',
+    'color': 'please enter a color [RRGGBB]',
+    'string': 'field should contain a string',
+    'integer': 'field should contain an integer',
+    'email': 'field should contain a valid email address',
+    'checked': 'field should be checked',
+    'url': 'field should contain a valid url'
 }
