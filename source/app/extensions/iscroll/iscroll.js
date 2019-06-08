4f0996b630d9f143cbbcfce49e93efe813db9d32
fedeghe <fedeghe@gmail.com>
2019-06-08 23:23:50 +0200

    more lintz

--- source/app/extensions/iscroll/iscroll.js
+++ source/app/extensions/iscroll/iscroll.js
@@ -1,167 +1,153 @@
 JMVC.extend('iscroll', {
-	init : function () {
-		JMVC.require('event_scroll/event_scroll', 'core/fx/fx');
-		console.debug('infinite scroll initialized')
-	},
-	create : function (node, ajurl, num) {
-		var active  = true,
-			waitnode,
-			triggerPadd = 50,
-			mode = 1,
-			height = 0,
-			appended = false,
-			timeout = false;
-			// 1 limit before append
-			// 2 
+    init: function () {
+        JMVC.require('event_scroll/event_scroll', 'core/fx/fx');
+        console.debug('infinite scroll initialized');
+    },
+    create: function (node, ajurl, num) {
+        var active = true,
+            waitnode,
+            triggerPadd = 50,
+            mode = 1,
+            height = 0,
+            appended = false,
+            timeout = false;
+        // 1 limit before append
+        // 2
 
-		function append(where){
-			console.debug(where)
-			var cnt = {top : '&uArr;', bottom : '&dArr;'};
-			if(!cnt[where]) {return false; }
-			waitnode = JMVC.dom.create('div', {'id' : 'JMVC_waitnodeiscroll', 'style':'text-align:center;height:0px; background-color:gray;line-height:0px;color:white;text-size:0px'}, cnt[where]);
-			if(where == 'top') {
-				JMVC.dom.prepend(node, waitnode);
-				return true;
-			} else if (where == 'bottom') {
-				JMVC.dom.append(node, waitnode);
-				return true;
-			}
-			return false;
+        function append (where) {
+            console.debug(where);
+            var cnt = { top: '&uArr;', bottom: '&dArr;' };
+            if (!cnt[where]) { return false; }
+            waitnode = JMVC.dom.create('div', { 'id': 'JMVC_waitnodeiscroll', 'style': 'text-align:center;height:0px; background-color:gray;line-height:0px;color:white;text-size:0px' }, cnt[where]);
+            if (where === 'top') {
+                JMVC.dom.prepend(node, waitnode);
+                return true;
+            } else if (where === 'bottom') {
+                JMVC.dom.append(node, waitnode);
+                return true;
+            }
+            return false;
+        }
 
-		}
+        JMVC.events.onScroll(node, function (e) {
+            if (!active) { return false; }
+            //
+            var detail = -e.detail || e.wheelDelta,
+                incr = detail > 0 ? 1 : -1,
+                de, se, st, ch;
 
-		JMVC.events.onScroll(node, function (e) {
+            switch (node) {
+            case JMVC.WD.body:
+                de = JMVC.WD.documentElement;
+                se = de.scrollHeight;
+                st = de.scrollTop || window.pageYOffset;
+                ch = de.clientHeight;
+                break;
+            default:
+                se = node.scrollHeight;
+                st = node.scrollTop;
+                ch = node.clientHeight;
+                break;
+            }
+            // after bottom
+            if (st > 0 && se === (st + ch) && incr < 0) {
+                switch (mode) {
+                case 1:
+                    !appended && (appended = append('bottom'));
 
-			if (!active) {return false; }
-			//
-			var detail = -e.detail || e.wheelDelta,
-				incr = detail>0 ? 1 : -1;
+                    // var tmpnode = JMVC.dom.find('#JMVC_waitnodeiscroll');
+                    timeout && JMVC.W.clearTimeout(timeout);
 
-			var de, se, st, ch;
-			switch (node) {
-				case JMVC.WD.body :
-					de = JMVC.WD.documentElement;
-					se = de.scrollHeight;
-					st = de.scrollTop || window.pageYOffset;
-					ch = de.clientHeight;
-				break;
-				default :
-					se = node.scrollHeight;
-					st = node.scrollTop;
-					ch = node.clientHeight; 
-				break;
-			}
-
-			//after bottom
-			if (st > 0 && se == (st + ch) && incr < 0) {
-				switch (mode) {
-					case 1 :
-						!appended && (appended = append('bottom'));
-						
-						//var tmpnode = JMVC.dom.find('#JMVC_waitnodeiscroll');
-						timeout && JMVC.W.clearTimeout(timeout);
-						
-						height += 2;
-						JMVC.css.style(waitnode,{
-							'height' :  height + 'px',
-							'fontSize' : height + 'px',
-							'lineHeight' : height + 'px'
-						});
-						//JMVC.css.style(waitnode, 'fontSize', height + 'px');
-						//JMVC.css.style(waitnode, 'lineHeight', height + 'px');
-						if (height == triggerPadd){
-							JMVC.W.clearTimeout(timeout);
-							mode = 2;
-							appended = false;
-							console.debug('now unactive');
-							height = 0;
-						} else {
-							timeout = JMVC.W.setTimeout(
-								function () {
-									JMVC.fx.animate(waitnode, 'height', 0, 5, function () {
-										height = 0;
-										JMVC.W.clearTimeout(timeout);
-										active = true;
-										appended = false;
-										JMVC.dom.remove(waitnode);
-										mode = 1;
-									});
-								},
-								1000
-							);
-						}
-					break;
-					case 2 :
-						active = false;
-						console.debug('cb');
-						window.setTimeout(function () {
-							console.debug('now active');
-							active = true;
-							appended = false;
-							JMVC.dom.remove(waitnode);
-							mode = 1;
-						}, 3000);
-					break;
-				}
-
-			//over top
-			} else if (st == 0 && incr > 0) {
-
-				switch(mode){
-					case 1 :
-						!appended && (appended = append('top'));
-						//var tmpnode = JMVC.dom.find('#JMVC_waitnodeiscroll');
-						timeout && JMVC.W.clearTimeout(timeout);
-						
-						
-						height += 2;
-						JMVC.css.style(waitnode,{
-							'height' :  height + 'px',
-							'fontSize' : height + 'px',
-							'lineHeight' : height + 'px'
-						});
-						if (height == triggerPadd){
-							JMVC.W.clearTimeout(timeout);
-							mode = 2;
-							appended = false;
-							console.debug('now unactive');
-							height = 0;
-						
-						} else {
-							timeout = JMVC.W.setTimeout(
-								function () {
-									JMVC.fx.animate(waitnode, 'height', 0, 5, function () {
-										height = 0;
-										JMVC.W.clearTimeout(timeout);
-										active = true;
-										appended = false;
-										JMVC.dom.remove(waitnode);
-										mode = 1;
-									});
-								},
-								1000
-							);
-						}
-					break;
-					case 2 :
-						
-						active = false;
-						console.debug('cb');
-						window.setTimeout(function () {
-							console.debug('now active');
-							active = true;
-							appended = false;
-							JMVC.dom.remove(waitnode);
-							mode = 1;
-						}, 3000);
-						
-					break;
-				}				
-			}
-		})
-	}
+                    height += 2;
+                    JMVC.css.style(waitnode, {
+                        'height': height + 'px',
+                        'fontSize': height + 'px',
+                        'lineHeight': height + 'px'
+                    });
+                    // JMVC.css.style(waitnode, 'fontSize', height + 'px');
+                    // JMVC.css.style(waitnode, 'lineHeight', height + 'px');
+                    if (height === triggerPadd) {
+                        JMVC.W.clearTimeout(timeout);
+                        mode = 2;
+                        appended = false;
+                        console.debug('now unactive');
+                        height = 0;
+                    } else {
+                        timeout = JMVC.W.setTimeout(
+                            function () {
+                                JMVC.fx.animate(waitnode, 'height', 0, 5, function () {
+                                    height = 0;
+                                    JMVC.W.clearTimeout(timeout);
+                                    active = true;
+                                    appended = false;
+                                    JMVC.dom.remove(waitnode);
+                                    mode = 1;
+                                });
+                            },
+                            1000
+                        );
+                    }
+                    break;
+                case 2:
+                    active = false;
+                    console.debug('cb');
+                    window.setTimeout(function () {
+                        console.debug('now active');
+                        active = true;
+                        appended = false;
+                        JMVC.dom.remove(waitnode);
+                        mode = 1;
+                    }, 3000);
+                    break;
+                }
+                // over top
+            } else if (st === 0 && incr > 0) {
+                switch (mode) {
+                case 1:
+                    !appended && (appended = append('top'));
+                    // var tmpnode = JMVC.dom.find('#JMVC_waitnodeiscroll');
+                    timeout && JMVC.W.clearTimeout(timeout);
+                    height += 2;
+                    JMVC.css.style(waitnode, {
+                        'height': height + 'px',
+                        'fontSize': height + 'px',
+                        'lineHeight': height + 'px'
+                    });
+                    if (height === triggerPadd) {
+                        JMVC.W.clearTimeout(timeout);
+                        mode = 2;
+                        appended = false;
+                        console.debug('now unactive');
+                        height = 0;
+                    } else {
+                        timeout = JMVC.W.setTimeout(
+                            function () {
+                                JMVC.fx.animate(waitnode, 'height', 0, 5, function () {
+                                    height = 0;
+                                    JMVC.W.clearTimeout(timeout);
+                                    active = true;
+                                    appended = false;
+                                    JMVC.dom.remove(waitnode);
+                                    mode = 1;
+                                });
+                            },
+                            1000
+                        );
+                    }
+                    break;
+                case 2:
+                    active = false;
+                    console.debug('cb');
+                    window.setTimeout(function () {
+                        console.debug('now active');
+                        active = true;
+                        appended = false;
+                        JMVC.dom.remove(waitnode);
+                        mode = 1;
+                    }, 3000);
+                    break;
+                }
+            }
+        });
+    }
 });
-
-
-
-
--- source/app/extensions/key/key.js
+++ source/app/extensions/key/key.js
@@ -19,310 +19,298 @@ function preventBackspace(e) {
     }
  */
 
-
 /**
  * This must be written from scratch, and extend JMVC.events
  *
  *
- * 
+ *
  */
-
 
 JMVC.extend('key', {
 
-	//EXPERIMENTAL
-	disable : function (element, shortcut) {
-		
-		JMVC.events.on(element, 'keydown', function (e) {
-			var code = JMVC.events.code(e);
-			/*
-			if (code && code == shortcut) {
-				return JMVC.events.kill(e);
-			}*/
-			e.keyCode = JMVC.util.rand(30, 120);
-			console.debug(e);
-		});
-	},
-	//EXPERIMENTAL
-	enable : function (element, shortcut) {
+    // EXPERIMENTAL
+    disable: function (element, shortcut) {
 
-	},
+        JMVC.events.on(element, 'keydown', function (e) {
+            // var code = JMVC.events.code(e);
+            // if (code && code == shortcut) {
+            //  return JMVC.events.kill(e);
+            // }
+            e.keyCode = JMVC.util.rand(30, 120);
+            console.debug(e);
+        });
+    },
+    // EXPERIMENTAL
+    enable: function (element, shortcut) {
+    },
 
-	
+    all_shortcuts: {}, // All the shortcuts are stored in this array
+    add: function (shortcutCombination, callback, opt) {
+        'use strict';
+        // Provide a set of default options
+        var defaultOptions = {
+                type: 'keydown',
+                propagate: false,
+                disableInInput: false,
+                target: JMVC.WD,
+                keycode: false
+            },
+            ele,
+            dfo,
+            func;
 
-	all_shortcuts : {},//All the shortcuts are stored in this array
-	add : function (shortcut_combination, callback, opt) {
+        if (!opt) {
+            opt = defaultOptions;
+        } else {
+            for (dfo in defaultOptions) {
+                if (typeof opt[dfo] === 'undefined') {
+                    opt[dfo] = defaultOptions[dfo];
+                }
+            }
+        }
 
-		'use strict';
+        ele = opt.target;
+        if (typeof opt.target === 'string') {
+            ele = JMVC.WD.getElementById(opt.target);
+        }
+        // var ths = this;
+        shortcutCombination = shortcutCombination.toLowerCase();
 
-		//Provide a set of default options
-		var default_options = {
-				'type' : 'keydown',
-				'propagate' : false,
-				'disable_in_input' : false,
-				'target' : JMVC.WD,
-				'keycode' : false
-			},
-			ele,
-			dfo,
-			func;
-			
-		if (!opt) {
-			opt = default_options;
-		} else {
-			for (dfo in default_options) {
-				if (typeof opt[dfo] === 'undefined') {
-					opt[dfo] = default_options[dfo];
-				}
-			}
-		}
+        // The function to be called at keypress
+        func = function (e) {
+            var element,
+                character,
+                code,
+                keys,
+                kp,
+                shiftNums,
+                specialKeys,
+                modifiers,
+                i,
+                l,
+                k;
+            e = e || JMVC.W.event;
 
-		ele = opt.target;
-		if (typeof opt.target === 'string') {
-			ele = JMVC.WD.getElementById(opt.target);
-		}
-		//var ths = this;
-		shortcut_combination = shortcut_combination.toLowerCase();
+            if (opt.disableInInput) { // Don't enable shortcut keys in Input, Textarea fields
+                if (e.target) {
+                    element = e.target;
+                } else if (e.srcElement) {
+                    element = e.srcElement;
+                }
+                if (element.nodeType === 3) {
+                    element = element.parentNode;
+                }
 
-		//The function to be called at keypress
-		func = function (e) {
-			var element,
-				character,
-				code,
-				keys,
-				kp,
-				shift_nums,
-				special_keys,
-				modifiers,
-				i,
-				l,
-				k;
-			e = e || JMVC.W.event;
-			
-			if (opt.disable_in_input) {//Don't enable shortcut keys in Input, Textarea fields
-				if (e.target) {
-					element = e.target;
-				} else if (e.srcElement) {
-					element = e.srcElement;
-				}
-				if (element.nodeType === 3) {
-					element = element.parentNode;
-				}
+                if (element.tagName === 'INPUT' || element.tagName === 'TEXTAREA') {
+                    return;
+                }
+            }
 
-				if (element.tagName === 'INPUT' || element.tagName === 'TEXTAREA') {
-					return;
-				}
-			}
+            // Find Which key is pressed
+            code = JMVC.events.code(e);
 
-			//Find Which key is pressed
-			code = JMVC.events.code(e);
+            character = String.fromCharCode(code).toLowerCase();
 
-			character = String.fromCharCode(code).toLowerCase();
+            if (code === 188) {
+                character = ','; // If the user presses , when the type is onkeydown
+            }
+            if (code === 190) {
+                character = '.'; // If the user presses , when the type is onkeydown
+            }
 
-			if (code === 188) {
-				character = ","; //If the user presses , when the type is onkeydown
-			}
-			if (code === 190) {
-				character = "."; //If the user presses , when the type is onkeydown
-			}
+            keys = shortcutCombination.split('+');
+            // Key Pressed - counts the number of valid keypresses - if it is same as the number of keys, the shortcut function is invoked
+            kp = 0;
 
-			keys = shortcut_combination.split("+");
-			//Key Pressed - counts the number of valid keypresses - if it is same as the number of keys, the shortcut function is invoked
-			kp = 0;
+            // Work around for stupid Shift key bug created by using lowercase - as a result the shift+num combination was broken
+            shiftNums = {
+                "`": " ~",
+                "1": "!",
+                "2": "@",
+                "3": "#",
+                "4": "$",
+                "5": "%",
+                "6": "^",
+                "7": "&",
+                "8": "*",
+                "9": "(",
+                "0": ")",
+                "-": "_",
+                "=": "+",
+                ";": ":",
+                "'": "\"",
+                ",": "<",
+                ".": ">",
+                "/": "?",
+                "\\": "|"
+            };
+            // Special Keys - and their codes
+            specialKeys = {
+                'esc': 27,
+                'escape': 27,
+                'tab': 9,
+                'space': 32,
+                'return': 13,
+                'enter': 13,
+                'backspace': 8,
 
-			//Work around for stupid Shift key bug created by using lowercase - as a result the shift+num combination was broken
-			shift_nums = {
-				"`" : " ~",
-				"1" : "!",
-				"2" : "@",
-				"3" : "#",
-				"4" : "$",
-				"5" : "%",
-				"6" : "^",
-				"7" : "&",
-				"8" : "*",
-				"9" : "(",
-				"0" : ")",
-				"-" : "_",
-				"=" : "+",
-				";" : ":",
-				"'" : "\"",
-				"," : "<",
-				"." : ">",
-				"/" : "?",
-				"\\" : "|"
-			};
-			//Special Keys - and their codes
-			special_keys = {
-				'esc' : 27,
-				'escape' : 27,
-				'tab' : 9,
-				'space' : 32,
-				'return' : 13,
-				'enter' : 13,
-				'backspace' : 8,
+                'scrolllock': 145,
+                'scroll_lock': 145,
+                'scroll': 145,
+                'capslock': 20,
+                'caps_lock': 20,
+                'caps': 20,
+                'numlock': 144,
+                'num_lock': 144,
+                'num': 144,
 
-				'scrolllock' : 145,
-				'scroll_lock' : 145,
-				'scroll' : 145,
-				'capslock' : 20,
-				'caps_lock' : 20,
-				'caps' : 20,
-				'numlock' : 144,
-				'num_lock' : 144,
-				'num' : 144,
+                'pause': 19,
+                'break': 19,
 
-				'pause' : 19,
-				'break' : 19,
+                'insert': 45,
+                'home': 36,
+                'delete': 46,
+                'end': 35,
 
-				'insert' : 45,
-				'home' : 36,
-				'delete' : 46,
-				'end' : 35,
+                'pageup': 33,
+                'page_up': 33,
+                'pu': 33,
 
-				'pageup' : 33,
-				'page_up' : 33,
-				'pu' : 33,
+                'pagedown': 34,
+                'page_down': 34,
+                'pd': 34,
 
-				'pagedown' : 34,
-				'page_down' : 34,
-				'pd' : 34,
+                'left': 37,
+                'up': 38,
+                'right': 39,
+                'down': 40,
 
-				'left' : 37,
-				'up' : 38,
-				'right' : 39,
-				'down' : 40,
+                'f1': 112,
+                'f2': 113,
+                'f3': 114,
+                'f4': 115,
+                'f5': 116,
+                'f6': 117,
+                'f7': 118,
+                'f8': 119,
+                'f9': 120,
+                'f10': 121,
+                'f11': 122,
+                'f12': 123
+                // 'mela' : 224
+            };
 
-				'f1' : 112,
-				'f2' : 113,
-				'f3' : 114,
-				'f4' : 115,
-				'f5' : 116,
-				'f6' : 117,
-				'f7' : 118,
-				'f8' : 119,
-				'f9' : 120,
-				'f10' : 121,
-				'f11' : 122,
-				'f12' : 123//,
-				
-				//'mela' : 224
-			};
+            modifiers = {
+                shift: { wanted: false, pressed: false },
+                ctrl: { wanted: false, pressed: false },
+                alt: { wanted: false, pressed: false },
+                meta: { wanted: false, pressed: false }	// Meta is Mac specific
+            };
 
-			modifiers = {
-				shift : { wanted : false, pressed : false},
-				ctrl : { wanted : false, pressed : false},
-				alt  : { wanted : false, pressed : false},
-				meta : { wanted : false, pressed : false}	//Meta is Mac specific
-			};
+            if (e.ctrlKey) {
+                modifiers.ctrl.pressed = true;
+            }
+            if (e.shiftKey) {
+                modifiers.shift.pressed = true;
+            }
+            if (e.altKey) {
+                modifiers.alt.pressed = true;
+            }
+            if (e.metaKey) {
+                modifiers.meta.pressed = true;
+            }
 
-			if (e.ctrlKey) {
-				modifiers.ctrl.pressed = true;
-			}
-			if (e.shiftKey) {
-				modifiers.shift.pressed = true;
-			}
-			if (e.altKey) {
-				modifiers.alt.pressed = true;
-			}
-			if (e.metaKey) {
-				modifiers.meta.pressed = true;
-			}
+            for (i = 0, l = keys.length; k = keys[i], i < l; i += 1) {
+                switch (k) {
+                case 'ctrl':
+                case 'control':
+                    kp += 1;
+                    modifiers.ctrl.wanted = true;
+                    break;
+                case 'shift':
+                    kp += 1;
+                    modifiers.shift.wanted = true;
+                    break;
+                case 'alt':
+                    kp += 1;
+                    modifiers.alt.wanted = true;
+                    break;
+                case 'meta':
+                    kp += 1;
+                    modifiers.meta.wanted = true;
+                    break;
+                default:
+                    if (k.length > 1) { // If it is a special key
+                        if (specialKeys[k] === code) {
+                            kp += 1;
+                        }
 
-			for (i = 0, l = keys.length; k = keys[i], i < l; i += 1) {
+                    } else if (opt.keycode) {
+                        if (opt.keycode === code) {
+                            kp += 1;
+                        }
 
-				switch (k) {
-				case 'ctrl':
-				case 'control':
-					kp += 1;
-					modifiers.ctrl.wanted = true;
-					break;
-				case 'shift':
-					kp += 1;
-					modifiers.shift.wanted = true;
-					break;
-				case 'alt':
-					kp += 1;
-					modifiers.alt.wanted = true;
-					break;
-				case 'meta':
-					kp += 1;
-					modifiers.meta.wanted = true;
-					break;
-				default:
-					if (k.length > 1) { //If it is a special key
-						if (special_keys[k] === code) {
-							kp += 1;
-						}
+                    } else { // The special keys did not match
+                        if (character === k) {
+                            kp += 1;
+                        } else {
+                            if (shiftNums[character] && e.shiftKey) { // Stupid Shift key bug created by using lowercase
+                                character = shiftNums[character];
+                                if (character === k) {
+                                    kp += 1;
+                                }
+                            }
+                        }
+                    }
+                    break;
+                }
 
-					} else if (opt.keycode) {
-						if (opt.keycode === code) {
-							kp += 1;
-						}
+            }
 
-					} else { //The special keys did not match
-						if (character === k) {
-							kp += 1;
-						} else {
-							if (shift_nums[character] && e.shiftKey) {//Stupid Shift key bug created by using lowercase
-								character = shift_nums[character];
-								if (character === k) {
-									kp += 1;
-								}
-							}
-						}
-					}
-					break;
-				}
+            if (kp === keys.length &&
+                modifiers.ctrl.pressed === modifiers.ctrl.wanted &&
+                modifiers.shift.pressed === modifiers.shift.wanted &&
+                modifiers.alt.pressed === modifiers.alt.wanted &&
+                modifiers.meta.pressed === modifiers.meta.wanted) {
+                callback(e);
 
-			}
+                if (!opt.propagate) {
+                    JMVC.events.kill(e);
+                }
+            }
+        };
+        this.all_shortcuts[shortcutCombination] = {
+            'callback': func,
+            'target': ele,
+            'event': opt.type
+        };
+        // Attach the function with the event
+        JMVC.events.on(window, opt.type, func);
+    },
 
-			if (kp === keys.length &&
-						modifiers.ctrl.pressed === modifiers.ctrl.wanted &&
-						modifiers.shift.pressed === modifiers.shift.wanted &&
-						modifiers.alt.pressed === modifiers.alt.wanted &&
-						modifiers.meta.pressed === modifiers.meta.wanted) {
-				callback(e);
+    // Remove the shortcut - just specify the shortcut and I will remove the binding
+    remove: function (shortcutCombination) {
+        'use strict';
+        var binding,
+            type,
+            ele,
+            callback;
+        shortcutCombination = shortcutCombination.toLowerCase();
+        binding = this.all_shortcuts[shortcutCombination];
+        this.all_shortcuts[shortcutCombination] = null;
+        delete this.all_shortcuts[shortcutCombination];
+        if (!binding) {
+            return;
+        }
+        type = binding.event;
+        ele = binding.target;
+        callback = binding.callback;
 
-				if (!opt.propagate) {
-					JMVC.events.kill(e);
-				}
-			}
-		};
-		this.all_shortcuts[shortcut_combination] = {
-			'callback' : func,
-			'target' : ele,
-			'event' : opt.type
-		};
-		//Attach the function with the event
-		JMVC.events.on(window, opt.type, func);
-	},
-
-	//Remove the shortcut - just specify the shortcut and I will remove the binding
-	remove : function (shortcut_combination) {
-
-		'use strict';
-
-		var binding,
-			type,
-			ele,
-			callback;
-		shortcut_combination = shortcut_combination.toLowerCase();
-		binding = this.all_shortcuts[shortcut_combination];
-		this.all_shortcuts[shortcut_combination] = null;
-		delete this.all_shortcuts[shortcut_combination];
-		if (!binding) {
-			return;
-		}
-		type = binding.event;
-		ele = binding.target;
-		callback = binding.callback;
-
-		JMVC.events.off(window, type);
-		ele['on' + type] = false;
-	}
+        JMVC.events.off(window, type);
+        ele['on' + type] = false;
+    }
 });
-/** 
+/**
  var objChars = {
 mozilla:{
 "Arrows":    {
