0e05a1dcd72fbba5d5da31dba616dabda10d356d
fedeghe <fedeghe@gmail.com>
2019-06-10 10:18:24 +0200

    lintz

--- source/app/extensions/string/string.js
+++ source/app/extensions/string/string.js
@@ -1,13 +1,13 @@
-JMVC.extend('string',{
-	hide : function(txt){
-		var out = [],
-			i;
-		for (i in txt){
-			out.push(txt.charCodeAt(i));
-		}
-		return out;
-	},
-	unhide : function(arr){
-		return String.fromCharCode.apply(null, arr);
-	}
+JMVC.extend('string', {
+    hide: function (txt) {
+        var out = [],
+            i;
+        for (i in txt) {
+            out.push(txt.charCodeAt(i));
+        }
+        return out;
+    },
+    unhide: function (arr) {
+        return String.fromCharCode.apply(null, arr);
+    }
 });
