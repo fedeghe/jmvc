2abd50d79cd376ff5627e8929e5b6c82674460ed
fedeghe <fedeghe@gmail.com>
2019-06-10 10:17:46 +0200

    lintz

--- source/app/extensions/perfs/arrayempty.js
+++ source/app/extensions/perfs/arrayempty.js
@@ -3,33 +3,32 @@
  * Abstract : get the fastest way to empty an array
  */
 
-//JMVC.head.lib('jQuery');
+// JMVC.head.lib('jQuery');
 JMVC.head.lib('underscore');
 JMVC.head.lib('prototype');
-
 JMVC.extend('array', {
 
-    empty1 : function (arr) {
+    empty1: function (arr) {
         while (arr.length) {
             arr.pop();
         }
         return arr;
     },
 
-    empty2 : function (arr) {
+    empty2: function (arr) {
         // all but references!!!
         arr = [];
         return arr;
     },
-    empty3 : function (arr) {
+    empty3: function (arr) {
         // no strict!!!
         // here supposed to throw an error
         arr.length = 0;
-        return r;
+        return arr;
     },
-    empty4 : function (arr) {
+    empty4: function (arr) {
         var r = [];
-        
+
         return r;
     }
 });
