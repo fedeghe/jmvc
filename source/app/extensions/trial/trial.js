676db57fd6f9704bce07798d149e5fc5c6ac5b3f
fedeghe <fedeghe@gmail.com>
2019-06-10 10:18:32 +0200

    lintz

--- source/app/extensions/trial/trial.js
+++ source/app/extensions/trial/trial.js
@@ -1,14 +1,13 @@
-JMVC.extend('mac',{
-	titlesay : function() {
-		var date = new Date();
-		var phrases = [
-			'Hello',
-			'WOW',
-			'Have a nice day',
-			date.getHours()+':'+date.getMinutes(),
-			'Enjoy JMVC'
-		];
-		JMVC.head.title('INFO :: '+phrases[JMVC.util.rand(0, phrases.length-1)]);
-	}
+JMVC.extend('mac', {
+    titlesay: function () {
+        var date = new Date(),
+            phrases = [
+                'Hello',
+                'WOW',
+                'Have a nice day',
+                date.getHours() + ':' + date.getMinutes(),
+                'Enjoy JMVC'
+            ];
+        JMVC.head.title('INFO :: ' + phrases[JMVC.util.rand(0, phrases.length - 1)]);
+    }
 });
-
