3f9a08acb9de8ffa4a5b6d595e01404bd0b9b09c
fedeghe <fedeghe@gmail.com>
2019-06-11 23:06:43 +0200

    lintz

--- source/app/controllers/jmvcdoc/show.js
+++ source/app/controllers/jmvcdoc/show.js
@@ -1,13 +1,11 @@
 JMVC.controllers.show = function () {
-
     this.action_index = function () {
-		JMVC.debug('yeah');
+        JMVC.debug('yeah');
         console.debug(arguments);
-	}
-    
-	//check http://www.jmvc.org/jmvcdoc_show/what/name/Federico/surname/Ghedina?age=36
-	this.action = function (){
-		JMVC.debug('Checking magic action');
-		console.debug(arguments);
-	}
+    };
+    // check http://www.jmvc.org/jmvcdoc_show/what/name/Federico/surname/Ghedina?age=36
+    this.action = function () {
+        JMVC.debug('Checking magic action');
+        console.debug(arguments);
+    };
 };
