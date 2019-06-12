086a0750b0766ccf67889831efb527846070227a
fedeghe <fedeghe@gmail.com>
2019-06-13 00:29:12 +0200

    lintz

--- source/app/controllers/test/obj.js
+++ source/app/controllers/test/obj.js
@@ -1,116 +1,124 @@
-JMVC.controllers.obj = function() {
+JMVC.controllers.obj = function () {
+    this.action_index = function () {
+        JMVC.events.loadify(1000);
 
-	this.action_index = function(){
-		
-		
-		
-		JMVC.events.loadify(1000);
-		
-		this.render(false, function test(){
-			"use strict";
-			
-			var o0 = [1,2,function(){alert('x')}, {a:{d:3}}],
-				o1 = [1,2,function(){alert('x')}, {a:{d:2}}],
-				o2 = [1,2,function(){alert('x')}, {a:{d:3}}, 1],
-				o3 = [1,2,function(){alert('x')}, {a:{d:3}}, 1],
-				o4 = [1,2,function(){alert('x')}, {a:{d:3}}, 2],
-				o5 = [1,2,2,3,4],
-				o6 = [1,2,2,3,4],
-				o7 = {a:1,b:2},
-				o8 = {a:1, b:2},
-				o9 = {a:1, b:3},
-				o10 = {a:[1,2,function(){alert('x')}, {a:{d:3}}],b:2},
-				o11 = {a:[1,2,function(){alert('x')}, {a:{d:3}}],b:2},
-				o12 = {a:[1,2,function(){alert('x')}, {a:{d:3}}],b:2, s:'w'},
-				o13 = {a:[1,2,function(){alert('x')}, {a:{d:3}}],b:2, s:{s:1}},
-				o14 = {a:[1,2,function(){alert('x')}, {a:{d:3}}],b:2, s:{s:1}},
-				o15 = {a:[1,2,function(){alert('x')}, {a:{d:3}}],b:2, s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{}}}}}}}}}}}}}}}}}}}},
-				o16 = {a:[1,2,function(){alert('x')}, {a:{d:3}}],b:2, s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{d:1}}}}}}}}}}}}}}}}}}}},
-				o17 = {a:[1,2,function(){alert('x')}, {a:{d:3}}],b:2, s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{d:1}}}}}}}}}}}}}}}}}}}},
-				o18 = {a:[1,2,function(){alert('x')}, {a:{d:3}}],b:2, s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{s:{W:{s:{s:{s:{d:1}}}}}}}}}}}}}}}}}}}};
-			
-			JMVC.test.initialize(true);
-			
-			JMVC.test.startAll();
-			
-			JMVC.test.describe('Testing a function for comparing Object without JSON object');
-			
-			JMVC.test.code('var o0 = ['+JSON.stringify(o0)+'],'+"\n"+
-					'	o1 = '+JSON.stringify(o1)+','+"\n"+
-					'	o2 = '+JSON.stringify(o2)+','+"\n"+
-					'	o3 = '+JSON.stringify(o3)+','+"\n"+
-					'	o4 = '+JSON.stringify(o4)+','+"\n"+
-					'	o5 = '+JSON.stringify(o5)+','+"\n"+
-					'	o6 = '+JSON.stringify(o6)+','+"\n"+
-					'	o7 = '+JSON.stringify(o7)+','+"\n"+
-					'	o8 = '+JSON.stringify(o8)+','+"\n"+
-					'	o9 = '+JSON.stringify(o9)+','+"\n"+
-					'	o10 = '+JSON.stringify(o10)+','+"\n"+
-					'	o11 = '+JSON.stringify(o11)+','+"\n"+
-					'	o12 = '+JSON.stringify(o12)+','+"\n"+
-					'	o13 = '+JSON.stringify(o13)+';'+"\n"+
-					'	o14 = '+JSON.stringify(o14)+';'+"\n"+
-					'	o15 = '+JSON.stringify(o15)+';'+"\n"+
-					'	o16 = '+JSON.stringify(o16)+';'+"\n"+
-					'	o17 = '+JSON.stringify(o17)+';'+"\n"+
-					'	o18 = '+JSON.stringify(o18)+';');
-			
-			JMVC.test.testValue("compare(o0, o1) = false", function () {return JMVC.object.compare(o0, o1); }, false);
-			JMVC.test.testValue("compare(o1, o2) = false", function () {return JMVC.object.compare(o1, o2); }, false);
-			JMVC.test.testValue("compare(o2, o3) = true", function () {return JMVC.object.compare(o2, o3); }, true);
-			
-			JMVC.test.testValue("compare(o3, o4) = true", function () {return JMVC.object.compare(o3, o4); }, false);
-			JMVC.test.testValue("compare(o4, o5) = true", function () {return JMVC.object.compare(o4, o5); }, false);
-			JMVC.test.testValue("compare(o5, o6) = true", function () {return JMVC.object.compare(o5, o6); }, true);
-			JMVC.test.testValue("compare(o6, o7) = true", function () {return JMVC.object.compare(o6, o7); }, false);
-			JMVC.test.testValue("compare(o7, o8) = true", function () {return JMVC.object.compare(o7, o8); }, true);
-			JMVC.test.testValue("compare(o8, o9) = true", function () {return JMVC.object.compare(o8, o9); }, false);
-			JMVC.test.testValue("compare(o9, o10) = true", function () {return JMVC.object.compare(o9, o10); }, false);
-			JMVC.test.testValue("compare(o10, o11) = true", function () {return JMVC.object.compare(o10, o11); }, true);
-			JMVC.test.testValue("compare(o11, o12) = true", function () {return JMVC.object.compare(o11, o12); }, false);
-			JMVC.test.testValue("compare(o12, o13) = true", function () {return JMVC.object.compare(o12, o13); }, false);
+        this.render(false, function test () {
+            'use strict';
+            var o0 = [1, 2, function () { alert('x'); }, { a: { d: 3 } }],
+                o1 = [1, 2, function () { alert('x'); }, { a: { d: 2 } }],
+                o2 = [1, 2, function () { alert('x'); }, { a: { d: 3 } }, 1],
+                o3 = [1, 2, function () { alert('x'); }, { a: { d: 3 } }, 1],
+                o4 = [1, 2, function () { alert('x'); }, { a: { d: 3 } }, 2],
+                o5 = [1, 2, 2, 3, 4],
+                o6 = [1, 2, 2, 3, 4],
+                o7 = { a: 1, b: 2 },
+                o8 = { a: 1, b: 2 },
+                o9 = { a: 1, b: 3 },
+                o10 = { a: [1, 2, function () { alert('x'); }, { a: { d: 3 } }], b: 2 },
+                o11 = { a: [1, 2, function () { alert('x'); }, { a: { d: 3 } }], b: 2 },
+                o12 = { a: [1, 2, function () { alert('x'); }, { a: { d: 3 } }], b: 2, s: 'w' },
+                o13 = { a: [1, 2, function () { alert('x'); }, { a: { d: 3 } }], b: 2, s: { s: 1 } },
+                o14 = { a: [1, 2, function () { alert('x'); }, { a: { d: 3 } }], b: 2, s: { s: 1 } },
+                o15 = { a: [1, 2, function () { alert('x'); }, { a: { d: 3 } }], b: 2, s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: {} } } } } } } } } } } } } } } } } } } },
+                o16 = { a: [1, 2, function () { alert('x'); }, { a: { d: 3 } }], b: 2, s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { d: 1 } } } } } } } } } } } } } } } } } } } },
+                o17 = { a: [1, 2, function () { alert('x'); }, { a: { d: 3 } }], b: 2, s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { d: 1 } } } } } } } } } } } } } } } } } } } },
+                o18 = { a: [1, 2, function () { alert('x'); }, { a: { d: 3 } }], b: 2, s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { s: { W: { s: { s: { s: { d: 1 } } } } } } } } } } } } } } } } } } } };
 
-			JMVC.test.testValue("compare(o13, o14) = true", function () {return JMVC.object.compare(o13, o14); }, true);
-			JMVC.test.testValue("compare(o14, o15) = true", function () {return JMVC.object.compare(o14, o15); }, false);
-			JMVC.test.testValue("compare(o15, o16) = true", function () {return JMVC.object.compare(o15, o16); }, false);
-			JMVC.test.testValue("compare(o16, o17) = true", function () {return JMVC.object.compare(o16, o17); }, true);
-			JMVC.test.testValue("compare(o17, o18) = true", function () {return JMVC.object.compare(o17, o18); }, false);
-			
-			
-			JMVC.test.testTime('JMVC.object.compare', JMVC.object.compare, 100, [o13, o14]);
-			
-			// -------------------		
-			JMVC.test.finishAll();			
-			
-		});
-	};
+            JMVC.test.initialize(true);
 
-	this.action_key = function (){
-		var baseObj = {
-			a : "a", b : "b", c : "c", d : "d",
-			e : "e", f : "f", g : "g", h : "h",
-			i : "i", l : "l", m : "m", n : "n",
-			o : "o", p : "p", q : "q", r : "r",
-			s : "s", t : "t", u : "u", v : "v",
-			z : "z"
-		};
+            JMVC.test.startAll();
 
-function kdirect (k) {
-	return k in baseObj
-}
-function direct (k) {
-	return baseObj.k !== 'undefined'
-}
-function hyperdirect (k) {
-	return baseObj.k !== 'undefined'
-}
+            JMVC.test.describe('Testing a function for comparing Object without JSON object');
 
+            JMVC.test.code('var o0 = [' + JSON.stringify(o0) + '],' + '\n' +
+                '   o1 = ' + JSON.stringify(o1) + ',' + '\n' +
+                '   o2 = ' + JSON.stringify(o2) + ',' + '\n' +
+                '   o3 = ' + JSON.stringify(o3) + ',' + '\n' +
+                '   o4 = ' + JSON.stringify(o4) + ',' + '\n' +
+                '   o5 = ' + JSON.stringify(o5) + ',' + '\n' +
+                '   o6 = ' + JSON.stringify(o6) + ',' + '\n' +
+                '   o7 = ' + JSON.stringify(o7) + ',' + '\n' +
+                '   o8 = ' + JSON.stringify(o8) + ',' + '\n' +
+                '   o9 = ' + JSON.stringify(o9) + ',' + '\n' +
+                '   o10 = ' + JSON.stringify(o10) + ',' + '\n' +
+                '   o11 = ' + JSON.stringify(o11) + ',' + '\n' +
+                '   o12 = ' + JSON.stringify(o12) + ',' + '\n' +
+                '   o13 = ' + JSON.stringify(o13) + ';' + '\n' +
+                '   o14 = ' + JSON.stringify(o14) + ';' + '\n' +
+                '   o15 = ' + JSON.stringify(o15) + ';' + '\n' +
+                '   o16 = ' + JSON.stringify(o16) + ';' + '\n' +
+                '   o17 = ' + JSON.stringify(o17) + ';' + '\n' +
+                '   o18 = ' + JSON.stringify(o18) + ';');
 
+            JMVC.test.testValue('compare(o0, o1) = false', function () { return JMVC.object.compare(o0, o1); }, false);
+            JMVC.test.testValue('compare(o1, o2) = false', function () { return JMVC.object.compare(o1, o2); }, false);
+            JMVC.test.testValue('compare(o2, o3) = true', function () { return JMVC.object.compare(o2, o3); }, true);
 
-		this.render(function () {
-			"use strict";
-			var JT = JMVC.test;
-			JT.initialize(true);
+            JMVC.test.testValue('compare(o3, o4) = true', function () { return JMVC.object.compare(o3, o4); }, false);
+            JMVC.test.testValue('compare(o4, o5) = true', function () { return JMVC.object.compare(o4, o5); }, false);
+            JMVC.test.testValue('compare(o5, o6) = true', function () { return JMVC.object.compare(o5, o6); }, true);
+            JMVC.test.testValue('compare(o6, o7) = true', function () { return JMVC.object.compare(o6, o7); }, false);
+            JMVC.test.testValue('compare(o7, o8) = true', function () { return JMVC.object.compare(o7, o8); }, true);
+            JMVC.test.testValue('compare(o8, o9) = true', function () { return JMVC.object.compare(o8, o9); }, false);
+            JMVC.test.testValue('compare(o9, o10) = true', function () { return JMVC.object.compare(o9, o10); }, false);
+            JMVC.test.testValue('compare(o10, o11) = true', function () { return JMVC.object.compare(o10, o11); }, true);
+            JMVC.test.testValue('compare(o11, o12) = true', function () { return JMVC.object.compare(o11, o12); }, false);
+            JMVC.test.testValue('compare(o12, o13) = true', function () { return JMVC.object.compare(o12, o13); }, false);
+
+            JMVC.test.testValue('compare(o13, o14) = true', function () { return JMVC.object.compare(o13, o14); }, true);
+            JMVC.test.testValue('compare(o14, o15) = true', function () { return JMVC.object.compare(o14, o15); }, false);
+            JMVC.test.testValue('compare(o15, o16) = true', function () { return JMVC.object.compare(o15, o16); }, false);
+            JMVC.test.testValue('compare(o16, o17) = true', function () { return JMVC.object.compare(o16, o17); }, true);
+            JMVC.test.testValue('compare(o17, o18) = true', function () { return JMVC.object.compare(o17, o18); }, false);
+
+            JMVC.test.testTime('JMVC.object.compare', JMVC.object.compare, 100, [o13, o14]);
+
+            // -------------------
+            JMVC.test.finishAll();
+        });
+    };
+
+    this.action_key = function () {
+        var baseObj = {
+            a: 'a',
+            b: 'b',
+            c: 'c',
+            d: 'd',
+            e: 'e',
+            f: 'f',
+            g: 'g',
+            h: 'h',
+            i: 'i',
+            l: 'l',
+            m: 'm',
+            n: 'n',
+            o: 'o',
+            p: 'p',
+            q: 'q',
+            r: 'r',
+            s: 's',
+            t: 't',
+            u: 'u',
+            v: 'v',
+            z: 'z'
+        };
+
+        function kdirect (k) {
+            return k in baseObj;
+        }
+        function direct (k) {
+            return baseObj.k !== 'undefined';
+        }
+        function hyperdirect (k) {
+            return baseObj.k !== 'undefined';
+        }
+
+        this.render(function () {
+            'use strict';
+            var JT = JMVC.test,
+                times = 10000,
+                top = ['a', 'b', 'c', 'd', 'f', 'g', 'ww', 'ee', 'rr', 'tt', 'yy', 'uu'];
+            JT.initialize(true);
             JT.startAll();
             JMVC.events.loadify(1000);
 
@@ -125,18 +133,15 @@ function hyperdirect (k) {
             JT.message('Hyperdirect version');
             JT.code(hyperdirect.toString());
 
-            var times = 10000,
-                top = ['a', 'b', 'c', 'd', 'f', 'g', 'ww', 'ee', 'rr', 'tt', 'yy', 'uu'];
-            JT.describe('<h2>Times comparison</h2>here the 2 functions are executed ' + times + ' times with the same input : '+ top);
+            JT.describe('<h2>Times comparison</h2>here the 2 functions are executed ' + times + ' times with the same input : ' + top);
 
             JT.testTime('kdirect', kdirect, times, top);
             JT.testTime('direct', direct, times, top);
             JT.testTime('hyperdirect', hyperdirect, times, top);
 
             JT.timeSummary();
-            
-            JT.finishAll();
 
-		});
-	};
+            JT.finishAll();
+        });
+    };
 };
