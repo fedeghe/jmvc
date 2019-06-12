2308c188e6a77ba75185d0322f2a93693030c999
fedeghe <fedeghe@gmail.com>
2019-06-12 23:52:30 +0200

    lintz

--- source/app/controllers/test/modal.js
+++ source/app/controllers/test/modal.js
@@ -1,26 +1,20 @@
-JMVC.controllers.modal = function() {
+JMVC.controllers.modal = function () {
+    this.action_index = function () {
+        JMVC.require('widget/modal/modal', 'event_scroll/event_scroll');
+        JMVC.events.loadify(1000);
 
-	this.action_index = function(){
+        this.render(false, function test () {
+            'use strict';
+            JMVC.test.initialize(true);
+            JMVC.test.startAll();
 
-		JMVC.require('widget/modal/modal', 'event_scroll/event_scroll');
+            JMVC.test.describe(JMVC.string.repeat('<br />', 20) + '<input type="button" id="openmodal" value="Open modal" style="cursor:pointer" />' + JMVC.string.repeat('<br />', 40));
 
-		JMVC.events.loadify(1000);
-
-		this.render(false,function test(){
-			"use strict";
-
-			JMVC.test.initialize(true);
-
-			JMVC.test.startAll();
-			
-			JMVC.test.describe(JMVC.string.repeat('<br />', 20) + '<input type="button" id="openmodal" value="Open modal" style="cursor:pointer" />' + JMVC.string.repeat('<br />', 40));
-			
-			JMVC.events.on(JMVC.dom.find('#openmodal'), 'click', function () {
-				var t = JMVC.dom.create('span', {}, 'Here is some content');
-				JMVC.modal.open(t, {title : 'Titolo del modal', bgcolor:'#777', shadow : true});
-			});
-
-			JMVC.test.finishAll();
-		});
-	}
+            JMVC.events.on(JMVC.dom.find('#openmodal'), 'click', function () {
+                var t = JMVC.dom.create('span', {}, 'Here is some content');
+                JMVC.modal.open(t, { title: 'Titolo del modal', bgcolor: '#777', shadow: true });
+            });
+            JMVC.test.finishAll();
+        });
+    };
 };
