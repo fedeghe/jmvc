172a8ee6de6994bb46325f9c6128055b70b438be
fedeghe <fedeghe@gmail.com>
2019-06-13 00:35:21 +0200

    lintz

--- source/app/controllers/iscroll.js
+++ source/app/controllers/iscroll.js
@@ -1,9 +1,9 @@
 JMVC.controllers.iscroll = function () {
     'use strict';
-	this.action_index = function () {
-		JMVC.require('iscroll/iscroll');
-		JMVC.getView('scroll').render(function () {	
-			JMVC.iscroll.create(JMVC.dom.find('#divnode'));
-		});
-	};
+    this.action_index = function () {
+        JMVC.require('iscroll/iscroll');
+        JMVC.getView('scroll').render(function () {
+            JMVC.iscroll.create(JMVC.dom.find('#divnode'));
+        });
+    };
 };
