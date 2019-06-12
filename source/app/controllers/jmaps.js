6c7d2154374e5998803214c81e97dd62ab1c2684
fedeghe <fedeghe@gmail.com>
2019-06-13 00:35:27 +0200

    lintz

--- source/app/controllers/jmaps.js
+++ source/app/controllers/jmaps.js
@@ -1,12 +1,13 @@
 JMVC.controllers.jmaps = function () {
     'use strict';
-	this.action_index = function () {
-		JMVC.require('core/color/color', 'core/lib/jmap/jmap');
-		var index = JMVC.getView('zero'),
-			jmap = false;
-		index.render({cback : function () {
-			var body = JMVC.dom.body();
-			jmap = JMVC.jmap.create(body, 400, 250, 3);
-		}});
-	};
+    this.action_index = function () {
+        JMVC.require('core/color/color', 'core/lib/jmap/jmap');
+        var index = JMVC.getView('zero');
+        index.render({
+            cback: function () {
+                var body = JMVC.dom.body();
+                JMVC.jmap.create(body, 400, 250, 3);
+            }
+        });
+    };
 };
