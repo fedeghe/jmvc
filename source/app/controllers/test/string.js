77d03c5a3e725d859d77e0990f213ab589ada49f
fedeghe <fedeghe@gmail.com>
2019-06-13 00:29:32 +0200

    lintz

--- source/app/controllers/test/string.js
+++ source/app/controllers/test/string.js
@@ -1,35 +1,28 @@
-JMVC.controllers.string = function() {
+JMVC.controllers.string = function () {
+    this.action_index = function () {
+        function es5endsWith (str, end) {
+            return !!(str.match(new RegExp(end + '$', 'i')));
+        }
+        function es5startsWith (str, start) {
+            return !!(str.match(new RegExp('^' + start, 'i')));
+        }
+        function es6endsWith (str, end) {
+            return str.endsWith(end);
+        }
+        function es6startsWith (str, start) {
+            return str.startsWith(start);
+        }
 
-    this.action_index = function(){
+        // es6startssWith
+        //
+        this.render(function test () {
+            'use strict';
+            var JT = JMVC.test,
+                times = 1E2;
 
-        
-    function es5endsWith(str, end) {
-        return !!(str.match(new RegExp(end + "$", 'i')));
-    }
-    function es5startsWith(str, start) {
-        return !!(str.match(new RegExp("^" + start, 'i')));
-    }
-    function es6endsWith(str, end) {
-        return str.endsWith(end);
-    }
-    function es6startsWith(str, start) {
-        return str.startsWith(start);
-    }
-
-// es6startssWith
-
-//
-
-
-        this.render(function test(){
-            "use strict";
-            var JT = JMVC.test;
-            
             JT.initialize(true);
             JT.startAll();
-            
             JMVC.events.loadify(500);
-            
             JT.describe('ES5 vs. ES6 endsWith/startsWith comparison');
 
             JT.message('es5endsWith');
@@ -40,27 +33,21 @@ JMVC.controllers.string = function() {
             JT.message('es6endsWith');
             JT.code(es6endsWith.toString());
             JT.message('es6startsWith');
-            JT.code(es6startsWith.toString());            
-            
-            var times = 1E2,
-                top = 1E8;
+            JT.code(es6startsWith.toString());
 
             // JT.testTime('es5endsWith', es5endsWith, times, ["hello world", "ld"]);
             // JT.testTime('es5startsWith', es5startsWith, times, ["hello world", "he"]);
             // JT.testTime('es6endsWith', es6endsWith, times, ["hello world", "ld"]);
             // JT.testTime('es6startsWith', es6startsWith, times, ["hello world", "he"]);
 
-            function generateEnd() {return ["Hello", "lo"]}
-            function generateStart() {return ["Hello", "He"]}
+            function generateEnd () { return [ 'Hello', 'lo' ]; }
+            function generateStart () { return [ 'Hello', 'He' ]; }
             JT.testTime('es5endsWith', es5endsWith, times, generateEnd);
             JT.testTime('es5startsWith', es5startsWith, times, generateStart);
             JT.testTime('es6endsWith', es6endsWith, times, generateEnd);
             JT.testTime('es6startsWith', es6startsWith, times, generateStart);
 
-
-
             JT.timeSummary();
-            
             JT.finishAll();
         });
     };
