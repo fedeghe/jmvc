fe3b09667e33aec0b6d0e2a792bcf0c234aa7440
fedeghe <fedeghe@gmail.com>
2019-06-12 07:57:17 +0200

    eslinz

--- source/app/controllers/test/fizzbuzz.js
+++ source/app/controllers/test/fizzbuzz.js
@@ -1,20 +1,18 @@
-JMVC.controllers.fizzbuzz = function() {
-
-    this.action_index = function(){
-
-        function fb1(n) {
-            var res = '',
+JMVC.controllers.fizzbuzz = function () {
+    this.action_index = function () {
+        function fb1 (n) {
+            var i,
                 max = n || 1E5,
                 inner = function (j) {
                     return (j % 3 ? '' : 'Fizz') + (j % 5 ? '' : 'Buzz') || j;
-                }
-            for (var i = 1, out = ''; i < max; i++, out = '') {
+                };
+            for (i = 1; i < max; i++) {
                 inner(i);
             }
             return true;
         }
 
-        function fb2(n) {
+        function fb2 (n) {
             var acc = 810092048,
                 top = n || 1E5,
                 number = 1,
@@ -30,38 +28,41 @@ JMVC.controllers.fizzbuzz = function() {
                     }
                     acc = acc >> 2 | c << 28;
                     return a;
-                },
-                end;
-             
+                };
+
             while (number <= top) {
-              bitwiseFizzBuzz(number)
-              number++;
+                bitwiseFizzBuzz(number);
+                number++;
             }
             return true;
         }
 
-        function fb3(n) {
-            var messages = [null, "Fizz", "Buzz", "FizzBuzz"],
-                acc = 810092048, //11 00 00 01 00 10 01 00 00 01 10 00 01 00 00
+        function fb3 (n) {
+            var // messages = [null, 'Fizz', 'Buzz', 'FizzBuzz'],
+                acc = 810092048, // 11 00 00 01 00 10 01 00 00 01 10 00 01 00 00
                 c = 0,
-                result,
+                // result,
                 i = 0;
-            for (; i <= n; i++)  {
+            for (; i <= n; i++) {
                 c = acc & 3;
-                result += (c > 0) ? messages[c] : i + ", ";
+                // result += (c > 0) ? messages[c] : i + ', ';
                 acc = acc >> 2 | c << 28;
+                // messages[c]
             }
+            return true;
         }
 
-        this.render(function test(){
-            "use strict";
-            var JT = JMVC.test;
-            
+        this.render(function test () {
+            'use strict';
+            var JT = JMVC.test,
+                times = 10,
+                top = 1E5;
+
             JT.initialize(true);
             JT.startAll();
-            
+
             JMVC.events.loadify(1000);
-            
+
             JT.describe('FizzBuzz time comparison');
 
             JT.message('First version');
@@ -73,16 +74,12 @@ JMVC.controllers.fizzbuzz = function() {
             JT.message('X version');
             JT.code(fb3.toString());
 
-            var times = 10,
-                top = 1E5;
-            JT.describe('<h2>Times comparison</h2>here the 2 functions are executed ' + times + ' times with the same input : '+ top);
-
+            JT.describe('<h2>Times comparison</h2>here the 2 functions are executed ' + times + ' times with the same input : ' + top);
             JT.testTime('fb1', fb1, times, [top]);
             JT.testTime('fb2', fb2, times, [top]);
             JT.testTime('fb3', fb3, times, [top]);
-
             JT.timeSummary();
-            
+
             JT.finishAll();
         });
     };
--- source/app/controllers/test/floor.js
+++ source/app/controllers/test/floor.js
@@ -1,56 +1,53 @@
-JMVC.controllers.floor = function() {
-
-    this.action_index = function(){
-
-        function floor_native(f) {
+JMVC.controllers.floor = function () {
+    this.action_index = function () {
+        function floorNative (f) {
             return Math.floor(f);
         }
-        function floor2NotBw(f) {
+        function floor2NotBw (f) {
             return ~~f;
         }
-        function floor_Bw(f) {
+        function floorBw (f) {
             return f >> 0;
         }
 
-        function floor_OR(f) {
+        function floorOr (f) {
             return f | 0;
         }
 
+        this.render(function test () {
+            'use strict';
+            var JT = JMVC.test,
+                times = 1E4,
+                f = function () { return [JMVC.util.range(1, 1000) + Math.random()]; };
 
-        this.render(function test(){
-            "use strict";
-            var JT = JMVC.test;
-            
             JT.initialize(true);
             JT.startAll();
-            
+
             JMVC.events.loadify(1000);
-            
+
             JT.describe('Floor fast');
 
             JT.message('Native version');
-            JT.code(floor_native.toString());
+            JT.code(floorNative.toString());
 
             JT.message('Double not bitwise version');
             JT.code(floor2NotBw.toString());
 
             JT.message('Bitwise 0 shift version');
-            JT.code(floor_Bw.toString());
+            JT.code(floorBw.toString());
 
             JT.message('OR version');
-            JT.code(floor_OR.toString());
+            JT.code(floorOr.toString());
 
-            var times = 1E4,
-                f = function () {return [JMVC.util.range(1,1000) + Math.random()]; };
             JT.describe('<h2>Times comparison</h2>here the 4 functions are executed ' + times + ' times with the same input');
 
-            JT.testTime('floor_native', floor_native, times, f);
+            JT.testTime('floorNative', floorNative, times, f);
             JT.testTime('floor2NotBw', floor2NotBw, times, f);
-            JT.testTime('floor_bw', floor_Bw, times, f);
-            JT.testTime('floor_OR', floor_OR, times, f);
+            JT.testTime('floor_bw', floorBw, times, f);
+            JT.testTime('floorOr', floorOr, times, f);
 
             JT.timeSummary();
-            
+
             JT.finishAll();
         });
     };
