61568698b490f4ad780b6babdbac5e6addd71c03
fedeghe <fedeghe@gmail.com>
2019-06-12 23:49:44 +0200

    lintz

--- source/app/controllers/test/key.js
+++ source/app/controllers/test/key.js
@@ -1,19 +1,13 @@
 JMVC.require('key/key');
-		
-JMVC.controllers.key = function() {
-	
-	this.action_index = function(s){
-		this.render(false,function test(){
-			"use strict";
-			
-			JMVC.test.initialize(true);
 
-			JMVC.test.startAll();
-			
-			JMVC.test.describe('<time datetime="2012-09-20">2012-09-20</time>');
-			
-			JMVC.test.finishAll();
-		});
-	};
-	
+JMVC.controllers.key = function () {
+    this.action_index = function (s) {
+        this.render(false, function test () {
+            'use strict';
+            JMVC.test.initialize(true);
+            JMVC.test.startAll();
+            JMVC.test.describe('<time datetime="2012-09-20">2012-09-20</time>');
+            JMVC.test.finishAll();
+        });
+    };
 };
