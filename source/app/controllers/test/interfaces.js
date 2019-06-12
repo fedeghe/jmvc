5c795beed45d5683f12d8d70978c23f81e8aff33
fedeghe <fedeghe@gmail.com>
2019-06-12 23:49:38 +0200

    lintz

--- source/app/controllers/test/interfaces.js
+++ source/app/controllers/test/interfaces.js
@@ -1,31 +1,27 @@
 JMVC.controllers.interfaces = function () {
+    this.action_index = function () {
+        JMVC.loadInterfaces('loader');
+        var a = function () { console.debug(arguments); },
+            // three foo constructors
+            T1 = function () {
+                this.load = this.unload = a;
+            },
+            // T2 = function () {
+            //     this.check = this.init = a;
+            // },
+            T3 = function () {
+                this.check =
+                    this.init =
+                    this.parse =
+                    this.inwit =
+                    this.love =
+                    this.hate =
+                    this.destroy = a;
+            };
 
-	this.action_index = function () {
-
-		JMVC.loadInterfaces('loader');
-
-		var a = function () {console.debug(arguments)},
-			//
-			// three foo constructors
- 			T1 = function () {
-				this.load = this.unload = a;
-			},
-			T2 = function () {
-				this.check = this.init = a;
-			},
-			T3 = function () {
-				this.check = 
-				this.init = 
-				this.parse = 
-				this.inwit = 
-				this.love = 
-				this.hate = 
-				this.destroy = a;
-			};
-
-		console.debug(JMVC.Interface.checkImplements(new T1, JMVC.interfaces.loader));
-		console.debug(JMVC.Interface.checkImplements(new T3, JMVC.interfaces.grep, JMVC.interfaces.mankind));
-		// this would fail
-		// console.debug(JMVC.Interface.checkImplements(new T2, JMVC.interfaces.loader))
-	};
+        console.debug(JMVC.Interface.checkImplements(new T1(), JMVC.interfaces.loader));
+        console.debug(JMVC.Interface.checkImplements(new T3(), JMVC.interfaces.grep, JMVC.interfaces.mankind));
+        // this would fail
+        // console.debug(JMVC.Interface.checkImplements(new T2, JMVC.interfaces.loader))
+    };
 };
