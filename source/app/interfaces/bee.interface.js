f3e530af24ceb195e9fecc40fb998298d4f687eb
fedeghe <fedeghe@gmail.com>
2019-07-03 07:52:37 +0200

    daily

--- source/app/interfaces/bee.interface.js
+++ source/app/interfaces/bee.interface.js
@@ -1,5 +1,5 @@
 JMVC.interfaces.bee = JMVC.Interface.create('bee', [
-    function tee(func, obj, str) {return 'obj';},
-    function rule(func, obj, str) {return 'obj';},
-    function run(func, obj, str) {return 'obj';}
+    function tee (func, obj, str) { return 'obj'; },
+    function rule (func, obj, str) { return 'obj'; },
+    function run (func, obj, str) { return 'obj'; }
 ]);
