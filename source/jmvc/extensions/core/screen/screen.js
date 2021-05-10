c772d59176027ce87fb3aa8d8df6702439ffe14c
fedeghe <fedeghe@gmail.com>
2021-05-10 22:46:56 +0200

    x

--- source/jmvc/extensions/core/screen/screen.js
+++ source/jmvc/extensions/core/screen/screen.js
@@ -2,7 +2,7 @@
 //
 
 JMVC.extend('screen', {
-    getViewportSize: function () {
+    getViewportSize: function() {
         if (typeof JMVC.W.innerWidth !== 'undefined') {
             return {
                 width: JMVC.W.innerWidth,
@@ -26,7 +26,7 @@ JMVC.extend('screen', {
         }
     },
 
-    bodySize: function () {
+    bodySize: function() {
         var body = JMVC.WD.body,
             html = JMVC.WD.documentElement;
         return [
@@ -35,7 +35,7 @@ JMVC.extend('screen', {
         ];
     },
 
-    getScreenData: function () {
+    getScreenData: function() {
         return {
             clientWidth: this.f_filterResults(
                 JMVC.W.innerWidth ? JMVC.W.innerWidth : 0,
@@ -60,7 +60,7 @@ JMVC.extend('screen', {
         };
     },
 
-    getNodePosition: function (elem) {
+    getNodePosition: function(elem) {
         var box = elem.getBoundingClientRect(),
             body = document.body,
             docElem = document.documentElement,
@@ -74,7 +74,7 @@ JMVC.extend('screen', {
         };
     },
 
-    f_filterResults: function (nWin, nDocel, nBody) {
+    f_filterResults: function(nWin, nDocel, nBody) {
         var nResult = nWin || 0;
         if (nDocel && (!nResult || (nResult > nDocel))) {
             nResult = nDocel;
@@ -82,14 +82,14 @@ JMVC.extend('screen', {
         return nBody && (!nResult || (nResult > nBody)) ? nBody : nResult;
     },
 
-    no_frames: function () {
+    no_frames: function() {
         if (JMVC.W.top.location !== JMVC.W.location) {
             JMVC.W.top.location.href = JMVC.WD.location.href;
         }
     },
 
-    'window': function () {
-        if (typeof (JMVC.W.innerWidth) === 'number') {
+    'window': function() {
+        if (typeof(JMVC.W.innerWidth) === 'number') {
             // Non-IE
             return {
                 width: JMVC.W.innerWidth,
@@ -115,14 +115,14 @@ JMVC.extend('screen', {
         };
     },
 
-    screen: function () {
+    screen: function() {
         return {
             width: JMVC.W.innerWidth,
             height: JMVC.W.innerHeight
         };
     },
 
-    page: function () {
+    page: function() {
         var d = {
                 width: typeof JMVC.WD.width !== 'undefined' ? JMVC.WD.width : JMVC.WD.body.offsetWidth,
                 height: typeof JMVC.WD.height !== 'undefined' ? JMVC.WD.height : JMVC.WD.body.offsetHeight
@@ -147,7 +147,7 @@ JMVC.extend('screen', {
     },
     */
 
-    canScroll: function (node) {
+    canScroll: function(node) {
         if (typeof node === 'undefined') {
             if (window.innerHeight) {
                 return document.body.offsetHeight > innerHeight;
@@ -160,7 +160,7 @@ JMVC.extend('screen', {
 
     // window.innerHeight >  (document.height || document.body.offsetHeight) ? window.innerHeight : document.height || document.body.offsetHeight
 
-    scroll: function () {
+    scroll: function() {
         return {
             left: JMVC.screen.f_filterResults(
                 JMVC.W.pageXOffset ? JMVC.W.pageXOffset : 0,
@@ -179,7 +179,7 @@ JMVC.extend('screen', {
      * take the screen dimensions of the square that needs to be centerer
      * returns the upper-left absolute position to be used
      */
-    centerHelper: function (w, h) {
+    centerHelper: function(w, h) {
         var win = JMVC.screen.window(),
             scroll = JMVC.screen.scroll();
         return {
@@ -188,7 +188,7 @@ JMVC.extend('screen', {
         };
     },
 
-    fullscreen: function () {
+    fullscreen: function() {
         JMVC.W.moveTo(0, 0);
         if (JMVC.WD.all) {
             JMVC.W.top.window.resizeTo(screen.availWidth, screen.availHeight);
@@ -202,7 +202,7 @@ JMVC.extend('screen', {
     },
 
     // http://davidwalsh.name/fullscreen
-    launchFullscreen: function (el) {
+    launchFullscreen: function(el) {
         el = el || JMVC.WD.body;
         if (el.requestFullscreen) {
             el.requestFullscreen();
@@ -215,7 +215,7 @@ JMVC.extend('screen', {
         }
     },
     // http://davidwalsh.name/fullscreen
-    exitFullscreen: function (el) {
+    exitFullscreen: function(el) {
         // var trg = el || document;
         if (el.exitFullscreen) {
             el.exitFullscreen();
@@ -226,7 +226,7 @@ JMVC.extend('screen', {
         }
     },
     enabled: document.fullscreenEnabled || document.mozFullScreenEnabled || document.webkitFullscreenEnabled,
-    el: function () {
+    el: function() {
         return document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement;
     }
 });
